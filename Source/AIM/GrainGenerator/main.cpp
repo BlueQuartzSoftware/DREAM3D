29db9726bd645779239165a1bcb52be71ce66680
Michael A. Jackson <mike.jackson@bluequartz.net>
2010-11-03 16:50:59 -0400

    Updating command line codes to use TCLAP instead of Boost::program_options

--- Source/AIM/GrainGenerator/main.cpp
+++ Source/AIM/GrainGenerator/main.cpp
@@ -10,6 +10,12 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 
+#include <string>
+#include <iostream>
+
+#include <tclap/CmdLine.h>
+#include <tclap/ValueArg.h>
+
 #include <MXA/Common/LogTime.h>
 #include <MXA/Utilities/MXALogger.h>
 #include <MXA/Utilities/MXAFileSystemPath.h>
@@ -21,13 +27,7 @@
 #include <AIM/GrainGenerator/GrainGenerator.h>
 
 
-#include <string>
-#include <iostream>
 
-//-- Boost Program Options
-#include <boost/program_options.hpp>
-
-#include <iostream>
 
 
 #define CHECK_ARG(var, mandatory)\
@@ -53,101 +53,60 @@ int main(int argc, char **argv)
   std::cout << logTime() << "Starting Synthetic Builder... " << std::endl;
   MXALOGGER_METHOD_VARIABLE_INSTANCE
 
-  std::string inputDir;
-  std::string outputDir;
 
-  int numGrains = 0;
-  int shapeClass = 0;
-
-  double xRes;
-  double yRes;
-  double zRes;
-  double xRes1;
-  double yRes1;
-  double zRes1;
-
-  double overlapAllowed = 0.0;
-  int overlapAssignment = 0;
-
-  int crystruct = AIM::Representation::UnknownCrystalStructure;
-
-  std::string logFile;
-
-  // Handle program options passed on command line.
-  boost::program_options::options_description desc("Possible Parameters");
-  desc.add_options()
-  ("help", "Produce help message")
-  ("inputDir", boost::program_options::value<std::string>(&inputDir), "REQUIRED: Input Directory")
-  ("outputDir", boost::program_options::value<std::string>(&outputDir), "REQUIRED: Output Directory")
-  ("numGrains", boost::program_options::value<int>(&numGrains), "REQUIRED: The total number of grains to be created")
-  ("shapeClass", boost::program_options::value<int>(&shapeClass), "REQUIRED: The shape class you desire (1)Ellipsoid (2)Superellipsoid (3)Cube-Octahedron:")
-  ("xRes,x", boost::program_options::value<double>(&xRes), "REQUIRED: X resolution of your volume")
-  ("yRes,y", boost::program_options::value<double>(&yRes), "REQUIRED: Y resolution of your volume")
-  ("zRes,z", boost::program_options::value<double>(&zRes), "REQUIRED: Z resolution of your volume")
-  ("overlapAllowed", boost::program_options::value<double>(&overlapAllowed), "REQUIRED: The overlap between grains that is acceptable")
-  ("overlapAssignment", boost::program_options::value<int>(&overlapAssignment), "REQUIRED: Enter how the overlap between grains is handled (1)Rigid (2)Progressive.")
-  ("crystruct", boost::program_options::value<int>(&crystruct), "REQUIRED: Do you have a HCP (1) or FCC (2) material")
-  ;
 
   int err = 0;
   try
   {
 
-    boost::program_options::variables_map vm;
-    boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
-    boost::program_options::notify(vm);
+    // Handle program options passed on command line.
+    TCLAP::CmdLine cmd("AIMRepresentation Grain Generator", ' ', AIMRepresentation::Version::Complete);
 
-    // Print help message if requested by user and return.
-    if (vm.count("help") || argc < 2)
-    {
-      std::cout << desc << std::endl;
-      return EXIT_SUCCESS;
-    }
-    if (vm.count("logfile") != 0)
-    {
-      logFile = MXAFileSystemPath::toNativeSeparators(logFile);
-    }
-    if (false == logFile.empty())
-    {
-      mxa_log.open(logFile);
-    }
-    mxa_log << logTime() << "Synthetic Builder Version " << AIMRepresentation::Version::Complete << " Starting " << std::endl;
+    TCLAP::ValueArg<std::string>   inputDir( "i", "inputDir", "Input Directory", true, "", "Input Directory");
+    cmd.add(inputDir);
 
-    mxa_log << "Parameters being used are: " << std::endl;
+    TCLAP::ValueArg<std::string > outputDir("", "outputDir", "Output Directory", true, "", "Output Directory");
+    cmd.add(outputDir);
 
-    CHECK_ARG( inputDir, true);
-    CHECK_ARG( outputDir, true);
-    CHECK_ARG( numGrains, true);
-    CHECK_ARG( shapeClass, true);
-    CHECK_ARG( xRes, true);
-    CHECK_ARG( yRes, true);
-    CHECK_ARG( zRes, true);
-    CHECK_ARG( overlapAllowed, true);
-    CHECK_ARG( overlapAssignment, true);
-    CHECK_ARG( crystruct, true);
+    TCLAP::ValueArg<int>  numGrains( "", "numGrains", "The total number of grains to be created", true, 0, "The total number of grains to be created");
+    cmd.add(numGrains);
+
+
+    TCLAP::ValueArg<int>  shapeClass( "", "shapeClass", "The shape class you desire (1)Ellipsoid (2)Superellipsoid (3)Cube-Octahedron:", false, 1, "The shape class you desire (1)Ellipsoid (2)Superellipsoid (3)Cube-Octahedron:");
+    cmd.add(shapeClass);
+
+    TCLAP::ValueArg<double>  xRes( "x", "xRes", "X resolution of your volume", true, 0.0, "X resolution of your volume");
+    cmd.add(xRes);
+    TCLAP::ValueArg<double>  yRes( "y", "yRes", "Y resolution of your volume", true, 0.0, "Y resolution of your volume");
+    cmd.add(yRes);
+    TCLAP::ValueArg<double>  zRes( "z", "zRes", "Z resolution of your volume", true, 0.0, "Z resolution of your volume");
+    cmd.add(zRes);
+    TCLAP::ValueArg<double>  overlapAllowed( "", "overlapAllowed", "The overlap between grains that is acceptable", true, 0.0, "The overlap between grains that is acceptable");
+    cmd.add(overlapAllowed);
+    TCLAP::ValueArg<int>  overlapAssignment( "", "overlapAssignment", "Enter how the overlap between grains is handled (1)Rigid (2)Progressive.", true, 1, "Enter how the overlap between grains is handled (1)Rigid (2)Progressive.");
+    cmd.add(overlapAssignment);
+    TCLAP::ValueArg<int>  crystruct( "", "crystruct", "Do you have a HCP (1) or FCC (2) material", true, 2, "Do you have a HCP (1) or FCC (2) material");
+    cmd.add(crystruct);
 
     GrainGenerator::Pointer r = GrainGenerator::New();
-    r->setInputDirectory(inputDir);
-    r->setOutputDirectory(outputDir);
-    r->setNumGrains(numGrains);
-    r->setShapeClass(shapeClass);
-    r->setXResolution(xRes);
-    r->setYResolution(yRes);
-    r->setZResolution(zRes);
-    r->setOverlapAllowed(overlapAllowed);
-    r->setOverlapAssignment(overlapAssignment);
-    r->setCrystalStructure( static_cast<AIM::Representation::CrystalStructure>(crystruct) );
+    r->setInputDirectory(inputDir.getValue());
+    r->setOutputDirectory(outputDir.getValue());
+    r->setNumGrains(numGrains.getValue());
+    r->setShapeClass(shapeClass.getValue());
+    r->setXResolution(xRes.getValue());
+    r->setYResolution(yRes.getValue());
+    r->setZResolution(zRes.getValue());
+    r->setOverlapAllowed(overlapAllowed.getValue());
+    r->setOverlapAssignment(overlapAssignment.getValue());
+    r->setCrystalStructure( static_cast<AIM::Representation::CrystalStructure>(crystruct.getValue()) );
 
     r->compute();
     err = r->getErrorCondition();
-  } catch (...)
+  }
+
+  catch (TCLAP::ArgException &e) // catch any exceptions
   {
-    std::cout << "Error on Input: Displaying help listing instead. **" << std::endl;
-    std::cout << desc << std::endl;
-    for (int i = 0; i < argc; ++i)
-    {
-      std::cout << argv[i] << std::endl;
-    }
+    std::cerr << logTime() << " error: " << e.error() << " for arg " << e.argId() << std::endl;
     return EXIT_FAILURE;
   }
   std::cout << "++++++++++++ Synthetic Builder Complete ++++++++++++" << std::endl;
