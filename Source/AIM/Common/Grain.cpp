2283642bc7f4a5e575c92da52baa4a4eaf2e7b29
Michael A. Jackson <mike.jackson@bluequartz.net>
2010-11-19 17:31:06 -0500

    Fixing issue where the voxel list for each grain was not copied correctly between Grain object instances.

--- Source/AIM/Common/Grain.cpp
+++ Source/AIM/Common/Grain.cpp
@@ -58,13 +58,298 @@ radius2(0.0),
 radius3(0.0),
 lowanglefraction(0.0)
 {
-  neighborlist = new std::vector<int>;
-  voxellist = new std::vector<int>;
-  ellipfunclist = new std::vector<double>;
-  neighborsurfarealist = new std::vector<double>;
-  misorientationlist = new std::vector<double>;
+  neighborlist = NULL; //new std::vector<int>;
+  voxellist = NULL; //new std::vector<int>;
+  ellipfunclist = NULL; //new std::vector<double>;
+  misorientationlist = NULL; //new std::vector<double>;
+  neighborsurfarealist = NULL; //new std::vector<double>;
 }
 
 // -----------------------------------------------------------------------------
 //
 // -----------------------------------------------------------------------------
+Grain::Grain(const Grain& grain)
+{
+  numvoxels = grain.numvoxels ;
+  active = grain.active ;
+  packquality = grain.packquality ;
+  numneighbors = grain.numneighbors ;
+  newgrainname = grain.newgrainname ;
+  gotcontainedmerged = grain.gotcontainedmerged ;
+  gottwinmerged = grain.gottwinmerged ;
+  gotcolonymerged = grain.gotcolonymerged ;
+  centroidx = grain.centroidx ;
+  centroidy = grain.centroidy ;
+  centroidz = grain.centroidz ;
+  surfacegrain = grain.surfacegrain ;
+  Ixx = grain.Ixx ;
+  Iyy = grain.Iyy ;
+  Izz = grain.Izz ;
+  Ixy = grain.Ixy;
+  Iyz = grain.Iyz ;
+  Ixz = grain.Ixz ;
+  omega3 = grain.omega3 ;
+  averagemisorientation = grain.averagemisorientation ;
+  kernelmisorientation = grain.kernelmisorientation;
+  twinnewnumber = grain.twinnewnumber;
+  colonynewnumber = grain.colonynewnumber;
+  red = grain.red ;
+  green = grain.green ;
+  blue = grain.blue ;
+  schmidfactor = grain.schmidfactor ;
+  convexity = grain.convexity ;
+  euler1 = grain.euler1 ;
+  euler2 = grain.euler2 ;
+  euler3 = grain.euler3 ;
+  axiseuler1 = grain.axiseuler1 ;
+  axiseuler2 = grain.axiseuler2 ;
+  axiseuler3 = grain.axiseuler3 ;
+  volume = grain.volume ;
+  equivdiameter = grain.equivdiameter ;
+  neigherror = grain.neigherror ;
+  radius1 = grain.radius1 ;
+  radius2 = grain.radius2 ;
+  radius3 = grain.radius3 ;
+  lowanglefraction = grain.lowanglefraction;
+
+  neighborlist = NULL; //new std::vector<int>;
+  voxellist = NULL; //new std::vector<int>;
+  ellipfunclist = NULL; //new std::vector<double>;
+  misorientationlist = NULL; //new std::vector<double>;
+  neighborsurfarealist = NULL; //new std::vector<double>;
+
+  if (NULL != grain.neighborlist)
+  {
+    neighborlist = new std::vector<int >;
+    neighborlist->assign(grain.neighborlist->begin(), grain.neighborlist->end());
+  }
+
+  if (NULL != grain.voxellist)
+  {
+    voxellist = new std::vector<int >;
+    voxellist->assign(grain.voxellist->begin(), grain.voxellist->end());
+  }
+
+  if (NULL != grain.ellipfunclist)
+  {
+    ellipfunclist = new std::vector<double >;
+    ellipfunclist->assign(grain.ellipfunclist->begin(), grain.ellipfunclist->end());
+  }
+  if (NULL != grain.misorientationlist)
+  {
+    misorientationlist = new std::vector<double >;
+    misorientationlist->assign(grain.misorientationlist->begin(), grain.misorientationlist->end());
+  }
+
+  if (NULL != grain.neighborsurfarealist)
+  {
+    neighborsurfarealist = new std::vector<double >;
+    neighborsurfarealist->assign(grain.neighborsurfarealist->begin(), grain.neighborsurfarealist->end());
+  }
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+Grain::Grain(Grain& grain)
+{
+  numvoxels = grain.numvoxels ;
+  active = grain.active ;
+  packquality = grain.packquality ;
+  numneighbors = grain.numneighbors ;
+  newgrainname = grain.newgrainname ;
+  gotcontainedmerged = grain.gotcontainedmerged ;
+  gottwinmerged = grain.gottwinmerged ;
+  gotcolonymerged = grain.gotcolonymerged ;
+  centroidx = grain.centroidx ;
+  centroidy = grain.centroidy ;
+  centroidz = grain.centroidz ;
+  surfacegrain = grain.surfacegrain ;
+  Ixx = grain.Ixx ;
+  Iyy = grain.Iyy ;
+  Izz = grain.Izz ;
+  Ixy = grain.Ixy;
+  Iyz = grain.Iyz ;
+  Ixz = grain.Ixz ;
+  omega3 = grain.omega3 ;
+  averagemisorientation = grain.averagemisorientation ;
+  kernelmisorientation = grain.kernelmisorientation;
+  twinnewnumber = grain.twinnewnumber;
+  colonynewnumber = grain.colonynewnumber;
+  red = grain.red ;
+  green = grain.green ;
+  blue = grain.blue ;
+  schmidfactor = grain.schmidfactor ;
+  convexity = grain.convexity ;
+  euler1 = grain.euler1 ;
+  euler2 = grain.euler2 ;
+  euler3 = grain.euler3 ;
+  axiseuler1 = grain.axiseuler1 ;
+  axiseuler2 = grain.axiseuler2 ;
+  axiseuler3 = grain.axiseuler3 ;
+  volume = grain.volume ;
+  equivdiameter = grain.equivdiameter ;
+  neigherror = grain.neigherror ;
+  radius1 = grain.radius1 ;
+  radius2 = grain.radius2 ;
+  radius3 = grain.radius3 ;
+  lowanglefraction = grain.lowanglefraction;
+
+  neighborlist = NULL; //new std::vector<int>;
+  voxellist = NULL; //new std::vector<int>;
+  ellipfunclist = NULL; //new std::vector<double>;
+  misorientationlist = NULL; //new std::vector<double>;
+  neighborsurfarealist = NULL; //new std::vector<double>;
+
+  if (NULL != grain.neighborlist)
+  {
+    neighborlist = new std::vector<int >;
+    neighborlist->assign(grain.neighborlist->begin(), grain.neighborlist->end());
+  }
+
+  if (NULL != grain.voxellist)
+  {
+    voxellist = new std::vector<int >;
+    voxellist->assign(grain.voxellist->begin(), grain.voxellist->end());
+  }
+
+  if (NULL != grain.ellipfunclist)
+  {
+    ellipfunclist = new std::vector<double >;
+    ellipfunclist->assign(grain.ellipfunclist->begin(), grain.ellipfunclist->end());
+  }
+  if (NULL != grain.misorientationlist)
+  {
+    misorientationlist = new std::vector<double >;
+    misorientationlist->assign(grain.misorientationlist->begin(), grain.misorientationlist->end());
+  }
+
+  if (NULL != grain.neighborsurfarealist)
+  {
+    neighborsurfarealist = new std::vector<double >;
+    neighborsurfarealist->assign(grain.neighborsurfarealist->begin(), grain.neighborsurfarealist->end());
+  }
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+Grain& Grain::operator=(const Grain& grain)
+{
+
+  if (this != &grain) // protect against invalid self-assignment
+  {
+  numvoxels = grain.numvoxels ;
+  active = grain.active ;
+  packquality = grain.packquality ;
+  numneighbors = grain.numneighbors ;
+  newgrainname = grain.newgrainname ;
+  gotcontainedmerged = grain.gotcontainedmerged ;
+  gottwinmerged = grain.gottwinmerged ;
+  gotcolonymerged = grain.gotcolonymerged ;
+  centroidx = grain.centroidx ;
+  centroidy = grain.centroidy ;
+  centroidz = grain.centroidz ;
+  surfacegrain = grain.surfacegrain ;
+  Ixx = grain.Ixx ;
+  Iyy = grain.Iyy ;
+  Izz = grain.Izz ;
+  Ixy = grain.Ixy;
+  Iyz = grain.Iyz ;
+  Ixz = grain.Ixz ;
+  omega3 = grain.omega3 ;
+  averagemisorientation = grain.averagemisorientation ;
+  kernelmisorientation = grain.kernelmisorientation;
+  twinnewnumber = grain.twinnewnumber;
+  colonynewnumber = grain.colonynewnumber;
+  red = grain.red ;
+  green = grain.green ;
+  blue = grain.blue ;
+  schmidfactor = grain.schmidfactor ;
+  convexity = grain.convexity ;
+  euler1 = grain.euler1 ;
+  euler2 = grain.euler2 ;
+  euler3 = grain.euler3 ;
+  axiseuler1 = grain.axiseuler1 ;
+  axiseuler2 = grain.axiseuler2 ;
+  axiseuler3 = grain.axiseuler3 ;
+  volume = grain.volume ;
+  equivdiameter = grain.equivdiameter ;
+  neigherror = grain.neigherror ;
+  radius1 = grain.radius1 ;
+  radius2 = grain.radius2 ;
+  radius3 = grain.radius3 ;
+  lowanglefraction = grain.lowanglefraction;
+
+  neighborlist = NULL; //new std::vector<int>;
+  voxellist = NULL; //new std::vector<int>;
+  ellipfunclist = NULL; //new std::vector<double>;
+  misorientationlist = NULL; //new std::vector<double>;
+  neighborsurfarealist = NULL; //new std::vector<double>;
+
+  if (NULL != grain.neighborlist)
+  {
+    neighborlist = new std::vector<int >;
+    neighborlist->assign(grain.neighborlist->begin(), grain.neighborlist->end());
+  }
+
+  if (NULL != grain.voxellist)
+  {
+    voxellist = new std::vector<int >;
+    voxellist->assign(grain.voxellist->begin(), grain.voxellist->end());
+  }
+
+  if (NULL != grain.ellipfunclist)
+  {
+    ellipfunclist = new std::vector<double >;
+    ellipfunclist->assign(grain.ellipfunclist->begin(), grain.ellipfunclist->end());
+  }
+  if (NULL != grain.misorientationlist)
+  {
+    misorientationlist = new std::vector<double >;
+    misorientationlist->assign(grain.misorientationlist->begin(), grain.misorientationlist->end());
+  }
+
+  if (NULL != grain.neighborsurfarealist)
+  {
+    neighborsurfarealist = new std::vector<double >;
+    neighborsurfarealist->assign(grain.neighborsurfarealist->begin(), grain.neighborsurfarealist->end());
+  }
+  }
+  return *this;
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+Grain::~Grain()
+{
+//  std::cout << "Grain::~Grain() Starting" << std::endl;
+  if (NULL != neighborlist)
+  {
+    delete neighborlist;
+  }
+  if (NULL != voxellist)
+  {
+    delete voxellist;
+  }
+  if (NULL != ellipfunclist)
+  {
+    delete ellipfunclist;
+  }
+  if (NULL != misorientationlist)
+  {
+    delete misorientationlist;
+  }
+  if (NULL != neighborsurfarealist)
+  {
+    delete neighborsurfarealist;
+  }
+
+//  std::cout << "Grain::~Grain() Ending" << std::endl;
+}
+
+
+
+
+
