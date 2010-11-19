948d3dcaad7822f5b53cc946d0e79b493a4b4a1f
Michael Jackson <mike.jackson@bluequartz.net>
2010-11-19 13:58:24 -0500

    Fixing configuration errors on windows

--- Source/MXA/Utilities/SourceList.cmake
+++ Source/MXA/Utilities/SourceList.cmake
@@ -1,3 +1,4 @@
+#message(STATUS "MXA/Utilities/SourceList.cmake")
 SET (MXA_Utilities_SRCS
   ${MXA_SOURCE_DIR}/MXA/Utilities/MXADir.cpp
   ${MXA_SOURCE_DIR}/MXA/Utilities/MXAFileInfo.cpp
@@ -10,16 +11,16 @@ SET (MXA_Utilities_HDRS
   ${MXA_SOURCE_DIR}/MXA/Utilities/MXAFileInfo.h
   ${MXA_SOURCE_DIR}/MXA/Utilities/MXALogger.h
 )
-cmp_IDE_SOURCE_PROPERTIES( "MXA/Utilities" "${MXA_Utilities_HDRS}" "${MXA_Utilities_SRCS}" "0")
+
 IF (MSVC)
     SET (MXA_Utilities_SRCS ${MXA_Utilities_SRCS} ${MXA_SOURCE_DIR}/MXA/Utilities/MXADirent.c )
     SET (MXA_Utilities_HDRS ${MXA_Utilities_HDRS} ${MXA_SOURCE_DIR}/MXA/Utilities/MXADirent.h )
 endif()
+cmp_IDE_SOURCE_PROPERTIES( "MXA/Utilities" "${MXA_Utilities_HDRS}" "${MXA_Utilities_SRCS}" "0")
 
 if ( ${MXA_INSTALL_FILES} EQUAL 1 )
     INSTALL (FILES ${MXA_Utilities_HDRS}
             DESTINATION include/MXA/Utilities
             COMPONENT Headers   )
 endif()
-cmp_IDE_SOURCE_PROPERTIES( "MXA/Utilities" "${MXA_Utilities_HDRS}" "${MXA_Utilities_SRCS}" "0")
 
