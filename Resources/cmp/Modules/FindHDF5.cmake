f176ff8bef22670b458f90ea9be28b782e39d31f
Michael A. Jackson <mike.jackson@bluequartz.net>
2010-11-22 14:32:07 -0500

    Fixing bug where the CMake variable HDF5_INSTALL was overwritten regardless if it was set in the CMakeGUI or from the command line

--- Resources/cmp/Modules/FindHDF5.cmake
+++ Resources/cmp/Modules/FindHDF5.cmake
@@ -19,7 +19,11 @@
 # ------------------ START FIND HDF5 LIBS --------------------------------------
 SET (HDF5_FOUND "NO")
 SET (HDF5_HL_FOUND "NO")
+
+# Only set HDF5_INSTALL to the environment variable if it is blank
+if (${HDF5_INSTALL} STREQUAL "")
 SET (HDF5_INSTALL  $ENV{HDF5_INSTALL})
+endif()
 
 SET(HDF5_INCLUDE_SEARCH_DIRS
   $ENV{HDF5_INSTALL}/include
