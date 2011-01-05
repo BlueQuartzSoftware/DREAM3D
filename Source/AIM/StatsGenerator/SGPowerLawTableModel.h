3d7bba20fe9d883692db264487b19ccd8768f21f
Michael Jackson <mike.jackson@bluequartz.net>
2011-01-05 16:45:11 -0500

    Adding new tablemodel for a PowerLaw Calculation

---
+++ Source/AIM/StatsGenerator/SGPowerLawTableModel.h
@@ -0,0 +1,112 @@
+/* ============================================================================
+ * Copyright (c) 2010, Michael A. Jackson (BlueQuartz Software)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of Michael A. Jackson nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+#ifndef _SG_POWER_LAW_TABLEMODEL_H_
+#define _SG_POWER_LAW_TABLEMODEL_H_
+
+#include <QtCore/QAbstractTableModel>
+#include <QtCore/QVector>
+#include <QtCore/QVariant>
+#include <QtGui/QColor>
+
+
+/**
+ * @class SGPowerLawTableModel SGPowerLawTableModel.h AIM/StatsGenerator/SGPowerLawTableModel.h
+ * @brief
+ * @author Michael A. Jackson for BlueQuartz Software
+ * @date Dec 22, 2010
+ * @version 1.0
+ */
+class SGPowerLawTableModel : public QAbstractTableModel
+{
+
+    Q_OBJECT;
+
+  public:
+
+    enum ColumnIndexes {
+      BinNumber = 0,
+      Alpha,
+      K,
+      Beta,
+      LineColor
+    };
+
+    SGPowerLawTableModel(QObject* parent = 0);
+    virtual ~SGPowerLawTableModel();
+
+    Qt::ItemFlags flags(const QModelIndex &index) const;
+    QVariant data(const QModelIndex &index,
+                  int role=Qt::DisplayRole) const;
+    QVariant headerData(int section, Qt::Orientation orientation,
+                        int role=Qt::DisplayRole) const;
+    int rowCount(const QModelIndex &parent=QModelIndex()) const;
+    int columnCount(const QModelIndex &parent=QModelIndex()) const;
+
+    bool setData(const QModelIndex &index, const QVariant &value,
+                 int role=Qt::EditRole);
+    bool setHeaderData(int, Qt::Orientation, const QVariant&,
+                       int=Qt::EditRole) { return false; }
+
+    bool insertRows(int row, int count,
+                    const QModelIndex &parent=QModelIndex());
+    bool removeRows(int row, int count,
+                    const QModelIndex &parent=QModelIndex());
+
+
+    QVector<qint32>& getBinNumbers() { return m_BinNumbers; }
+    QVector<double>& getAlphas() { return m_Alpha; }
+    QVector<double>& getKs() { return m_K; }
+    QVector<double>& getBetas() { return m_Beta; }
+    QVector<QString>& getColors() { return m_Colors; }
+
+    qint32 getBinNumber(qint32 row) { return m_BinNumbers[row]; }
+    double getAlpha(qint32 row) { return m_Alpha[row]; }
+    double getK(qint32 row) { return m_K[row]; }
+    double getBeta(qint32 row) { return m_Beta[row]; }
+    QString getColor(qint32 row) { return m_Colors[row]; }
+
+
+  private:
+    int m_ColumnCount;
+    int m_RowCount;
+
+    QVector<qint32> m_BinNumbers;
+    QVector<double> m_Alpha;
+    QVector<double> m_K;
+    QVector<double> m_Beta;
+    QVector<QString> m_Colors;
+
+
+    SGPowerLawTableModel(const SGPowerLawTableModel&); // Copy Constructor Not Implemented
+    void operator=(const SGPowerLawTableModel&); // Operator '=' Not Implemented
+};
+
+#endif /* _SG_POWER_LAW_TABLEMODEL_H_ */
