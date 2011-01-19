277a4595c450372cb7437e06f1d6f82b1c03c72b
Mike Jackson <mike.jackson@bluequartz.net>
2011-01-19 16:23:15 -0500

    Adjusting all the text files into properly formatted HTML files

--- License/License.cmake
+++ License/License.cmake
@@ -1,14 +1,17 @@
 # --------------------------------------------------------------------
 # create Resource files for the various license files that are used and 
 # also create a header file that lists all the License Files
-set(LICENSE_FILES ${PROJECT_SOURCE_DIR}/License/Boost.license
+set(LICENSE_FILES 
+                   ${PROJECT_SOURCE_DIR}/ReadMe.txt.in
+                   ${PROJECT_SOURCE_DIR}/License.txt.in
+                   ${PROJECT_SOURCE_DIR}/License/Boost.license
              #     ${PROJECT_SOURCE_DIR}/License/ITK.license
-                  ${PROJECT_SOURCE_DIR}/License/MXA.license
-                  ${PROJECT_SOURCE_DIR}/License/Qt.license
+                   ${PROJECT_SOURCE_DIR}/License/MXA.license
+                   ${PROJECT_SOURCE_DIR}/License/Qt.license
              #     ${PROJECT_SOURCE_DIR}/License/tiff.license 
                    ${PROJECT_SOURCE_DIR}/License/Qwt.license
                    ${PROJECT_SOURCE_DIR}/License/HDF5.license
-             )
+           )
 set(QRC_LICENSE_FILES "")
 set(LICENSE_HEADER_FILE  ${PROJECT_BINARY_DIR}/AIM/License/${PROJECT_NAME}LicenseFiles.h)
 file(WRITE ${LICENSE_HEADER_FILE} "#ifndef _LICENSE_FILES_H_\n")
@@ -19,14 +22,15 @@ foreach(lf ${LICENSE_FILES})
     get_filename_component(cmp_text_file_name ${lf} NAME)
     get_filename_component(lf_fn ${lf} NAME_WE)
     # Copy the text file into the Build Directory
-    configure_file("${lf}" ${PROJECT_BINARY_DIR}/AIM/License/${cmp_text_file_name}   COPYONLY )
+    configure_file("${lf}" ${PROJECT_BINARY_DIR}/AIM/License/${lf_fn}.license   COPYONLY )
         
     # create the Qt Resource File
+    set (CMP_RESOURCE_FILE_NAME ${lf_fn}.license)
     configure_file(${CMP_CONFIGURED_FILES_SOURCE_DIR}/QtResourceFile.qrc.in 
                    ${PROJECT_BINARY_DIR}/AIM/License/${lf_fn}.qrc)
                    
     set(QRC_LICENSE_FILES ${QRC_LICENSE_FILES} ${PROJECT_BINARY_DIR}/AIM/License/${lf_fn}.qrc)
-    file(APPEND ${LICENSE_HEADER_FILE} " << \":/${cmp_text_file_name}\"")
+    file(APPEND ${LICENSE_HEADER_FILE} " << \":/${lf_fn}.license\"")
     
 endforeach(lf ${LICENSE_FILES})
 file(APPEND ${LICENSE_HEADER_FILE}  ");\n")
