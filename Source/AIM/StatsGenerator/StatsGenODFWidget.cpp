0afb391fb30c56b32647ec8d5f0cc3e8c30261a4
Michael A. Jackson <mike.jackson@bluequartz.net>
2011-01-27 13:57:38 -0500

    Adding ODF Tab where the user can create the ODF data and display 3 plots of the data. This consists of a custom QWidget, custom Table Model and Item Delegate

---
+++ Source/AIM/StatsGenerator/StatsGenODFWidget.cpp
@@ -0,0 +1,222 @@
+/* ============================================================================
+ * Copyright (c) 2010, Michael A. Jackson (BlueQuartz Software)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of Michael A. Jackson nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+#include "StatsGenODFWidget.h"
+
+//-- C++ Includes
+#include <iostream>
+
+//-- Qt Includes
+#include <QtGui/QAbstractItemDelegate>
+
+#include <qwt.h>
+#include <qwt_plot.h>
+#include <qwt_plot_curve.h>
+
+
+#include "SGODFTableModel.h"
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+StatsGenODFWidget::StatsGenODFWidget(QWidget *parent) :
+QWidget(parent),
+m_TableModel(NULL)
+{
+  this->setupUi(this);
+  this->setupGui();
+}
+
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+StatsGenODFWidget::~StatsGenODFWidget()
+{
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void StatsGenODFWidget::setPlotTitle(QString title)
+{
+  // this->m_PlotTitle->setText(title);
+}
+
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+int StatsGenODFWidget::readDataFromHDF5(H5ReconStatsReader::Pointer reader,
+                                         QVector<double>  &bins,
+                                         const std::string &hdf5GroupName)
+{
+  int err = 0;
+
+  return err;
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+int StatsGenODFWidget::writeDataToHDF5(H5ReconStatsWriter::Pointer writer,
+                                        const std::string &hdf5GroupName)
+{
+  int err = 0;
+
+  return err;
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void StatsGenODFWidget::setupGui()
+{
+
+  // Setup the TableView and Table Models
+  QHeaderView* headerView = new QHeaderView(Qt::Horizontal, m_TableView);
+  headerView->setResizeMode(QHeaderView::Interactive);
+  m_TableView->setHorizontalHeader(headerView);
+  headerView->show();
+
+  resetTableModel();
+
+  initQwtPlot("Plot 1", "y axis", m_Plot1);
+  initQwtPlot("Plot 2", "y axis", m_Plot2);
+  initQwtPlot("Plot 3", "y axis", m_Plot3);
+
+
+
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void StatsGenODFWidget::initQwtPlot(QString xAxisName, QString yAxisName, QwtPlot* plot)
+{
+  plot->setAxisTitle(QwtPlot::xBottom, xAxisName);
+  plot->setAxisTitle(QwtPlot::yLeft, yAxisName);
+  plot->setCanvasBackground(QColor(Qt::white));
+
+#if 0
+  m_grid = new QwtPlotGrid;
+  m_grid->enableXMin(true);
+  m_grid->enableYMin(true);
+  m_grid->setMajPen(QPen(Qt::gray, 0, Qt::SolidLine));
+  m_grid->setMinPen(QPen(Qt::lightGray, 0, Qt::DotLine));
+  m_grid->attach(m_PlotView);
+#endif
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void StatsGenODFWidget::resetTableModel()
+{
+
+  if (NULL != m_TableModel)
+  {
+    m_TableModel->deleteLater();
+  }
+
+  m_TableModel = new SGODFTableModel;
+
+  m_TableView->setModel(m_TableModel);
+  QAbstractItemDelegate* aid = m_TableModel->getItemDelegate();
+
+  m_TableView->setItemDelegate(aid);
+
+//
+//  connect(m_TableModel, SIGNAL(layoutChanged()),
+//    this, SLOT(updatePlotCurves()));
+//  connect(m_TableModel, SIGNAL(dataChanged(const QModelIndex &, const QModelIndex &)),
+//    this, SLOT(updatePlotCurves()));
+
+}
+
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void StatsGenODFWidget::setXAxisName(QString name)
+{
+//  m_PlotView->setAxisTitle(QwtPlot::xBottom, name);
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void StatsGenODFWidget::setYAxisName(QString name)
+{
+ // m_PlotView->setAxisTitle(QwtPlot::yLeft, name);
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void StatsGenODFWidget::on_m_CalculateODFBtn_clicked()
+{
+  QwtPlotCurve* curve = m_PlotCurves[0];
+  int err = 0;
+
+  int size = 256;
+  QwtArray<double > x;
+  QwtArray<double > y;
+  StatsGen sg;
+  double xMax, yMax;
+
+  // Initialize xMax and yMax....
+
+  // err = sg.CalculateODF(inputs, x, y);
+
+
+  if (err == 1)
+  {
+    //TODO: Present Error Message
+    return;
+  }
+
+  for (int i = 0; i < size; ++i)
+  {
+    //   std::cout << x[i] << "  " << y[i] << std::endl;
+    if (x[i] > xMax)
+    {
+      xMax = x[i];
+    }
+    if (y[i] > yMax)
+    {
+      yMax = y[i];
+    }
+  }
+  curve->setData(x, y);
+
+  m_Plot1->setAxisScale(QwtPlot::yLeft, 0.0, yMax);
+  m_Plot1->setAxisScale(QwtPlot::xBottom, 0.0, xMax);
+
+}
