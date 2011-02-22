e7a7ba8fceae154366e3c8f727779c3cdfb3239d
Michael A. Groeber <michael.groeber@wpafb.af.mil>
2011-02-22 15:35:19 -0500

    fixing errors in misorientationcalculations and quaternions classes that were making no grains form, causing issues in finding the euclidean maps

--- Source/AIM/ANG/H5AngDataLoader.cpp
+++ Source/AIM/ANG/H5AngDataLoader.cpp
@@ -176,7 +176,7 @@ int H5AngDataLoader::loadData(Voxel voxels[], int xpoints, int ypoints, int zpoi
     H5AngReader::Pointer reader = H5AngReader::New();
     reader->setFileName(getFilename());
     reader->setHDF5Path(StringUtils::numToString(slice + m_ZStartIndex));
-    reader->setUserOrigin(AngReader::NoOrientation);
+	reader->setUserOrigin(AngReader::UpperRightOrigin);
 
     err = reader->readFile();
     if (err < 0)
