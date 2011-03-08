914e9bb5cbcd483305ae644b5cb291a34b432a88
Mike Jackson <mike.jackson@bluequartz.net>
2011-03-07 21:34:49 -0500

    Fixing memory leaks in Voxel class by implementing shared pointer wrappers for each of the vector<*> variables.

--- Source/AIM/Common/Voxel.cpp
+++ Source/AIM/Common/Voxel.cpp
@@ -35,9 +35,9 @@ unassigned(0)
   nearestneighbor[0] = 0; nearestneighbor[1] = 0; nearestneighbor[2] = 0;
   nearestneighbordistance[0] = 0.0; nearestneighbordistance[1] = 0.0; nearestneighbordistance[2] = 0.0;
   quat[0] = 1.0; quat[1] = 0.0; quat[2] = 0.0; quat[3] = 0.0; quat[4] = 0.0;
-  grainlist = new std::vector<int>;
-  ellipfunclist = new std::vector<double>;
-  neighborlist = new std::vector<int>;
+  grainlist = IntVectorType(new std::vector<int>(0) );
+  ellipfunclist = DoubleVectorType(new std::vector<double>(0) );
+  neighborlist = IntVectorType(new std::vector<int>(0) );
 }
 
 // -----------------------------------------------------------------------------
@@ -45,9 +45,45 @@ unassigned(0)
 // -----------------------------------------------------------------------------
 Voxel::~Voxel()
 {
-#if 1
+#if 0
   if (NULL != grainlist) { delete grainlist; grainlist = NULL;}
   if (NULL != ellipfunclist) { delete ellipfunclist; ellipfunclist = NULL;}
   if (NULL != neighborlist) { delete neighborlist; neighborlist = NULL;}
 #endif
 }
+
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void Voxel::deepCopy(Voxel* voxel)
+{
+  if (voxel == this) { return; } // The pointers are the same just return
+
+   grainname = voxel->grainname;
+   confidence = voxel->confidence;
+   imagequality = voxel->imagequality;
+   imagequality2 = voxel->imagequality2;
+   ellipfunc = voxel->ellipfunc;
+   alreadychecked = voxel->alreadychecked;
+   COPY_ARRAY_3(nearestneighbor, voxel);
+
+   COPY_ARRAY_3(nearestneighbordistance, voxel);
+   euler1 = voxel->euler1;
+   euler2 = voxel->euler2;
+   euler3 = voxel->euler3;
+   neighbor = voxel->neighbor;
+   numowners = voxel->numowners;
+   grainmisorientation = voxel->grainmisorientation;
+   misorientationgradient = voxel->misorientationgradient;
+   kernelmisorientation = voxel->kernelmisorientation;
+   surfacevoxel = voxel->surfacevoxel;
+   unassigned = voxel->unassigned;
+
+   COPY_ARRAY_5(quat, voxel);
+   DEEP_COPY_SHARED_VECTOR(grainlist, voxel, IntVectorType, int)
+   DEEP_COPY_SHARED_VECTOR(ellipfunclist, voxel, DoubleVectorType, double)
+   DEEP_COPY_SHARED_VECTOR(neighborlist, voxel, IntVectorType, int)
+
+}
+
