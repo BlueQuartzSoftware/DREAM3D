e1bb81119ec908fda61b35ad82df089aee16fd7e
Michael A. Jackson <mike.jackson@bluequartz.net>
2011-03-24 12:47:20 -0400

    Implementing the writing and reading of the HKLFamily data from the .ang file into the HDF5 ang files. The H5AngReader will now also parse the HKLFamily data correctly.

--- Source/AIM/ANG/H5AngReader.h
+++ Source/AIM/ANG/H5AngReader.h
@@ -39,7 +39,56 @@
 #include "AIM/ANG/AngReader.h"
 #include "AIM/ANG/AngPhase.h"
 
-
+/**
+ * @class H5AngReader H5AngReader.h AIM/ANG/H5AngReader.h
+ * @brief
+ * @author Michael A. Jackson for BlueQuartz Software
+ * @date Mar 24, 2011
+ * @version 1.0
+ *
+ * TSL OIM Scans are stored in the HDF5 file into a heirarchy of folders where
+ * each folder stores an individual .ang file. The folder has a simple index as
+ * its name, for example if the index of the .ang file was 11 then the HDF folder
+ * that contains that .ang file's data has an HDF Group name of "11". Within this
+ * folder there are 2 more folders: <b>Data</b> and <b>Header</b>.\n
+ *   The <b>Data</b> folder contains all the columner data from the .ang file where
+ * each column is its own HDF data set. The <b>Header</b> folder contains the complete
+ * header data from the .ang file as either data sets or as subfolders. The dataset
+ * with the name <b>OriginalFile</b> contains the unchanged complete header from
+ * the .ang file in a single string HDF data set. Each of the header keys has been
+ * also saved to its own HDF data set which can be seen with any HDF5 data viewer
+ * application. For the Phase data inside the header a new HDF group is created
+ * called <b>Phases</b> which contains each of the phase data. Again each phase
+ * has an HDF5 group where its data is collected with a simple numerical index
+ * as its name. Within each Phase's HDF Group folder are the various data sets
+ * that make up the Phase data. They are:
+ * @li Phase
+ * @li MaterialName
+ * @li Formula
+ * @li Info
+ * @li Symmetry
+ * @li LatticeConstants
+ * @li NumberHKLFamilies
+ *
+ * The HKLFamilies form another sub hdf group within the Phase group. This folder
+ * contains datasets where each data set holds a single HKLFamily from the Phase
+ * data. The dataset is an HDF5 Compound Data Type where you have the following
+ * layout
+ * @code
+ * typedef struct
+{
+    int h;
+    int k;
+    int l;
+    int s1;
+    float diffractionIntensity;
+    int s2;
+} HKLFamily_t;
+ * @endcode
+ *
+ * After reading the HDF5 file one can use the various methods to extract the phases
+ * or other header information or all the columnar data.
+ */
 class AIMCOMMON_EXPORT H5AngReader : public AngReader
 {
   public:
@@ -58,6 +107,8 @@ class AIMCOMMON_EXPORT H5AngReader : public AngReader
 
   protected:
     H5AngReader();
+
+    int readHKLFamilies(hid_t hklGid, AngPhase::Pointer phase);
 
   private:
     AngPhase::Pointer   m_CurrentPhase;
