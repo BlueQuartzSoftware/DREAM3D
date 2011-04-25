fb774eb6472e2a9d885fd936354dc18140a33acf
Michael A. Jackson <mike.jackson@bluequartz.net>
2011-04-25 16:28:15 -0400

    Fixing license information in source files. Adjusting number of columns for HTML output from DOxygen

--- Source/ANG/H5AngImporter.h
+++ Source/ANG/H5AngImporter.h
@@ -27,6 +27,9 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
  * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+///////////////////////////////////////////////////////////////////////////////
+// This code was partly written under US Air Force Contract FA8650-07-D-5800
+///////////////////////////////////////////////////////////////////////////////
 
 #ifndef _H5ANGIMPORTER_H_
 #define _H5ANGIMPORTER_H_
