c0561f596bf3f64d5928a6759150b3f3a2538d6f
Michael A. Groeber <michael.groeber@wpafb.af.mil>
2011-04-29 10:52:34 -0400

    more changes in the mdf calculation area

--- Source/StatsGenerator/StatsGenMDFWidget.cpp
+++ Source/StatsGenerator/StatsGenMDFWidget.cpp
@@ -120,6 +120,7 @@ void StatsGenMDFWidget::on_m_MDFUpdateBtn_clicked()
   QwtArray<double> y;
 
   QwtArray<double> angles;
+  QwtArray<double> axes;
   QwtArray<double> weights;
   QwtArray<double> odf = generateODFData();
 
@@ -130,10 +131,10 @@ void StatsGenMDFWidget::on_m_MDFUpdateBtn_clicked()
   int size = 1000;
 
   if (m_CrystalStructure == AIM::Reconstruction::Cubic) {
-    err = sg.GenCubicMDFPlotData(angles, weights, x, y, size);
+    err = sg.GenCubicMDFPlotData(angles, axes, weights, odf, x, y, size);
   }
   else if (m_CrystalStructure == AIM::Reconstruction::Hexagonal) {
-    err = sg.GenHexMDFPlotData(angles, weights, x, y, size);
+    err = sg.GenHexMDFPlotData(angles, axes, weights, odf, x, y, size);
   }
 
   std::cout << "on_m_MDFUpdateBtn_clicked" << std::endl;
