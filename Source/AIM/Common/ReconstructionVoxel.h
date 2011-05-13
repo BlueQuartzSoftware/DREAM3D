6f3ef765d7a8860b1ec0915ba4cb2e47750b3691
Michael A. Groeber <michael.groeber@wpafb.af.mil>
2011-05-13 09:37:26 -0400

    adding and removing voxel classes

--- Source/AIM/Common/ReconstructionVoxel.h
+++ Source/AIM/Common/ReconstructionVoxel.h
@@ -29,8 +29,8 @@
  * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
-#ifndef _VOXEL_H_
-#define _VOXEL_H_
+#ifndef _RECONSTRUCTIONVOXEL_H_
+#define _RECONSTRUCTIONVOXEL_H_
 
 #if defined (_MSC_VER)
 #define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
@@ -45,59 +45,54 @@
 #include "AIM/Common/Constants.h"
 
 /**
-* @class Voxel Voxel.h AIM/Common/Voxel.h
+* @class ReconstructionVoxel ReconstructionVoxel.h AIM/Common/ReconstructionVoxel.h
 * @brief Support class for the MicroGen3D class
 * @author Michael A. Jackson for BlueQuartz Software, Dr. Michael Groeber for USAFRL
 * @date Nov 4, 2009
 * @version 1.0
 */
-class AIMCOMMON_EXPORT Voxel
+class AIMCOMMON_EXPORT ReconstructionVoxel
 {
   public:
-    MXA_SHARED_POINTERS(Voxel);
-    MXA_STATIC_NEW_MACRO(Voxel);
-    MXA_TYPE_MACRO(Voxel);
-    Voxel();
-    virtual ~Voxel();
+    MXA_SHARED_POINTERS(ReconstructionVoxel);
+    MXA_STATIC_NEW_MACRO(ReconstructionVoxel);
+    MXA_TYPE_MACRO(ReconstructionVoxel);
+    ReconstructionVoxel();
+    virtual ~ReconstructionVoxel();
 
     /**
      * @brief Performs a Depp copy of the Grain object by making copies of the contained
      * data and not just copying the pointers to the data
      * @param grain Another Grain object to copy from.
      */
-    void deepCopy(Voxel* voxel);
+    void deepCopy(ReconstructionVoxel* reconstructionvoxel);
 
     int grainname;
-    double confidence;
-    double imagequality;
-    double imagequality2;
-   	double ellipfunc;
+    float confidence;
+    float imagequality;
     int alreadychecked;
     int nearestneighbor[3];
-    double nearestneighbordistance[3];
-    double euler1;
-    double euler2;
-    double euler3;
+    float nearestneighbordistance[3];
+    float euler1;
+    float euler2;
+    float euler3;
     int phase;
     int neighbor;
-    int numowners;
-    double grainmisorientation;
-    double misorientationgradient;
-    double kernelmisorientation;
+    float grainmisorientation;
+    float misorientationgradient;
+    float kernelmisorientation;
     int surfacevoxel;
     int unassigned;
-    double quat[5];
+    float quat[5];
 
-    IntVectorType grainlist;
-    DoubleVectorType ellipfunclist;
     IntVectorType neighborlist;
 
   protected:
 
 
   private:
-    Voxel(const Voxel&); // Copy Constructor Not Implemented
-    void operator=(const Voxel&); // Operator '=' Not Implemented
+    ReconstructionVoxel(const ReconstructionVoxel&); // Copy Constructor Not Implemented
+    void operator=(const ReconstructionVoxel&); // Operator '=' Not Implemented
 };
 
-#endif /* VOXEL_H_ */
+#endif /* RECONSTRUCTIONVOXEL_H_ */
