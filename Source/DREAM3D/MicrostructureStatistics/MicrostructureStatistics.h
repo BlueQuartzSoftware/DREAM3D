ce9e82d57e43e00266dc0c833953fdce288cf36a
Michael A. Jackson <mike.jackson@bluequartz.net>
2011-06-07 10:39:51 -0400

    Adding a BinStepSize property

--- Source/DREAM3D/MicrostructureStatistics/MicrostructureStatistics.h
+++ Source/DREAM3D/MicrostructureStatistics/MicrostructureStatistics.h
@@ -77,6 +77,9 @@ class DREAM3DLib_EXPORT MicrostructureStatistics  : public AbstractPipeline
     MXA_INSTANCE_PROPERTY(bool, ComputeODF);
     MXA_INSTANCE_PROPERTY(bool, ComputeMDF);
 
+    // Options
+    MXA_INSTANCE_PROPERTY(float, BinStepStize);
+
     /**
     * @brief
     * @param ostream
