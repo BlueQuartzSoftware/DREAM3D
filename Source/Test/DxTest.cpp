2e3b11c16a1d8f05d7f853e331a194453a164119
Michael A. Jackson <mike.jackson@bluequartz.net>
2011-06-10 08:36:25 -0400

    Adding in an experimental Dx file writer to the Reconstruction Module to test the latest Surface Meshing Codes from Sukbin Lee at CMU. Adding a quick sanity check test to convert the dx file back to a vtk file

---
+++ Source/Test/DxTest.cpp
@@ -0,0 +1,41 @@
+
+
+#include <iostream>
+
+
+#include "DREAM3D/GrainGenerator/GrainGeneratorFunc.h"
+#include "DREAM3D/StructureReaders/DXStructureReader.h"
+#include "DREAM3D/Common/VTKUtils/VTKFileWriters.hpp"
+
+
+
+int main(int argc, char **argv)
+{
+
+  GrainGeneratorFunc::Pointer m = GrainGeneratorFunc::New();
+
+  DXStructureReader::Pointer reader = DXStructureReader::New();
+  reader->setInputFileName(argv[1]);
+
+  std::cout << "Reading DX File...." << std::endl;
+
+  int err = reader->readStructure(m.get());
+  if (err < 0)
+  {
+    std::cout << "Error Reading Structure." << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  std::cout << "Writing VTK File..." << std::endl;
+  VTKFileWriters::Pointer writer = VTKFileWriters::New();
+  err = writer->writeGrainVisualizationFile(m.get(), argv[2], false, false, false);
+  if (err < 0)
+  {
+    std::cout << "Error Writing VTK Structure." << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  std::cout << "Done." << std::endl;
+  return EXIT_SUCCESS;
+}
+
