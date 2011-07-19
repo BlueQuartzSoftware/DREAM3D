e35ea8d30708af6226812cbad453b853dac49a1e
Michael A. Jackson <mike.jackson@bluequartz.net>
2011-07-19 15:47:21 -0400

    Removing extra code that is not needed

--- Source/Plugins/Reconstruction/UI/ReconstructionWidget.cpp
+++ Source/Plugins/Reconstruction/UI/ReconstructionWidget.cpp
@@ -98,10 +98,8 @@ void ReconstructionWidget::readSettings(QSettings &prefs)
 
   prefs.beginGroup("Reconstruction");
   READ_FILEPATH_SETTING(prefs, m_, H5InputFile, "");
-  if (verifyPathExists(m_H5InputFile->text(), m_H5InputFile) )
-  {
-    on_m_H5InputFile_textChanged(QString(""));
-  }
+  on_m_H5InputFile_textChanged(QString(""));
+
   READ_FILEPATH_SETTING(prefs, m_, OutputDir, "");
   READ_STRING_SETTING(prefs, m_, OutputFilePrefix, "Reconstruction_")
 
@@ -328,6 +326,10 @@ void ReconstructionWidget::on_m_H5InputFile_textChanged(const QString &text)
 
   if (verifyPathExists(m_H5InputFile->text(), m_H5InputFile) )
   {
+    QFileInfo fi(m_H5InputFile->text());
+    QString outPath = fi.absolutePath() + QDir::separator() + fi.baseName() + "_Reconstruction";
+    outPath = QDir::toNativeSeparators(outPath);
+    m_OutputDir->setText(outPath);
     m_SetSliceInfo();
   }
 
