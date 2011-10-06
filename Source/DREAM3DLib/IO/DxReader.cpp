67620d7060f43393b6e98ecb97f8401eb1b66742
Mike Jackson <mike.jackson@bluequartz.net>
2011-10-06 09:39:39 -0400

    Fixing test error with stats gen and fixing segfault with the DxReader code on windows

--- Source/DREAM3DLib/IO/DxReader.cpp
+++ Source/DREAM3DLib/IO/DxReader.cpp
@@ -191,7 +191,7 @@ int DxReader::readFile()
     //    if(tokens.size()==20)
     //      finished_header = true;
 
-    if(finished_header && ((tokens[0] == "attribute") || static_cast<int>(index) == nz * ny * nx))
+    if( static_cast<int>(index) == nz * ny * nx || finished_header && tokens.size() != 0 && tokens[0] == "attribute" )
     {
       finished_data = true;
     }
