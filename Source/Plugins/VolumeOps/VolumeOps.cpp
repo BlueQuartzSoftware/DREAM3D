159c23c81d7e940de63ffe34a48b89599ad56897
Michael Groeber <michael.groeber@wpafb.af.mil>
2011-12-09 09:27:30 -0500

    breaking volume ops into filters

--- Source/Plugins/VolumeOps/VolumeOps.cpp
+++ Source/Plugins/VolumeOps/VolumeOps.cpp
@@ -45,7 +45,9 @@
 #include "DREAM3DLib/HDF5/H5VoxelReader.h"
 #include "DREAM3DLib/HDF5/H5VoxelWriter.h"
 
-#include "VolumeOps/Algorithms/CropVolume.h"
+#include "DREAM3DLib/StatisticsFilters/LoadVolume.h"
+#include "DREAM3DLib/ManipulationFilters/CropVolume.h"
+#include "DREAM3DLib/ManipulationFilters/ChangeResolution.h"
 
 // -----------------------------------------------------------------------------
 //
@@ -70,65 +72,65 @@ void VolumeOps::execute()
   // Start the Benchmark Clock
   START_CLOCK()
 
-  m = VolumeOpsFunc::New();
-
-
-  updateProgressAndMessage("Reading the Voxel Dimensions and Resolution", 2);
-
-  // Load up the voxel data
-  H5VoxelReader::Pointer h5Reader = H5VoxelReader::New();
-  h5Reader->setFileName(m_H5InputFile);
-  int dims[3];
-  float spacing[3];
-  float origin[3];
-  err = h5Reader->getSizeResolutionOrigin(dims, spacing, origin);
-  if (err < 0)
-  {
-    CHECK_FOR_ERROR(VolumeOpsFunc, "Error Reading the Dimensions and Resolution from the input File.", err);
-    setErrorCondition(err);
-    return;
-  }
-
-  m->xpoints_old = dims[0];
-  m->ypoints_old = dims[1];
-  m->zpoints_old = dims[2];
-  m->totalpoints_old = dims[0] * dims[1] * dims[2];
-  m->resx_old = spacing[0];
-  m->resy_old = spacing[1];
-  m->resz_old = spacing[2];
-  m->sizex_old = dims[0]*spacing[0];
-  m->sizey_old = dims[1]*spacing[1];
-  m->sizez_old = dims[2]*spacing[2];
-  m->resx = m_XRes;
-  m->resy = m_YRes;
-  m->resz = m_ZRes;
-  m->sizex = (m_XMax-m_XMin)*m->resx_old;
-  m->sizey = (m_YMax-m_YMin)*m->resy_old;
-  m->sizez = (m_ZMax-m_ZMin)*m->resz_old;
-  m->xpoints = int(m->sizex / m->resx);
-  m->ypoints = int(m->sizey / m->resy);
-  m->zpoints = int(m->sizez / m->resz);
-  m->totalpoints = m->xpoints * m->ypoints * m->zpoints;
-  m->xstart = m_XMin*spacing[0];
-  m->ystart = m_YMin*spacing[1];
-  m->zstart = m_ZMin*spacing[2];
-
-  m->initialize();
+  m = DataContainer::New();
 
   updateProgressAndMessage(("Reading the Voxel Data from the HDF5 File"), 10);
-  err = h5Reader->readVoxelData(m->m_GrainIndicies_old, m->m_Phases_old, m->m_Euler1s_old, m->m_Euler2s_old, m->m_Euler3s_old, m->crystruct, m->phaseType, m->totalpoints_old);
-  CHECK_FOR_ERROR(VolumeOpsFunc, (h5Reader->getErrorMessage()), err);
-  CHECK_FOR_CANCELED(VolumeOpsFunc, "VolumeOps was canceled",  readVoxelData)
+  LoadVolume::Pointer load_volume = LoadVolume::New();
+  load_volume->setInputFile(m_H5InputFile);
+  load_volume->addObserver(static_cast<Observer*>(this));
+  load_volume->setDataContainer(m.get());
+  load_volume->execute();
+  err = load_volume->getErrorCondition();
+  CHECK_FOR_ERROR(DataContainer, "Error Loading Volume", err);
+  CHECK_FOR_CANCELED(DataContainer, "VolumeOps was canceled",  load_volume)
 
- // updateProgressAndMessage(("Operating on Volume"), 50);
-  CropVolume::Pointer cropVolume = CropVolume::New();
-  cropVolume->setDataPtr(m);
-  cropVolume->addObserver(this);
-  cropVolume->run();
+  float sizex = (m_XMax-m_XMin)*m->resx;
+  float sizey = (m_YMax-m_YMin)*m->resy;
+  float sizez = (m_ZMax-m_ZMin)*m->resz;
+  int xp = int(sizex / m_XRes);
+  int yp = int(sizey / m_YRes);
+  int zp = int(sizez / m_ZRes);
+  int totpoints = xp * yp * zp;
+  float xstart = m_XMin*m->resx;
+  float ystart = m_YMin*m->resy;
+  float zstart = m_ZMin*m->resz;
 
+  // updateProgressAndMessage(("Operating on Volume"), 50);
+  if(m->xpoints != xp || m->ypoints != yp || m->zpoints != zp)
+  {
+	  updateProgressAndMessage(("Cropping Volume"), 50);
+	  CropVolume::Pointer crop_volume = CropVolume::New();
+	  crop_volume->setXStart(xstart);
+	  crop_volume->setYStart(ystart);
+	  crop_volume->setZStart(zstart);
+	  crop_volume->setXP(xp);
+	  crop_volume->setYP(yp);
+	  crop_volume->setZP(zp);
+	  crop_volume->addObserver(static_cast<Observer*>(this));
+	  crop_volume->setDataContainer(m.get());
+	  crop_volume->execute();
+	  err = crop_volume->getErrorCondition();
+	  CHECK_FOR_ERROR(DataContainer, "Error Croping Volume", err);
+	  CHECK_FOR_CANCELED(DataContainer, "VolumeOps was canceled",  crop_volume)
+  }
 
-  CHECK_FOR_ERROR(VolumeOpsFunc, "Error operating on volume", err);
-  CHECK_FOR_CANCELED(VolumeOpsFunc, "VolumeOps was canceled",  manipulate_volume)
+  if(m->resx != m_XRes || m->resy != m_YRes || m->resz != m_ZRes)
+  {
+	  updateProgressAndMessage(("Changing Resolution"), 50);
+	  ChangeResolution::Pointer change_resolution = ChangeResolution::New();
+	  change_resolution->setXRes(m_XRes);
+	  change_resolution->setYRes(m_YRes);
+	  change_resolution->setZRes(m_ZRes);
+	  change_resolution->setXP(xp);
+	  change_resolution->setYP(yp);
+	  change_resolution->setZP(zp);
+	  change_resolution->addObserver(static_cast<Observer*>(this));
+	  change_resolution->setDataContainer(m.get());
+	  change_resolution->execute();
+	  err = change_resolution->getErrorCondition();
+	  CHECK_FOR_ERROR(DataContainer, "Error Changing Resolution", err);
+	  CHECK_FOR_CANCELED(DataContainer, "VolumeOps was canceled",  change_resolution)
+  }
 
   /** ********** This section writes the Voxel Data for the VolumeOps Module *** */
   // Create a new HDF5 Volume file by overwriting any HDF5 file that may be in the way
@@ -136,12 +138,12 @@ void VolumeOps::execute()
   H5VoxelWriter::Pointer h5VolWriter = H5VoxelWriter::New();
   if (h5VolWriter.get() == NULL)
   {
-    CHECK_FOR_ERROR(VolumeOpsFunc, "The HDF5 Voxel file could not be created. Does the path exist and do you have write access to the output directory.", -1)
+    CHECK_FOR_ERROR(DataContainer, "The HDF5 Voxel file could not be created. Does the path exist and do you have write access to the output directory.", -1)
   }
   h5VolWriter->setFileName(m_H5OutputFile);
   updateProgressAndMessage(("Writing HDF5 Voxel Data File"), 90);
-  err = h5VolWriter->writeData<VolumeOpsFunc>(m.get());
-  CHECK_FOR_ERROR(VolumeOpsFunc, "The HDF5 Voxel file could not be written to. Does the path exist and do you have write access to the output directory.", err);
+  err = h5VolWriter->writeData<DataContainer>(m.get());
+  CHECK_FOR_ERROR(DataContainer, "The HDF5 Voxel file could not be written to. Does the path exist and do you have write access to the output directory.", err);
 
   updateProgressAndMessage(("Generation Completed"), 100);
 }
--- Source/Plugins/VolumeOps/VolumeOpsFunc.cpp
+++ Source/Plugins/VolumeOps/VolumeOpsFunc.cpp
@@ -43,36 +43,7 @@
 // -----------------------------------------------------------------------------
 VolumeOpsFunc::VolumeOpsFunc()
 {
-  m_HexOps = HexagonalOps::New();
-  m_OrientationOps.push_back(dynamic_cast<OrientationMath*> (m_HexOps.get()));
 
-  m_CubicOps = CubicOps::New();
-  m_OrientationOps.push_back(dynamic_cast<OrientationMath*> (m_CubicOps.get()));
-
-  m_OrthoOps = OrthoRhombicOps::New();
-  m_OrientationOps.push_back(dynamic_cast<OrientationMath*> (m_OrthoOps.get()));
-
-  grain_indicies = NULL;
-  phases = NULL;
-  euler1s = NULL;
-  euler2s = NULL;
-  euler3s = NULL;
-  grain_indicies_old = NULL;
-  phases_old = NULL;
-  euler1s_old = NULL;
-  euler2s_old = NULL;
-  euler3s_old = NULL;
-
-  INIT_AIMARRAY(m_GrainIndicies,int);
-  INIT_AIMARRAY(m_Phases,int);
-  INIT_AIMARRAY(m_Euler1s,float);
-  INIT_AIMARRAY(m_Euler2s,float);
-  INIT_AIMARRAY(m_Euler3s,float);
-  INIT_AIMARRAY(m_GrainIndicies_old,int);
-  INIT_AIMARRAY(m_Phases_old,int);
-  INIT_AIMARRAY(m_Euler1s_old,float);
-  INIT_AIMARRAY(m_Euler2s_old,float);
-  INIT_AIMARRAY(m_Euler3s_old,float);
 }
 
 // -----------------------------------------------------------------------------
@@ -86,30 +57,5 @@ VolumeOpsFunc::~VolumeOpsFunc()
 // -----------------------------------------------------------------------------
 //
 // -----------------------------------------------------------------------------
-void VolumeOpsFunc::initialize()
-{
-  grain_indicies = m_GrainIndicies->WritePointer(0, totalpoints);
-  phases = m_Phases->WritePointer(0, totalpoints);
-  euler1s = m_Euler1s->WritePointer(0, totalpoints);
-  euler2s = m_Euler2s->WritePointer(0, totalpoints);
-  euler3s = m_Euler3s->WritePointer(0, totalpoints);
-  for (int i = 0; i < totalpoints; ++i)
-  {
-    euler1s[i] = -1.0f;
-    euler2s[i] = -1.0f;
-    euler3s[i] = -1.0f;
-  }
-  grain_indicies_old = m_GrainIndicies_old->WritePointer(0, totalpoints_old);
-  phases_old = m_Phases_old->WritePointer(0, totalpoints_old);
-  euler1s_old = m_Euler1s_old->WritePointer(0, totalpoints_old);
-  euler2s_old = m_Euler2s_old->WritePointer(0, totalpoints_old);
-  euler3s_old = m_Euler3s_old->WritePointer(0, totalpoints_old);
-  for (int i = 0; i < totalpoints_old; ++i)
-  {
-    euler1s_old[i] = -1.0f;
-    euler2s_old[i] = -1.0f;
-    euler3s_old[i] = -1.0f;
-  }
-}
 
 
