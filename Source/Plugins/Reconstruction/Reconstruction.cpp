d45a9e5c99ee5cf8ba7d297f13217fb18264f87c
Mike Jackson <mike.jackson@bluequartz.net>
2011-12-15 16:36:22 -0500

    Updating Reconstruction to the newer pipeline code layout

--- Source/Plugins/Reconstruction/Reconstruction.cpp
+++ Source/Plugins/Reconstruction/Reconstruction.cpp
@@ -54,28 +54,15 @@
 #include "DREAM3DLib/ReconstructionFilters/MergeTwins.h"
 #include "DREAM3DLib/ReconstructionFilters/MergeColonies.h"
 
-
-
 #define MIKE_G_DEBUG 0
-
 
 // -----------------------------------------------------------------------------
 //
 // -----------------------------------------------------------------------------
 Reconstruction::Reconstruction() :
-m_H5AngFile(""),
-m_OutputDirectory("."),
-m_OutputFilePrefix("Reconstruction_"),
-m_MergeTwins(false),
-m_MergeColonies(false),
-m_MinAllowedGrainSize(0),
-m_MisorientationTolerance(0.0),
-m_WriteBinaryVTKFiles(true),
-m_WriteVtkFile(true),
-m_WritePhaseId(true),
+    m_H5AngFile(""), m_OutputDirectory("."), m_OutputFilePrefix("Reconstruction_"), m_MergeTwins(false), m_MergeColonies(false), m_MinAllowedGrainSize(0), m_MisorientationTolerance(0.0), m_WriteBinaryVTKFiles(true), m_WriteVtkFile(true), m_WritePhaseId(true),
 //m_WriteImageQuality(true),
-m_WriteIPFColor(true),
-m_WriteHDF5GrainFile(false)
+    m_WriteIPFColor(true), m_WriteHDF5GrainFile(false)
 {
 
 }
@@ -90,8 +77,6 @@ Reconstruction::~Reconstruction()
 #define MAKE_OUTPUT_FILE_PATH(outpath, filename)\
     std::string outpath = m_OutputDirectory + MXADir::Separator + m_OutputFilePrefix + filename;
 
-
-
 // -----------------------------------------------------------------------------
 //
 // -----------------------------------------------------------------------------
@@ -105,7 +90,8 @@ void Reconstruction::execute()
 
   m_OutputDirectory = MXADir::toNativeSeparators(m_OutputDirectory);
 
-  START_CLOCK()
+  // Create a Vector to hold all the filters. Later on we will execute all the filters
+  std::vector<AbstractFilter::Pointer> pipeline;
 
   updateProgressAndMessage(("Loading Slices"), 10);
   LoadSlices::Pointer load_slices = LoadSlices::New();
@@ -116,81 +102,73 @@ void Reconstruction::execute()
   load_slices->setQualityMetricFilters(getQualityMetricFilters());
   load_slices->setRefFrameZDir(getRefFrameZDir());
   load_slices->setMisoTolerance(m_MisorientationTolerance);
-  load_slices->addObserver(static_cast<Observer*>(this));
-  load_slices->setDataContainer(m.get());
-  setCurrentFilter(load_slices);
-  load_slices->execute();
-  err = load_slices->getErrorCondition();
-  CHECK_FOR_ERROR(DataContainer, "Error Loading Slices", err)
-  CHECK_FOR_CANCELED(DataContainer, "Reconstruction Was canceled", load_slices)
+  pipeline.push_back(load_slices);
 
-  updateProgressAndMessage(("Aligning Sections"), 20);
   AlignSections::Pointer align_sections = AlignSections::New();
   align_sections->setmisorientationtolerance(m_MisorientationTolerance);
   align_sections->setalignmeth(m_AlignmentMethod);
-  align_sections->addObserver(static_cast<Observer*>(this));
-  align_sections->setDataContainer(m.get());
-  setCurrentFilter(align_sections);
-  align_sections->execute();
-  err = align_sections->getErrorCondition();
-  CHECK_FOR_ERROR(DataContainer, "Error Aligning Sections", err)
-  CHECK_FOR_CANCELED(DataContainer, "Reconstruction Was canceled", align_sections)
+  pipeline.push_back(align_sections);
 
-  updateProgressAndMessage(("Segmenting Grains"), 30);
   SegmentGrains::Pointer segment_grains = SegmentGrains::New();
   segment_grains->setMisoTolerance(m_MisorientationTolerance);
-  segment_grains->addObserver(static_cast<Observer*>(this));
-  segment_grains->setDataContainer(m.get());
-  setCurrentFilter(segment_grains);
-  segment_grains->execute();
-  err = segment_grains->getErrorCondition();
-  CHECK_FOR_ERROR(DataContainer, "Error Segmenting Grains", err)
-  CHECK_FOR_CANCELED(DataContainer, "Reconstruction Was canceled", segment_grains)
+  pipeline.push_back(segment_grains);
 
-  updateProgressAndMessage(("Cleaning Up Grains"), 40);
   CleanupGrains::Pointer cleanup_grains = CleanupGrains::New();
   cleanup_grains->setmisorientationtolerance(m_MisorientationTolerance);
-  cleanup_grains->addObserver(static_cast<Observer*>(this));
-  cleanup_grains->setDataContainer(m.get());
-  setCurrentFilter(cleanup_grains);
-  cleanup_grains->execute();
-  err = cleanup_grains->getErrorCondition();
-  CHECK_FOR_ERROR(DataContainer, "Error Cleaning Up Grains", err)
-  CHECK_FOR_CANCELED(DataContainer, "Reconstruction was canceled", cleanup_grains)
+  pipeline.push_back(cleanup_grains);
 
-  if (m_MergeTwins == true)
+
+  if(m_MergeTwins == true)
   {
-    updateProgressAndMessage(("Merging Twins"), 60);
-	MergeTwins::Pointer merge_twins = MergeTwins::New();
-	merge_twins->addObserver(static_cast<Observer*>(this));
-	merge_twins->setDataContainer(m.get());
-	setCurrentFilter(merge_twins);
-	merge_twins->execute();
-	err = merge_twins->getErrorCondition();
-	CHECK_FOR_ERROR(DataContainer, "Error Merging Twins", err)
-	CHECK_FOR_CANCELED(DataContainer, "Reconstruction Was canceled", merge_twins)
-    CHECK_FOR_CANCELED(DataContainer, "Reconstruction was canceled", merge_twins)
+    MergeTwins::Pointer merge_twins = MergeTwins::New();
+    pipeline.push_back(merge_twins);
   }
 
-  if (m_MergeColonies == true)
+  if(m_MergeColonies == true)
   {
-    updateProgressAndMessage(("Merging Colonies"), 60);
-	MergeColonies::Pointer merge_colonies = MergeColonies::New();
-	merge_colonies->addObserver(static_cast<Observer*>(this));
-	merge_colonies->setDataContainer(m.get());
-	setCurrentFilter(merge_colonies);
-	merge_colonies->execute();
-	err = merge_colonies->getErrorCondition();
-	CHECK_FOR_ERROR(DataContainer, "Error Merging Colonies", err)
-	CHECK_FOR_CANCELED(DataContainer, "Reconstruction Was canceled", merge_colonies)
+    MergeColonies::Pointer merge_colonies = MergeColonies::New();
+    pipeline.push_back(merge_colonies);
   }
 
+  // Start a Benchmark Clock so we can keep track of each filter's execution time
+  START_CLOCK()
+  // Start looping through the Pipeline
+  float progress = 0.0f;
+  std::stringstream ss;
+  for (std::vector<AbstractFilter::Pointer>::iterator filter = pipeline.begin(); filter != pipeline.end(); ++filter)
+  {
+    progress = progress + 1.0f;
+    pipelineProgress(progress / (pipeline.size() + 1) * 100.0f);
+    ss.str("");
+    ss << "Executing Filter [" << progress << "/" << pipeline.size() << "] - " << (*filter)->getNameOfClass();
+    pipelineProgressMessage(ss.str());
+    (*filter)->addObserver(static_cast<Observer*>(this));
+    (*filter)->setDataContainer(m.get());
+    setCurrentFilter(*filter);
+    (*filter)->execute();
+    (*filter)->removeObserver(static_cast<Observer*>(this));
+    err = (*filter)->getErrorCondition();
+    if(err < 0)
+    {
+      setErrorCondition(err);
+      pipelineErrorMessage((*filter)->getErrorMessage().c_str());
+      pipelineProgress(100);
+      pipelineFinished();
+      return;
+    }CHECK_FOR_CANCELED(DataContainer, "Reconstruction was canceled", write_fielddata)
+
+    if(DREAM3D_BENCHMARKS)
+    {
+      std::cout << (*filter)->getNameOfClass() << " Finish Time(ms): " << (MXA::getMilliSeconds() - millis) << std::endl;
+      millis = MXA::getMilliSeconds();
+    }
+  }
 
   /** ********** This section writes the Voxel Data for the Stats Module *** */
   // Create a new HDF5 Volume file by overwriting any HDF5 file that may be in the way
-  MAKE_OUTPUT_FILE_PATH ( hdf5VolumeFile, DREAM3D::Reconstruction::H5VoxelFile)
+  MAKE_OUTPUT_FILE_PATH( hdf5VolumeFile, DREAM3D::Reconstruction::H5VoxelFile)
   H5VoxelWriter::Pointer h5VolWriter = H5VoxelWriter::New();
-  if (h5VolWriter.get() == NULL)
+  if(h5VolWriter.get() == NULL)
   {
     CHECK_FOR_ERROR(DataContainer, "The HDF5 Voxel file could not be created. Does the path exist and do you have write access to the output directory.", -1)
   }
@@ -199,12 +177,11 @@ void Reconstruction::execute()
   err = h5VolWriter->writeData<DataContainer>(m.get());
   CHECK_FOR_ERROR(DataContainer, "The HDF5 Voxel file could not be written to. Does the path exist and do you have write access to the output directory.", err);
 
-
   /** ********** This section writes the VTK files for visualization *** */
   updateProgressAndMessage(("Writing VTK Visualization File"), 93);
-  if (m_WriteVtkFile)
+  if(m_WriteVtkFile)
   {
-    MAKE_OUTPUT_FILE_PATH ( reconVisFile, DREAM3D::Reconstruction::VisualizationVizFile);
+    MAKE_OUTPUT_FILE_PATH( reconVisFile, DREAM3D::Reconstruction::VisualizationVizFile);
 
     // Setup all the classes that will help us write the Scalars to the VTK File
     std::vector<VtkScalarWriter*> scalarsToWrite;
@@ -218,16 +195,16 @@ void Reconstruction::execute()
       scalarsToWrite.push_back(w0);
     }
 
-    if (m_WritePhaseId == true){
-      VtkScalarWriter* w0 =
-        static_cast<VtkScalarWriter*>(new VoxelGoodVoxelScalarWriter<DataContainer>(m.get()));
+    if(m_WritePhaseId == true)
+    {
+      VtkScalarWriter* w0 = static_cast<VtkScalarWriter*>(new VoxelGoodVoxelScalarWriter<DataContainer>(m.get()));
       w0->m_WriteBinaryFiles = m_WriteBinaryVTKFiles;
       scalarsToWrite.push_back(w0);
     }
 
-    if (m_WriteIPFColor == true) {
-      VtkScalarWriter* w0 =
-        static_cast<VtkScalarWriter*>(new VoxelIPFColorScalarWriter<DataContainer>(m.get()));
+    if(m_WriteIPFColor == true)
+    {
+      VtkScalarWriter* w0 = static_cast<VtkScalarWriter*>(new VoxelIPFColorScalarWriter<DataContainer>(m.get()));
       w0->m_WriteBinaryFiles = m_WriteBinaryVTKFiles;
       scalarsToWrite.push_back(w0);
     }
@@ -238,7 +215,7 @@ void Reconstruction::execute()
     err = vtkWriter.write<DataContainer>(reconVisFile, m.get(), scalarsToWrite);
 
     // Now Delete all the Scalar Helpers that we just created and used.
-    for (std::vector<VtkScalarWriter*>::iterator iter = scalarsToWrite.begin(); iter != scalarsToWrite.end(); ++iter )
+    for (std::vector<VtkScalarWriter*>::iterator iter = scalarsToWrite.begin(); iter != scalarsToWrite.end(); ++iter)
     {
       delete (*iter);
     }
@@ -247,11 +224,10 @@ void Reconstruction::execute()
   }
   /* ******* End VTK Visualization File Writing Section ****** */
 
-
   CHECK_FOR_CANCELED(DataContainer, "Reconstruction was canceled", vtk_viz_files)
 
   /* ********** Optionally write the .h5grain file */
-  if (m_WriteHDF5GrainFile)
+  if(m_WriteHDF5GrainFile)
   {
     updateProgressAndMessage(("Writing Out HDF5 Grain File. This may take a few minutes to complete."), 99);
     MAKE_OUTPUT_FILE_PATH( hdf5GrainFile, DREAM3D::Reconstruction::HDF5GrainFile);
@@ -264,10 +240,8 @@ void Reconstruction::execute()
 
   // Clean up all the memory by forcibly setting a NULL pointer to the Shared
   // pointer object.
-  m = DataContainer::NullPointer();  // Clean up the memory
+  m = DataContainer::NullPointer(); // Clean up the memory
 }
-
-
 
 #define PRINT_PROPERTY( out, var)\
   out << #var << ": " << m_##var << std::endl;
@@ -278,18 +252,8 @@ void Reconstruction::execute()
 void Reconstruction::printSettings(std::ostream &ostream)
 {
   ostream << "Reconstruction Settings Being Used" << std::endl;
-    PRINT_PROPERTY(ostream, H5AngFile)
-    PRINT_PROPERTY(ostream, ZStartIndex)
-    PRINT_PROPERTY(ostream, ZEndIndex)
-    PRINT_PROPERTY(ostream, OutputDirectory)
-    PRINT_PROPERTY(ostream, MergeTwins)
-    PRINT_PROPERTY(ostream, MergeColonies)
-    PRINT_PROPERTY(ostream, MinAllowedGrainSize)
-    PRINT_PROPERTY(ostream, MisorientationTolerance)
-    PRINT_PROPERTY(ostream, AlignmentMethod)
+  PRINT_PROPERTY(ostream, H5AngFile)PRINT_PROPERTY(ostream, ZStartIndex)PRINT_PROPERTY(ostream, ZEndIndex)PRINT_PROPERTY(ostream, OutputDirectory)PRINT_PROPERTY(ostream, MergeTwins)PRINT_PROPERTY(ostream, MergeColonies)PRINT_PROPERTY(ostream, MinAllowedGrainSize)PRINT_PROPERTY(ostream, MisorientationTolerance)PRINT_PROPERTY(ostream, AlignmentMethod)
 
-    PRINT_PROPERTY(ostream, WriteVtkFile)
-    PRINT_PROPERTY(ostream, WritePhaseId)
-    PRINT_PROPERTY(ostream, WriteIPFColor)
-    PRINT_PROPERTY(ostream, WriteHDF5GrainFile)
+  PRINT_PROPERTY(ostream, WriteVtkFile)PRINT_PROPERTY(ostream, WritePhaseId)PRINT_PROPERTY(ostream, WriteIPFColor)
+  PRINT_PROPERTY(ostream, WriteHDF5GrainFile)
 }
