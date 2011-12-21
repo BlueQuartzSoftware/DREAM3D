69ce75069484332ed4211b8a9c0bfa92d5cb5cb5
Mike Jackson <mike.jackson@bluequartz.net>
2011-12-21 11:26:00 -0500

    Fixing missing return statements

--- Source/DataContainerTest/NeighborList.h
+++ Source/DataContainerTest/NeighborList.h
@@ -1,9 +1,38 @@
-/*
- * NeighborList.h
+/* ============================================================================
+ * Copyright (c) 2011 Michael A. Jackson (BlueQuartz Software)
+ * Copyright (c) 2011 Dr. Michael A. Groeber (US Air Force Research Laboratories)
+ * All rights reserved.
  *
- *  Created on: Dec 20, 2011
- *      Author: mjackson
- */
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of Michael A. Groeber, Michael A. Jackson, the US Air Force,
+ * BlueQuartz Software nor the names of its contributors may be used to endorse
+ * or promote products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  This code was written under United States Air Force Contract number
+ *                           FA8650-07-D-5800
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 #ifndef NEIGHBORLIST_H_
 #define NEIGHBORLIST_H_
@@ -31,16 +60,18 @@ class NeighborList : public IDataArray
 
     virtual ~NeighborList() {}
 
+    /* ******* IDataArray Interface. These Must be implemented ***** */
+
     virtual void SetName(const std::string &name) { m_Name = name;}
     virtual std::string GetName() { return m_Name;}
-    /* These methods are empty as there is no real way to get at the data */
 
+    /* These methods are empty as there is no real way to get at the data */
     void takeOwnership() {}
     void releaseOwnership() {}
 
     void* GetVoidPointer(size_t i)
     {
-
+      return NULL;
     }
 
     size_t GetNumberOfTuples()
@@ -60,10 +91,8 @@ class NeighborList : public IDataArray
 
     int32_t Resize(size_t size)
     {
-      //TODO: Implement the resizing feature
+      return 0;
     }
-
-
 
     void addEntry(int grainId, int value)
     {
@@ -131,7 +160,7 @@ class NeighborList : public IDataArray
   private:
     std::string m_Name;
 
-    std::map<int, SharedVectorType >  _data;
+    std::map<int, SharedVectorType>  _data;
 
 
     NeighborList(const NeighborList&); // Copy Constructor Not Implemented
