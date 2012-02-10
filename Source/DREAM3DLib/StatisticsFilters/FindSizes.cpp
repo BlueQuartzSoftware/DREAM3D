137223fda017fea0234aa6b94eaa3a4db841e885
Mike Jackson <mike.jackson@bluequartz.net>
2012-02-10 15:28:42 -0500

    Adding API to AbstractFilter to check if a specific filter is either ahead of current filter or behind it in the pipeline. Also added code to keep the previous/next filters updated for each filter when a filter is added or removed in the pipeline. Updated FindNeighbors test to make sure this is working correctly

--- Source/DREAM3DLib/StatisticsFilters/FindSizes.cpp
+++ Source/DREAM3DLib/StatisticsFilters/FindSizes.cpp
@@ -78,7 +78,10 @@ void FindSizes::dataCheck(bool preflight, size_t voxels, size_t fields, size_t e
   m_StatsDataArray = StatsDataArray::SafeObjectDownCast<IDataArray*, StatsDataArray*>(m->getEnsembleData(DREAM3D::EnsembleData::Statistics).get());
   if(m_StatsDataArray == NULL)
   {
-	m_StatsDataArray->fillArrayWithNewStatsData(ensembles);
+    StatsDataArray::Pointer p = StatsDataArray::New();
+    m_StatsDataArray = p.get();
+    m_StatsDataArray->fillArrayWithNewStatsData(ensembles);
+    m->addEnsembleData(DREAM3D::EnsembleData::Statistics, p);
   }
 
   setErrorMessage(ss.str());
