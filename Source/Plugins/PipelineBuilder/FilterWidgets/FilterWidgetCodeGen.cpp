a2ca534642d96b7641a0dfdf1009353e4aa00d71
Mike Jackson <mike.jackson@bluequartz.net>
2012-03-05 15:20:55 -0500

    Streamlining the compilation of the Generated Filter Widgets by #include the moc'ed file into the generated .cpp file which will reduce the number of compiled files by one for EACH filter generated

--- Source/Plugins/PipelineBuilder/FilterWidgets/FilterWidgetCodeGen.cpp
+++ Source/Plugins/PipelineBuilder/FilterWidgets/FilterWidgetCodeGen.cpp
@@ -384,6 +384,8 @@ void createSourceFile( const std::string &group, const std::string &filter)
   }
   fprintf(f, "\n}\n");
 
+  fprintf(f, "#include \"moc_Q%sWidget.cxx\"\n", filter.c_str());
+
   fclose(f);
 
 
