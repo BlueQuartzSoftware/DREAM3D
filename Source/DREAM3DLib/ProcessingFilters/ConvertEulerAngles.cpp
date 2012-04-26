f8e2c00877993180eb8e959172e7f41e0d7b9130
Mike Jackson <mike.jackson@bluequartz.net>
2012-04-26 11:45:34 -0400

    Adding a new filter to convert Euler Angles

---
+++ Source/DREAM3DLib/ProcessingFilters/ConvertEulerAngles.cpp
@@ -0,0 +1,190 @@
+/* ============================================================================
+ * Copyright (c) 2012 Michael A. Jackson (BlueQuartz Software)
+ * Copyright (c) 2012 Dr. Michael A. Groeber (US Air Force Research Laboratories)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of Michael A. Groeber, Michael A. Jackson, the US Air Force,
+ * BlueQuartz Software nor the names of its contributors may be used to endorse
+ * or promote products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  This code was written under United States Air Force Contract number
+ *                           FA8650-07-D-5800
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+#include "ConvertEulerAngles.h"
+
+#if DREAM3D_USE_PARALLEL_ALGORITHMS
+#include <tbb/parallel_for.h>
+#include <tbb/blocked_range.h>
+#include <tbb/partitioner.h>
+#include <tbb/task_scheduler_init.h>
+#endif
+
+
+#include "DREAM3DLib/Common/DREAM3DMath.h"
+
+class ConvertEulerAnglesImpl
+{
+    float* m_CellEulerAngles;
+    float  convFactor;
+  public:
+    ConvertEulerAnglesImpl(float* data, float factor) :
+      m_CellEulerAngles(data),
+      convFactor(factor)
+    {}
+    virtual ~ConvertEulerAnglesImpl(){}
+
+    void convert(size_t start, size_t end) const
+    {
+      for (size_t i = start; i < end; i++)
+      {
+        m_CellEulerAngles[i] = m_CellEulerAngles[i] * convFactor;
+      }
+    }
+
+#if DREAM3D_USE_PARALLEL_ALGORITHMS
+    void operator()(const tbb::blocked_range<size_t> &r) const
+    {
+      convert(r.begin(), r.end());
+    }
+#endif
+
+
+};
+
+
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+ConvertEulerAngles::ConvertEulerAngles() :
+AbstractFilter(),
+m_CellEulerAnglesArrayName(DREAM3D::CellData::EulerAngles),
+m_ConversionFactor(M_PI/180.0), // We convert from Degrees to Radians by Default
+m_CellEulerAngles(NULL)
+{
+  setupFilterOptions();
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+ConvertEulerAngles::~ConvertEulerAngles()
+{
+}
+
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void ConvertEulerAngles::setupFilterOptions()
+{
+  std::vector<FilterOption::Pointer> options;
+  {
+    FilterOption::Pointer option = FilterOption::New();
+    option->setPropertyName("ConversionFactor");
+    option->setHumanLabel("Conversion Factor");
+    option->setWidgetType(FilterOption::DoubleWidget);
+    option->setValueType("float");
+    option->setCastableValueType("double");
+    options.push_back(option);
+  }
+  setFilterOptions(options);
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void ConvertEulerAngles::writeFilterOptions(AbstractFilterOptionsWriter* writer)
+{
+  writer->writeValue("ConversionFactor", getConversionFactor() );
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void ConvertEulerAngles::dataCheck(bool preflight, size_t voxels, size_t fields, size_t ensembles)
+{
+  setErrorCondition(0);
+  std::stringstream ss;
+  DataContainer* m = getDataContainer();
+
+  GET_PREREQ_DATA(m, DREAM3D, CellData, CellEulerAngles, ss, -301, float, FloatArrayType, voxels, 3);
+
+  setErrorMessage(ss.str());
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void ConvertEulerAngles::preflight()
+{
+  dataCheck(true, 1, 1, 1);
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void ConvertEulerAngles::execute()
+{
+  DataContainer* m = getDataContainer();
+  if (NULL == m)
+  {
+    setErrorCondition(-1);
+    std::stringstream ss;
+    ss << getNameOfClass() << " DataContainer was NULL";
+    setErrorMessage(ss.str());
+    return;
+  }
+  setErrorCondition(0);
+  int64_t totalPoints = m->getTotalPoints();
+  size_t numgrains = m->getNumFieldTuples();
+  size_t numensembles = m->getNumEnsembleTuples();
+  dataCheck(false, totalPoints, numgrains, numensembles);
+  if (getErrorCondition() < 0)
+  {
+    return;
+  }
+
+//  float convFactor = static_cast<float>(M_PI / 180.0);
+//  if (m_RadiansToDegrees)
+//  {
+//    convFactor = static_cast<float>(180.0 / M_PI);
+//  }
+
+  totalPoints = totalPoints * 3;
+  std::cout << "ConvertEulerAngles: " << m_ConversionFactor << std::endl;
+#if DREAM3D_USE_PARALLEL_ALGORITHMS
+//#if 0
+  tbb::parallel_for(tbb::blocked_range<size_t>(0, totalPoints),
+                    ConvertEulerAnglesImpl(m_CellEulerAngles, m_ConversionFactor), tbb::auto_partitioner());
+
+#else
+  ConvertEulerAnglesImpl serial(m_CellEulerAngles, m_ConversionFactor);
+  serial.convert(0, totalPoints);
+#endif
+
+  notify("Complete", 0, Observable::UpdateProgressMessage);
+}
