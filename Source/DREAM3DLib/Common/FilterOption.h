12c1a0f925379e2a00b4a6decfdecd3f16dce429
Mike Jackson <mike.jackson@bluequartz.net>
2012-05-04 12:02:43 -0400

    Fixing misspelling in enumeration variable

--- Source/DREAM3DLib/Common/FilterOption.h
+++ Source/DREAM3DLib/Common/FilterOption.h
@@ -29,7 +29,7 @@ class FilterOption
       OutputFileWidget,
       BooleanWidget,
       IntConstrainedWidget,
-      DoubleContrainedWidget,
+      DoubleConstrainedWidget,
       ChoiceWidget,
       CustomWidget
     };
