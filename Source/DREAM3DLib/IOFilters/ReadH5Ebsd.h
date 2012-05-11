415a3362025f9cdd6178c500634baaeb42583931
Michael Groeber <michael.groeber@wpafb.af.mil>
2012-05-11 15:22:40 -0400

    adding files

---
+++ Source/DREAM3DLib/IOFilters/ReadH5Ebsd.h
@@ -0,0 +1,163 @@
+
+/* ============================================================================
+ * Copyright (c) 2011 Michael A. Jackson (BlueQuartz Software)
+ * Copyright (c) 2011 Dr. Michael A. Groeber (US Air Force Research Laboratories)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of Michael A. Groeber, Michael A. Jackson, the US Air Force,
+ * BlueQuartz Software nor the names of its contributors may be used to endorse
+ * or promote products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  This code was written under United States Air Force Contract number
+ *                           FA8650-07-D-5800
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+#ifndef ReadH5Ebsd_H_
+#define ReadH5Ebsd_H_
+
+#include <vector>
+#include <string>
+
+#include "EbsdLib/EbsdConstants.h"
+#include "EbsdLib/TSL/AngConstants.h"
+#include "EbsdLib/HKL/CtfConstants.h"
+
+
+#include "DREAM3DLib/DREAM3DLib.h"
+#include "DREAM3DLib/Common/DREAM3DSetGetMacros.h"
+#include "DREAM3DLib/Common/AbstractFilter.h"
+#include "DREAM3DLib/Common/DataContainer.h"
+#include "DREAM3DLib/Common/QualityMetricFilter.h"
+#include "DREAM3DLib/Common/OrientationMath.h"
+
+
+/**
+ * @class ReadH5Ebsd ReadH5Ebsd.h DREAM3DLib/ReconstructionFilters/ReadH5Ebsd.h
+ * @brief
+ * @author
+ * @date Nov 19, 2011
+ * @version 1.0
+ */
+class DREAM3DLib_EXPORT ReadH5Ebsd : public AbstractFilter
+{
+  public:
+    DREAM3D_SHARED_POINTERS(ReadH5Ebsd);
+    DREAM3D_STATIC_NEW_MACRO(ReadH5Ebsd);
+    DREAM3D_TYPE_MACRO_SUPER(ReadH5Ebsd, AbstractFilter);
+
+    virtual ~ReadH5Ebsd();
+
+    //------ Created Cell Data
+    DREAM3D_INSTANCE_STRING_PROPERTY(CellEulerAnglesArrayName)
+    DREAM3D_INSTANCE_STRING_PROPERTY(CellPhasesArrayName)
+    DREAM3D_INSTANCE_STRING_PROPERTY(GoodVoxelsArrayName)
+    //------ Created Ensemble Data
+    DREAM3D_INSTANCE_STRING_PROPERTY(CrystalStructuresArrayName)
+
+    DREAM3D_INSTANCE_STRING_PROPERTY(H5EbsdFile)
+    DREAM3D_INSTANCE_PROPERTY(Ebsd::RefFrameZDir, RefFrameZDir)
+    DREAM3D_INSTANCE_PROPERTY(int, ZStartIndex);
+    DREAM3D_INSTANCE_PROPERTY(int, ZEndIndex);
+    DREAM3D_INSTANCE_PROPERTY(DataArray<unsigned int>::Pointer, PhaseTypes)
+    DREAM3D_INSTANCE_PROPERTY(std::vector<QualityMetricFilter::Pointer>, QualityMetricFilters)
+
+
+    virtual const std::string getGroupName() { return DREAM3D::FilterGroups::IOFilters; }
+    virtual const std::string getHumanLabel() { return "Read H5Ebsd File"; }
+
+	virtual void writeFilterOptions(AbstractFilterOptionsWriter* writer);
+
+	/**
+     * @brief Reimplemented from @see AbstractFilter class
+     */
+    virtual void execute();
+    virtual void preflight();
+
+  protected:
+    ReadH5Ebsd();
+
+  private:
+    int32_t* m_CellPhases;
+    bool* m_GoodVoxels;
+    float* m_CellEulerAngles;
+
+    unsigned int* m_CrystalStructures;
+
+    unsigned long long int Seed;
+
+    int tempxpoints;
+    int tempypoints;
+    int totaltemppoints;
+
+    void dataCheck(bool preflight, size_t voxels, size_t fields, size_t ensembles);
+
+    ReadH5Ebsd(const ReadH5Ebsd&); // Copy Constructor Not Implemented
+    void operator=(const ReadH5Ebsd&); // Operator '=' Not Implemented
+
+    /**
+     * @brief This method reads the values for the phase type, crystal structure
+     * and precipitate fractions from the EBSD file.
+     * @param reader The EbsdReader instance
+     * @param precipFractions Container to hold the precipitate fractions (out)
+     * @param crystalStructures Container to hold the crystal structures (out)
+     * @return Zero/Positive on Success - Negative on error.
+     */
+    template<typename EbsdReader, typename EbsdPhase>
+    int loadInfo(EbsdReader* reader)
+    {
+      reader->setFileName(m_H5EbsdFile);
+      reader->setSliceStart(m_ZStartIndex);
+      reader->setSliceEnd(m_ZEndIndex);
+
+      std::vector<typename EbsdPhase::Pointer> phases = reader->getPhases();
+      if (phases.size() == 0)
+      {
+        return -1;
+      }
+
+      DataArray<unsigned int>::Pointer crystalStructures = DataArray<unsigned int>::CreateArray(phases.size() + 1, DREAM3D::EnsembleData::CrystalStructures);
+
+      // Initialize the zero'th element to unknowns. The other elements will
+      // be filled in based on values from the data file
+      crystalStructures->SetValue(0, Ebsd::CrystalStructure::UnknownCrystalStructure);
+      if (m_PhaseTypes.get() == NULL || m_PhaseTypes->GetSize() == 0)
+      {
+        return -1;
+      }
+      m_PhaseTypes->SetValue(0, DREAM3D::PhaseType::UnknownPhaseType);
+      for(size_t i=0;i<phases.size();i++)
+      {
+        int phaseID = phases[i]->getPhaseIndex();
+        crystalStructures->SetValue(phaseID, phases[i]->determineCrystalStructure() );
+      }
+      getDataContainer()->addEnsembleData(DREAM3D::EnsembleData::CrystalStructures, crystalStructures);
+      getDataContainer()->addEnsembleData(DREAM3D::EnsembleData::PhaseTypes, m_PhaseTypes);
+      getDataContainer()->setNumEnsembleTuples(crystalStructures->GetNumberOfTuples());
+      return 0;
+    }
+};
+
+#endif /* ReadH5Ebsd_H_ */
---
+++ Source/FilterWidgets/IOFiltersWidgets/QReadH5EbsdWidget.h
@@ -0,0 +1,103 @@
+/* ============================================================================
+ * Copyright (c) 2011, Michael A. Jackson (BlueQuartz Software)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of Michael A. Jackson nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+#ifndef QReadH5Ebsd_H_
+#define QReadH5Ebsd_H_
+
+#include <QtCore/QObject>
+#include <QtGui/QFileDialog>
+
+
+#include "DREAM3DLib/Common/DREAM3DSetGetMacros.h"
+#include "DREAM3DLib/IOFilters/ReadH5Ebsd.h"
+#include "PipelineBuilder/QFilterWidget.h"
+
+#include "FilterWidgets/ui_QReadH5EbsdWidget.h"
+
+class QualityMetricTableModel;
+
+
+class QReadH5EbsdWidget : public QFilterWidget, private Ui::QReadH5EbsdWidget
+{
+  Q_OBJECT;
+  public:
+    QReadH5EbsdWidget(QWidget* parent = NULL);
+    virtual ~QReadH5EbsdWidget();
+
+    virtual AbstractFilter::Pointer getFilter();
+
+    virtual void writeOptions(QSettings &prefs);
+    virtual void readOptions(QSettings &prefs);
+
+  protected slots:
+
+    void on_addQualityMetric_clicked();
+
+    void on_removeQualityMetric_clicked();
+
+    void m_SetSliceInfo();
+
+    // Auto Hookup Slots
+    void on_m_H5EbsdBtn_clicked();
+    void on_m_H5EbsdFile_textChanged(const QString &text);
+
+
+  private slots:
+    void phaseTypeEdited(int i);
+
+  private:
+    QualityMetricTableModel*    m_QualityMetricTableModel;
+    bool                        m_phaseTypeEdited;
+    bool                        rotateslice;
+    bool                        reorderarray;
+    bool                        aligneulers;
+    QString                     m_OpenDialogLastDirectory;
+
+    void setupGui();
+    bool verifyPathExists(QString outFilePath, QLineEdit* lineEdit);
+
+    /**
+     * @brief
+     * @return
+     */
+    bool checkPhaseTypes();
+
+    /**
+     * @brief
+     * @param r
+     */
+    void setupQualityMetricFilters(ReadH5Ebsd::Pointer filter);
+
+    QReadH5EbsdWidget(const QReadH5EbsdWidget&); // Copy Constructor Not Implemented
+    void operator=(const QReadH5EbsdWidget&); // Operator '=' Not Implemented
+};
+
+
+#endif /* QReadH5Ebsd_H_ */
