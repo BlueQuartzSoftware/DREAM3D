120a922efcfbaff4a470b2a87a09ce803018de54
Mike Jackson <mike.jackson@bluequartz.net>
2012-05-14 15:41:55 -0400

    Fixing compile errors with the Sampling Plugin

--- Source/Plugins/SamplingPlugin/Code/Sampling/CropVolume.h
+++ Source/Plugins/SamplingPlugin/Code/Sampling/CropVolume.h
@@ -53,7 +53,7 @@
  * @date Nov 19, 2011
  * @version 1.0
  */
-class DREAM3DLib_EXPORT CropVolume : public AbstractFilter
+class CropVolume : public AbstractFilter
 {
   public:
     DREAM3D_SHARED_POINTERS(CropVolume);
