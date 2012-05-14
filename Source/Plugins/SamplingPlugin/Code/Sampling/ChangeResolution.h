74a22b835c89af063aca9e687644e20e993af47b
Mike Jackson <mike.jackson@bluequartz.net>
2012-05-14 15:41:55 -0400

    Fixing compile errors with the Sampling Plugin

--- Source/Plugins/SamplingPlugin/Code/Sampling/ChangeResolution.h
+++ Source/Plugins/SamplingPlugin/Code/Sampling/ChangeResolution.h
@@ -53,7 +53,7 @@
  * @date Nov 19, 2011
  * @version 1.0
  */
-class DREAM3DLib_EXPORT ChangeResolution : public AbstractFilter
+class ChangeResolution : public AbstractFilter
 {
   public:
     DREAM3D_SHARED_POINTERS(ChangeResolution);
