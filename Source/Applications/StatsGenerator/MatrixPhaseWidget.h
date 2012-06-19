907e7c9e4ac7d19c8f00c948cf4a6d5eb2123443
Mike Jackson <mike.jackson@bluequartz.net>
2012-06-19 09:20:55 -0400

    Correcting bad commit where all the new Stats Generator stuff was deleted. Correcting .ui files to have proper capitalization for the promoted SGWidget

---
+++ Source/Applications/StatsGenerator/MatrixPhaseWidget.h
@@ -0,0 +1,89 @@
+/*
+ * MatrixPhaseWidget.h
+ *
+ *  Created on: Jun 14, 2012
+ *      Author: mjackson
+ */
+
+#ifndef MatrixPhaseWidget_H_
+#define MatrixPhaseWidget_H_
+
+#include "SGWidget.h"
+#include "ui_MatrixPhaseWidget.h"
+
+#include "DREAM3DLib/Common/Constants.h"
+#include "DREAM3DLib/Common/DataContainer.h"
+#include "StatsGenerator/Presets/AbstractMicrostructurePreset.h"
+
+class QwtPlotZoomer;
+class QwtPlotPicker;
+class QwtPlotPanner;
+class QwtPlotGrid;
+class QwtPlotCurve;
+class QwtPlotMarker;
+
+/*
+ *
+ */
+class MatrixPhaseWidget : public SGWidget, private Ui::MatrixPhaseWidget
+{
+
+  Q_OBJECT ;
+
+   public:
+     MatrixPhaseWidget(QWidget *parent = 0);
+     virtual ~MatrixPhaseWidget();
+
+     void updatePlots();
+
+     void setPhaseIndex(int index);
+     int getPhaseIndex();
+
+     MXA_INSTANCE_PROPERTY(unsigned int, PhaseType);
+     MXA_INSTANCE_PROPERTY(float, PhaseFraction);
+     MXA_INSTANCE_PROPERTY(float, TotalPhaseFraction);
+     MXA_INSTANCE_PROPERTY(bool, DataHasBeenGenerated);
+
+     void extractStatsData(DataContainer::Pointer m, int index);
+
+     void setCrystalStructure(unsigned int xtal);
+     unsigned int getCrystalStructure();
+
+     QString getComboString();
+
+     int gatherStatsData(DataContainer::Pointer m);
+
+   public slots:
+     void on_m_GenerateDefaultData_clicked();
+
+   protected slots:
+
+     void dataWasEdited();
+   protected:
+
+     /**
+      * @brief Enables or Disables all the widgets in a list
+      * @param b
+      */
+     void setWidgetListEnabled(bool b);
+
+     void setupGui();
+
+     /**
+      * @brief Enables or disables the various PlotWidgetTabs
+      * @param b Enable or disable the plotwidgets
+      */
+     void setTabsPlotTabsEnabled(bool b);
+
+   private:
+     int                  m_PhaseIndex;
+     unsigned int  m_CrystalStructure;
+
+     QList<QWidget*>      m_WidgetList;
+     QwtPlotGrid*         m_grid;
+
+     MatrixPhaseWidget(const MatrixPhaseWidget&); // Copy Constructor Not Implemented
+     void operator=(const MatrixPhaseWidget&); // Operator '=' Not Implemented
+};
+
+#endif /* MatrixPhaseWidget_H_ */
