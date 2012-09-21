3bd4c301f8237aa27ed0d77974eaf40fda683bd0
Michael Groeber <michael.groeber@wpafb.af.mil>
2012-09-21 10:43:18 -0400

    adding another alignment filter

---
+++ Source/DREAM3DLib/ReconstructionFilters/AlignSectionsFeatureCentroid.cpp
@@ -0,0 +1,218 @@
+/* ============================================================================
+ * Copyright (c) 2011 Michael A. Jackson (BlueQuartz Software)
+ * Copyright (c) 2011 Dr. Michael A. Groeber (US Air Force Research Laboratories)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of Michael A. Groeber, Michael A. Jackson, the US Air Force,
+ * BlueQuartz Software nor the names of its contributors may be used to endorse
+ * or promote products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  This code was written under United States Air Force Contract number
+ *                           FA8650-07-D-5800
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+#include "AlignSectionsFeatureCentroid.h"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include "DREAM3DLib/Common/Constants.h"
+#include "DREAM3DLib/Common/DREAM3DMath.h"
+#include "DREAM3DLib/Common/OrientationMath.h"
+#include "DREAM3DLib/Common/DREAM3DRandom.h"
+#include "DREAM3DLib/Common/DataArray.hpp"
+
+#include "DREAM3DLib/OrientationOps/CubicOps.h"
+#include "DREAM3DLib/OrientationOps/HexagonalOps.h"
+#include "DREAM3DLib/OrientationOps/OrthoRhombicOps.h"
+
+#include "DREAM3DLib/GenericFilters/FindCellQuats.h"
+
+#define ERROR_TXT_OUT 1
+#define ERROR_TXT_OUT1 1
+
+using namespace std;
+
+const static float m_pi = static_cast<float>(M_PI);
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+AlignSectionsFeatureCentroid::AlignSectionsFeatureCentroid() :
+AlignSections(),
+m_GoodVoxelsArrayName(DREAM3D::CellData::GoodVoxels),
+m_GoodVoxels(NULL)
+{
+  setupFilterParameters();
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+AlignSectionsFeatureCentroid::~AlignSectionsFeatureCentroid()
+{
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void AlignSectionsFeatureCentroid::setupFilterParameters()
+{
+  // Run the superclass first.
+  //AlignSections::setupFilterParameters();
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void AlignSectionsFeatureCentroid::dataCheck(bool preflight, size_t voxels, size_t fields, size_t ensembles)
+{
+  setErrorCondition(0);
+  std::stringstream ss;
+  DataContainer* m = getDataContainer();
+
+  if(true == getWriteAlignmentShifts() && getAlignmentShiftFileName().empty() == true)
+  {
+    ss << "The Alignment Shift file name must be set before executing this filter.";
+    setErrorCondition(-1);
+  }
+
+  GET_PREREQ_DATA(m, DREAM3D, CellData, GoodVoxels, ss, -303, bool, BoolArrayType, voxels, 1)
+}
+
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void AlignSectionsFeatureCentroid::preflight()
+{
+  dataCheck(true, 1, 1, 1);
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void AlignSectionsFeatureCentroid::execute()
+{
+  setErrorCondition(0);
+  DataContainer* m = getDataContainer();
+  if(NULL == m)
+  {
+    setErrorCondition(-999);
+    notifyErrorMessage("The DataContainer Object was NULL", -999);
+    return;
+  }
+
+  int64_t totalPoints = m->getTotalPoints();
+  size_t numgrains = m->getNumFieldTuples();
+  size_t numensembles = m->getNumEnsembleTuples();
+  dataCheck(false, totalPoints, numgrains, numensembles);
+  if (getErrorCondition() < 0)
+  {
+    return;
+  }
+
+  AlignSections::execute();
+
+  // If there is an error set this to something negative and also set a message
+ notifyStatusMessage("Aligning Sections Complete");
+}
+
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void AlignSectionsFeatureCentroid::find_shifts(std::vector<int> &xshifts, std::vector<int> &yshifts)
+{
+  DataContainer* m = getDataContainer();
+  //int64_t totalPoints = m->totalPoints();
+
+  ofstream outFile;
+  if (getWriteAlignmentShifts() == true) {
+    outFile.open(getAlignmentShiftFileName().c_str());
+  }
+  size_t udims[3] = {0,0,0};
+  m->getDimensions(udims);
+#if (CMP_SIZEOF_SIZE_T == 4)
+  typedef int32_t DimType;
+#else
+  typedef int64_t DimType;
+#endif
+  DimType dims[3] = {
+    static_cast<DimType>(udims[0]),
+    static_cast<DimType>(udims[1]),
+    static_cast<DimType>(udims[2]),
+  };
+
+  int newxshift = 0;
+  int newyshift = 0;
+  int count = 0;
+  int slice = 0;
+  int64_t point;
+//  int xspot, yspot;
+  float xRes = m->getXRes();
+  float yRes = m->getYRes();
+  std::vector<float> xCentroid(dims[2],0.0);
+  std::vector<float> yCentroid(dims[2],0.0);
+
+  for (DimType iter = 0; iter < dims[2]; iter++)
+  {
+    count = 0;
+    xCentroid[iter] = 0;
+    yCentroid[iter] = 0;
+    std::stringstream ss;
+    ss << "Aligning Sections - Determining Shifts - " << ((float)iter/dims[2])*100 << " Percent Complete";
+  //  notifyStatusMessage(ss.str());
+    slice = static_cast<int>( (dims[2] - 1) - iter );
+    for (DimType l = 0; l < dims[1]; l++)
+    {
+      for (DimType n = 0; n < dims[0]; n++)
+      {
+		  point = static_cast<int>( ((slice) * dims[0] * dims[1]) + (l * dims[0]) + n );
+          if(m_GoodVoxels[point] == true)
+		  {
+			xCentroid[iter] = xCentroid[iter] + (float(n)*xRes);
+			yCentroid[iter] = yCentroid[iter] + (float(l)*yRes);
+            count++;
+		  }
+      }
+    }
+	xCentroid[iter] = xCentroid[iter]/float(count);
+	yCentroid[iter] = yCentroid[iter]/float(count);
+  }
+  for (DimType iter = 1; iter < dims[2]; iter++)
+  {
+    xshifts[iter] = xshifts[iter-1] + int((xCentroid[iter]-xCentroid[iter-1])/xRes);
+    yshifts[iter] = yshifts[iter-1] + int((yCentroid[iter]-yCentroid[iter-1])/yRes);
+    if (getWriteAlignmentShifts() == true) {
+      outFile << slice << "	" << slice+1 << "	" << newxshift << "	" << newyshift << "	" << xshifts[iter] << "	" << yshifts[iter] << endl;
+    }
+  }
+  if (getWriteAlignmentShifts() == true) {
+    outFile.close();
+  }
+}
