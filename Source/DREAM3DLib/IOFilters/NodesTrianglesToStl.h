5d47611cbba7409926d14ac74e0c773e42f8dc2d
Mike Jackson <mike.jackson@bluequartz.net>
2012-10-10 13:33:21 -0400

    Fixing issues where a warning emitted by a filter would stop the pipeline from executing. Message Table now has correct row background color based on the type of message (Warning or Error). Headers of the QfilterWidget will change colors when there is an error or warning.

--- Source/DREAM3DLib/IOFilters/NodesTrianglesToStl.h
+++ Source/DREAM3DLib/IOFilters/NodesTrianglesToStl.h
@@ -80,7 +80,7 @@ class NodesTrianglesToStl : public AbstractFilter
     * @brief This returns a string that is displayed in the GUI. It should be readable
     * and understandable by humans.
     */
-    virtual const std::string getHumanLabel() { return "Nodes & Triangles To STL"; }
+    virtual const std::string getHumanLabel() { return "Convert Nodes/Triangles Files To STL Files"; }
 
     /**
     * @brief This method will instantiate all the end user settable options/parameters
