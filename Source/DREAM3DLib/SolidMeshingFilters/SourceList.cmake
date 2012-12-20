63c2d46ca4b103c8aff674325ae03bb507d0d3bd
Michael A. Groeber <michael.groeber@wpafb.af.mil>
2012-12-20 10:53:04 -0500

    more reorganization stuff

--- Source/DREAM3DLib/SolidMeshingFilters/SourceList.cmake
+++ Source/DREAM3DLib/SolidMeshingFilters/SourceList.cmake
@@ -2,7 +2,7 @@
 #-- Your License or copyright can go here
 #--////////////////////////////////////////////////////////////////////////////
 
-set (DREAM3D_FILTER_GROUP_NAME SolidMeshFilters)
+set (DREAM3D_FILTER_GROUP_NAME SolidMeshingFilters)
 set (${DREAM3D_FILTER_GROUP_NAME}_FILTERS_HDRS "")
 
 START_FILTER_GROUP("${DREAM3D_FILTER_GROUP_NAME}")
