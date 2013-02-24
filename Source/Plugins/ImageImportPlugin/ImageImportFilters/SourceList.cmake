c8f4ea011cd5cc168609252005e111eefece2f2b
Mike Jackson <mike.jackson@bluequartz.net>
2013-02-24 11:21:32 -0500

    Updating ImageImport Plugin build system files

---
+++ Source/Plugins/ImageImportPlugin/ImageImportFilters/SourceList.cmake
@@ -0,0 +1,12 @@
+#--////////////////////////////////////////////////////////////////////////////
+#-- Your License or copyright can go here
+#--////////////////////////////////////////////////////////////////////////////
+
+set (DREAM3D_FILTER_GROUP_NAME ImageImportFilters)
+set (${DREAM3D_FILTER_GROUP_NAME}_FILTERS_HDRS "")
+
+START_FILTER_GROUP(${ImageImportPlugin_BINARY_DIR} "${DREAM3D_FILTER_GROUP_NAME}")
+
+
+ADD_DREAM3D_FILTER( "ImageImportPlugin" "ImageImportPlugin" ${DREAM3D_FILTER_GROUP_NAME} ImportImageStack ImportImageStack.html "" TRUE)
+
