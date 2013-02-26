e12984b03ae9ccc1c3e8bbbb24d4e5b7b5c1dbc8
unknown <GroebeMA@52ZHTVL3-5079MG.wrightpatterson.afmc.ds.af.mil>
2013-02-26 12:37:13 -0500

    adding copying filter

---
+++ Source/DREAM3DLib/GenericFilters/CopyFieldArrayToCellArray.cpp
@@ -0,0 +1,205 @@
+/* ============================================================================
+ * Copyright (c) 2012 Michael A. Jackson (BlueQuartz Software)
+ * Copyright (c) 2012 Dr. Michael A. Groeber (US Air Force Research Laboratories)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of Michael A. Groeber, Michael A. Jackson, the US Air Force,
+ * BlueQuartz Software nor the names of its contributors may be used to endorse
+ * or promote products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  This code was written under United States Air Force Contract number
+ *                           FA8650-07-D-5800
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+#include "CopyFieldArrayToCellArray.h"
+
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+CopyFieldArrayToCellArray::CopyFieldArrayToCellArray() :
+  AbstractFilter(),
+  m_GrainIdsArrayName(DREAM3D::CellData::GrainIds),
+  m_SelectedFieldArrayName(""),
+  m_GrainIds(NULL)
+{
+  setupFilterParameters();
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+CopyFieldArrayToCellArray::~CopyFieldArrayToCellArray()
+{
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void CopyFieldArrayToCellArray::setupFilterParameters()
+{
+  std::vector<FilterParameter::Pointer> parameters;
+  {
+    FilterParameter::Pointer option = FilterParameter::New();
+    option->setHumanLabel("Field Array Name");
+    option->setPropertyName("SelectedFieldArrayName");
+    option->setWidgetType(FilterParameter::VoxelFieldArrayNameSelectionWidget);
+    option->setValueType("string");
+    option->setUnits("");
+    parameters.push_back(option);
+  }
+
+  setFilterParameters(parameters);
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void CopyFieldArrayToCellArray::writeFilterParameters(AbstractFilterParametersWriter* writer)
+{
+  writer->writeValue("SelectedFieldArrayName", getSelectedFieldArrayName() );
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void CopyFieldArrayToCellArray::dataCheck(bool preflight, size_t voxels, size_t fields, size_t ensembles)
+{
+  setErrorCondition(0);
+  std::stringstream ss;
+  VoxelDataContainer* m = getVoxelDataContainer();
+
+  GET_PREREQ_DATA( m, DREAM3D, CellData, GrainIds, ss, -301, int32_t, Int32ArrayType, voxels, 1)
+
+  if(m_SelectedFieldArrayName.empty() == true)
+  {
+    setErrorCondition(-11000);
+    notifyErrorMessage("An array from the Voxel Data Container must be selected.", getErrorCondition());
+  }
+
+}
+
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void CopyFieldArrayToCellArray::preflight()
+{
+  /* Place code here that sanity checks input arrays and input values. Look at some
+  * of the other DREAM3DLib/Filters/.cpp files for sample codes */
+  dataCheck(true, 1, 1, 1);
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void CopyFieldArrayToCellArray::execute()
+{
+  VoxelDataContainer* m = getVoxelDataContainer();
+  if(NULL == m)
+  {
+    setErrorCondition(-999);
+    notifyErrorMessage("The DataContainer Object was NULL", -999);
+    return;
+  }
+  setErrorCondition(0);
+  int64_t voxels = m->getTotalPoints();
+  int64_t fields = m->getTotalPoints();
+  dataCheck(false, voxels, fields, m->getNumEnsembleTuples());
+  if (getErrorCondition() < 0)
+  {
+    return;
+  }
+  //int err = 0;
+  std::stringstream ss;
+
+  IDataArray::Pointer inputData = m->getFieldData(m_SelectedFieldArrayName);
+  if (NULL == inputData.get())
+  {
+    ss.str("");
+    ss << "Selected array '" << m_SelectedFieldArrayName << "' does not exist in the Voxel Data Container. Was it spelled correctly?";
+    setErrorCondition(-11001);
+    notifyErrorMessage(ss.str(), getErrorCondition());
+    return;
+  }
+
+  std::string dType = inputData->getTypeAsString();
+  IDataArray::Pointer p = IDataArray::NullPointer();
+  if (dType.compare("int8_t"))
+  {
+	  p = Int8ArrayType::CreateArray(voxels, inputData->GetNumberOfComponents(), m_SelectedFieldArrayName);
+  }
+  else if (dType.compare("uint8_t"))
+  {
+      p = UInt8ArrayType::CreateArray(voxels, inputData->GetNumberOfComponents(), m_SelectedFieldArrayName);
+  }
+  else if (dType.compare("int16_t"))
+  {
+      p = Int16ArrayType::CreateArray(voxels, inputData->GetNumberOfComponents(), m_SelectedFieldArrayName);
+  }
+  else if (dType.compare("uint16_t"))
+  {
+      p = UInt16ArrayType::CreateArray(voxels, inputData->GetNumberOfComponents(), m_SelectedFieldArrayName);
+  }
+  else if (dType.compare("int32_t"))
+  {
+      p = Int32ArrayType::CreateArray(voxels, inputData->GetNumberOfComponents(), m_SelectedFieldArrayName);
+  }
+  else if (dType.compare("uint32_t"))
+  {
+      p = UInt32ArrayType::CreateArray(voxels, inputData->GetNumberOfComponents(), m_SelectedFieldArrayName);
+  }
+  else if (dType.compare("int64_t"))
+  {
+      p = Int64ArrayType::CreateArray(voxels, inputData->GetNumberOfComponents(), m_SelectedFieldArrayName);
+  }
+  else if (dType.compare("uint64_t"))
+  {
+      p = UInt64ArrayType::CreateArray(voxels, inputData->GetNumberOfComponents(), m_SelectedFieldArrayName);
+  }
+  else if (dType.compare("float"))
+  {
+      p = FloatArrayType::CreateArray(voxels, inputData->GetNumberOfComponents(), m_SelectedFieldArrayName);
+  }
+  else if (dType.compare("double"))
+  {
+      p = DoubleArrayType::CreateArray(voxels, inputData->GetNumberOfComponents(), m_SelectedFieldArrayName);
+  }
+
+  for(size_t i=0;i<voxels;i++)
+  {
+	  
+  }
+
+  m->addCellData(p->GetName(), p);
+  addCreatedCellData(p->GetName());
+
+  notifyStatusMessage("Complete");
+}
+
+
+
+
