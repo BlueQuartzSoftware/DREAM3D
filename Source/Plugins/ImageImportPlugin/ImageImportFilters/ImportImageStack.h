e8e7d3807464ddc2da44302ca2e29218c275f71b
Mike Jackson <mike.jackson@bluequartz.net>
2013-03-08 10:44:26 -0500

    resetting ImageImportPlugin back to original settings.

--- Source/Plugins/ImageImportPlugin/ImageImportFilters/ImportImageStack.h
+++ Source/Plugins/ImageImportPlugin/ImageImportFilters/ImportImageStack.h
@@ -74,8 +74,7 @@ class ImportImageStack : public AbstractFilter
     * a different group if you want. The string returned here will be displayed
     * in the GUI for the filter
     */
-	virtual const std::string getGroupName() { return DREAM3D::FilterGroups::IOFilters; }
-	virtual const std::string getSubGroupName() { return DREAM3D::FilterSubGroups::InputFilters; }
+    virtual const std::string getGroupName() { return "ImageImport"; }
 
     /**
     * @brief This returns a string that is displayed in the GUI. It should be readable
