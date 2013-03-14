36e1d7042637b0875bffcebeca6b96da3327e6f1
Michael A. Groeber <michael.groeber@wpafb.af.mil>
2013-03-14 15:01:06 -0400

    finishing generate ensemble statistics filter

--- Source/FilterWidgets/DREAM3DLib/StatisticsFiltersWidgets/QGenerateEnsembleStatisticsWidget.cpp
+++ Source/FilterWidgets/DREAM3DLib/StatisticsFiltersWidgets/QGenerateEnsembleStatisticsWidget.cpp
@@ -85,6 +85,9 @@ AbstractFilter::Pointer QGenerateEnsembleStatisticsWidget::getFilter()
   filter->setOmega3DistributionFitType(m_Omega3DistributionWidget->getFitType());
   filter->setNeighborhoodDistribution(m_NeighborhoodDistributionWidget->isChecked());
   filter->setNeighborhoodDistributionFitType(m_NeighborhoodDistributionWidget->getFitType());
+  filter->setODF(m_ODFWidget->isChecked());
+  filter->setMDF(m_MDFWidget->isChecked());
+  filter->setAxisODF(m_AxisODFWidget->isChecked());
 
   return filter;
 }
@@ -186,19 +189,18 @@ void QGenerateEnsembleStatisticsWidget::setupGui()
 
 
 
-  m_SizeDistributionWidget = new DistributionTypeWidget( QString::fromAscii("Size Distribution"), distributionTypeFrame);
-  m_SizeDistributionWidget->setStyleSheet("");
-  distributionTypeLayout->addWidget(m_SizeDistributionWidget);
-  m_AspectRatioDistributionWidget = new DistributionTypeWidget( QString::fromAscii("Aspect Ratio Distribution"), distributionTypeFrame);
-  m_AspectRatioDistributionWidget->setStyleSheet("");
-  distributionTypeLayout->addWidget(m_AspectRatioDistributionWidget);
-  m_Omega3DistributionWidget = new DistributionTypeWidget( QString::fromAscii("Omega3 Distribution"), distributionTypeFrame);
-  m_Omega3DistributionWidget->setStyleSheet("");
-  distributionTypeLayout->addWidget(m_Omega3DistributionWidget);
   m_NeighborhoodDistributionWidget = new DistributionTypeWidget( QString::fromAscii("Neighborhood Distribution"), distributionTypeFrame);
   m_NeighborhoodDistributionWidget->setStyleSheet("");
-  distributionTypeLayout->addWidget(m_NeighborhoodDistributionWidget);
-
+  distributionTypeLayout->insertWidget(0, m_NeighborhoodDistributionWidget);
+  m_Omega3DistributionWidget = new DistributionTypeWidget( QString::fromAscii("Omega3 Distribution"), distributionTypeFrame);
+  m_Omega3DistributionWidget->setStyleSheet("");
+  distributionTypeLayout->insertWidget(0, m_Omega3DistributionWidget);
+  m_AspectRatioDistributionWidget = new DistributionTypeWidget( QString::fromAscii("Aspect Ratio Distribution"), distributionTypeFrame);
+  m_AspectRatioDistributionWidget->setStyleSheet("");
+  distributionTypeLayout->insertWidget(0, m_AspectRatioDistributionWidget);
+  m_SizeDistributionWidget = new DistributionTypeWidget( QString::fromAscii("Size Distribution"), distributionTypeFrame);
+  m_SizeDistributionWidget->setStyleSheet("");
+  distributionTypeLayout->insertWidget(0, m_SizeDistributionWidget);
 
 }
 
