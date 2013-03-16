5322952392dc461f015c290135f26101d3789015
Mike Jackson <mike.jackson@bluequartz.net>
2013-03-16 13:50:23 -0400

    Fixing spurious error reported from the HDF5 library when certain code paths are used to close the HDF5 file. Questions posted to the HDF5 forum for help in resolving this issue.

--- Source/DREAM3DLib/IOFilters/VoxelDataContainerReader.cpp
+++ Source/DREAM3DLib/IOFilters/VoxelDataContainerReader.cpp
@@ -176,7 +176,7 @@ int VoxelDataContainerReader::getSizeResolutionOrigin(hid_t fileId, int64_t volD
   hid_t dcGid = H5Gopen(fileId, DREAM3D::HDF5::VoxelDataContainerName.c_str(), 0);
   if (dcGid < 0) // Check to see if this was a Version 3 or earlier file
   {
-    dcGid = H5Gopen(m_HdfFileId, DREAM3D::HDF5::DataContainerName.c_str(), 0);
+    dcGid = H5Gopen(fileId, DREAM3D::HDF5::DataContainerName.c_str(), 0);
   }
   if(dcGid < 0)
   {
