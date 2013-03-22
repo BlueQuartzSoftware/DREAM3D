cfd748e9e7a129033fe14e07baf85382909989ee
Michael Groeber <michael.groeber@wpafb.af.mil>
2013-03-22 14:57:16 -0400

    adding a couple new filters to rename arrays

---
+++ Source/DREAM3DLib/GenericFilters/RenameCellArray.cpp
@@ -0,0 +1,142 @@
+/* ============================================================================
+ * Copyright (c) 2012 Michael A. Jackson (BlueQuartz Software)
+ * Copyright (c) 2012 Dr. Michael A. Groeber (US Air Force Research Laboratories)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of Michael A. Groeber, Michael A. Jackson, the US Air Force,
+ * BlueQuartz Software nor the names of its contributors may be used to endorse
+ * or promote products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  This code was written under United States Air Force Contract number
+ *                           FA8650-07-D-5800
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+#include "RenameCellArray.h"
+
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+RenameCellArray::RenameCellArray() :
+  AbstractFilter(),
+  m_SelectedCellArrayName(""),
+  m_NewCellArrayName("")
+{
+  setupFilterParameters();
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+RenameCellArray::~RenameCellArray()
+{
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void RenameCellArray::setupFilterParameters()
+{
+  std::vector<FilterParameter::Pointer> parameters;
+  {
+    FilterParameter::Pointer option = FilterParameter::New();
+    option->setHumanLabel("Current Cell Array Name");
+    option->setPropertyName("SelectedCellArrayName");
+    option->setWidgetType(FilterParameter::VoxelCellArrayNameSelectionWidget);
+    option->setValueType("string");
+    option->setUnits("");
+    parameters.push_back(option);
+  }
+  {
+    FilterParameter::Pointer parameter = FilterParameter::New();
+    parameter->setHumanLabel("New Cell Array Name");
+    parameter->setPropertyName("NewCellArrayName");
+    parameter->setWidgetType(FilterParameter::StringWidget);
+    parameter->setValueType("string");
+    parameters.push_back(parameter);
+  }
+
+  setFilterParameters(parameters);
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void RenameCellArray::writeFilterParameters(AbstractFilterParametersWriter* writer)
+{
+  writer->writeValue("SelectedCellArrayName", getSelectedCellArrayName() );
+  writer->writeValue("NewCellArrayName", getNewCellArrayName() );
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void RenameCellArray::dataCheck(bool preflight, size_t voxels, size_t fields, size_t ensembles)
+{
+  setErrorCondition(0);
+  std::stringstream ss;
+
+  if(m_SelectedCellArrayName.empty() == true)
+  {
+    setErrorCondition(-11000);
+    notifyErrorMessage("An array from the Voxel Data Container must be selected.", getErrorCondition());
+  }
+}
+
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void RenameCellArray::preflight()
+{
+  /* Place code here that sanity checks input arrays and input values. Look at some
+  * of the other DREAM3DLib/Filters/.cpp files for sample codes */
+  dataCheck(true, 1, 1, 1);
+}
+
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void RenameCellArray::execute()
+{
+  VoxelDataContainer* m = getVoxelDataContainer();
+  if(NULL == m)
+  {
+    setErrorCondition(-999);
+    notifyErrorMessage("The DataContainer Object was NULL", -999);
+    return;
+  }
+  setErrorCondition(0);
+  std::stringstream ss;
+
+  bool check = m->renameCellData(m_SelectedCellArrayName, m_NewCellArrayName);
+
+  if(check == false) ss << "Array to be renamed could not be found in DataContainer";
+
+  notifyStatusMessage("Complete");
+}
+
--- Source/DREAM3DLib/SyntheticBuildingFilters/PackPrimaryPhases.cpp
+++ Source/DREAM3DLib/SyntheticBuildingFilters/PackPrimaryPhases.cpp
@@ -58,7 +58,7 @@
 #include "DREAM3DLib/ShapeOps/CylinderOps.h"
 #include "DREAM3DLib/ShapeOps/EllipsoidOps.h"
 #include "DREAM3DLib/ShapeOps/SuperEllipsoidOps.h"
-#include "DREAM3DLib/GenericFilters/FindNeighbors.h"
+#include "DREAM3DLib/StatisticsFilters/FindNeighbors.h"
 #include "DREAM3DLib/GenericFilters/RenumberGrains.h"
 #include "DREAM3DLib/IOFilters/FieldDataCSVWriter.h"
 #include "DREAM3DLib/IOFilters/DataContainerWriter.h"
@@ -194,10 +194,13 @@ class AssignVoxelsGapsImpl
                 float axis2comp = coordsRotated[1] * Invradcur[1];
                 float axis3comp = coordsRotated[2] * Invradcur[2];
                 inside = m_ShapeOps->inside(axis1comp, axis2comp, axis3comp);
-                if (inside >= 0 && newowners[index] > 0)
+//                if (inside >= 0 && newowners[index] > 0)
+                if (inside >= 0 && newowners[index] > 0 && inside > ellipfuncs[index])
                 {
-                    newowners[index] = -2;
+                    newowners[index] = curGrain;
                     ellipfuncs[index] = inside;
+//                    newowners[index] = -2;
+//                    ellipfuncs[index] = inside;
                 }
                 else if (inside >= 0 && newowners[index] == -1)
                 {
@@ -889,20 +892,20 @@ void PackPrimaryPhases::execute()
       randomgrain = firstPrimaryField + int(rg.genrand_res53() * (numgrains-firstPrimaryField));
       good = false;
       count = 0;
-      while(good == false && count < (numgrains-firstPrimaryField))
-      {
-        xc = m_Centroids[3*randomgrain];
-        yc = m_Centroids[3*randomgrain+1];
-        zc = m_Centroids[3*randomgrain+2];
-        column = static_cast<int>( (xc - (m_HalfPackingRes[0])) * m_OneOverPackingRes[0] );
-        row = static_cast<int>( (yc - (m_HalfPackingRes[1])) * m_OneOverPackingRes[1] );
-        plane = static_cast<int>( (zc - (m_HalfPackingRes[2])) * m_OneOverPackingRes[2] );
-        grainOwnersIdx = (m_PackingPoints[0]*m_PackingPoints[1]*plane) + (m_PackingPoints[0]*row) + column;
-        if(grainOwners[grainOwnersIdx] > 1) good = true;
-        else randomgrain++;
-        if(randomgrain >= numgrains) randomgrain = firstPrimaryField;
-        count++;
-      }
+      //while(good == false && count < (numgrains-firstPrimaryField))
+      //{
+      //  xc = m_Centroids[3*randomgrain];
+      //  yc = m_Centroids[3*randomgrain+1];
+      //  zc = m_Centroids[3*randomgrain+2];
+      //  column = static_cast<int>( (xc - (m_HalfPackingRes[0])) * m_OneOverPackingRes[0] );
+      //  row = static_cast<int>( (yc - (m_HalfPackingRes[1])) * m_OneOverPackingRes[1] );
+      //  plane = static_cast<int>( (zc - (m_HalfPackingRes[2])) * m_OneOverPackingRes[2] );
+      //  grainOwnersIdx = (m_PackingPoints[0]*m_PackingPoints[1]*plane) + (m_PackingPoints[0]*row) + column;
+      //  if(grainOwners[grainOwnersIdx] > 1) good = true;
+      //  else randomgrain++;
+      //  if(randomgrain >= numgrains) randomgrain = firstPrimaryField;
+      //  count++;
+      //}
       Seed++;
 
       count = 0;
@@ -961,20 +964,20 @@ void PackPrimaryPhases::execute()
       randomgrain = firstPrimaryField + int(rg.genrand_res53() * (numgrains-firstPrimaryField));
       good = false;
       count = 0;
-      while(good == false && count < (numgrains-firstPrimaryField))
-      {
-        xc = m_Centroids[3*randomgrain];
-        yc = m_Centroids[3*randomgrain+1];
-        zc = m_Centroids[3*randomgrain+2];
-        column = static_cast<int>( (xc - (m_HalfPackingRes[0])) * m_OneOverPackingRes[0] );
-        row = static_cast<int>( (yc - (m_HalfPackingRes[1])) * m_OneOverPackingRes[1] );
-        plane = static_cast<int>( (zc - (m_HalfPackingRes[2])) * m_OneOverPackingRes[2] );
-        grainOwnersIdx = (m_PackingPoints[0]*m_PackingPoints[1]*plane) + (m_PackingPoints[0]*row) + column;
-        if(grainOwners[grainOwnersIdx] > 1) good = true;
-        else randomgrain++;
-        if(randomgrain >= numgrains) randomgrain = firstPrimaryField;
-        count++;
-      }
+      //while(good == false && count < (numgrains-firstPrimaryField))
+      //{
+      //  xc = m_Centroids[3*randomgrain];
+      //  yc = m_Centroids[3*randomgrain+1];
+      //  zc = m_Centroids[3*randomgrain+2];
+      //  column = static_cast<int>( (xc - (m_HalfPackingRes[0])) * m_OneOverPackingRes[0] );
+      //  row = static_cast<int>( (yc - (m_HalfPackingRes[1])) * m_OneOverPackingRes[1] );
+      //  plane = static_cast<int>( (zc - (m_HalfPackingRes[2])) * m_OneOverPackingRes[2] );
+      //  grainOwnersIdx = (m_PackingPoints[0]*m_PackingPoints[1]*plane) + (m_PackingPoints[0]*row) + column;
+      //  if(grainOwners[grainOwnersIdx] > 1) good = true;
+      //  else randomgrain++;
+      //  if(randomgrain >= numgrains) randomgrain = firstPrimaryField;
+      //  count++;
+      //}
       Seed++;
       oldxc = m_Centroids[3 * randomgrain];
       oldyc = m_Centroids[3 * randomgrain + 1];
@@ -2088,6 +2091,14 @@ void PackPrimaryPhases::assign_gaps_only()
         m_CellPhases[j] = m_FieldPhases[m_GrainIds[neighbor]];
       }
     }
+	if(counter == 1)
+	{
+		for(int i = 0; i < 1000; i++)
+		{
+				ss.str("");
+				ss << "Assign Gaps|| Cycle#: " << counter << " || Remaining Unassigned Voxel Count: " << count;
+		}
+	}
   }
 }
 // -----------------------------------------------------------------------------
