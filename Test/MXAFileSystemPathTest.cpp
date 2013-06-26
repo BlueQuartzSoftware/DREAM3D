8f3d2739f2c9f648599e3dd41bbf94e192b2876e
Joey Kleingers <joey.kleingers@gmail.com>
2013-06-26 10:41:48 -0400

    Fixed variable declaration issue in MXAFileSystemPathTest.cpp

--- Test/MXAFileSystemPathTest.cpp
+++ Test/MXAFileSystemPathTest.cpp
@@ -163,7 +163,7 @@ int FileNameTest()
   ext = "";
   CheckFile(filepath, filename, ext);
 
-  exists = MXADir::exists(std::string(""));
+  bool exists = MXADir::exists(std::string(""));
   DREAM3D_REQUIRE_EQUAL(exists, false);
 
   return err;
