14b944b9f656495e0743b01be47c164004fde53a
Michael Jackson <mike.jackson@bluequartz.net>
2013-07-17 11:47:18 -0400

    Searching for .hpp files also

--- Resources/Windows_Convert_LineEndings.sh
+++ Resources/Windows_Convert_LineEndings.sh
@@ -20,3 +20,13 @@ do
  # mv "$x.tmp" "$x"
  dos2unix -U $x
 done
+
+files=`find . -type f -name "*.hpp"`
+
+for x in $files;
+do
+	echo "Converting $x"
+ # tr -d '\015' < "$x" > "$x.tmp"
+ # mv "$x.tmp" "$x"
+ dos2unix -U $x
+done
