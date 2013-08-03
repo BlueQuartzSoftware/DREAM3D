26a45ac72d816dae6926537ed33613f750e313f8
Michael Jackson <mike.jackson@bluequartz.net>
2013-08-02 20:44:39 -0400

    Adding another frequently used constant

--- Source/DREAM3DLib/Common/DREAM3DMath.h
+++ Source/DREAM3DLib/Common/DREAM3DMath.h
@@ -131,7 +131,7 @@ namespace DREAM3D
     static const float k_Pi = static_cast<float>(M_PI);
     static const double k_SqrtPi = sqrt(M_PI);
     static const double k_2OverSqrtPi = 2.0 / sqrt(M_PI);
-    static const double k_HalfSqrtPi = 0.5 * sqrt(M_PI);
+    static const double k_HalfOfSqrtPi = sqrt(M_PI) / 2.0;
     static const double k_2Pi = 2.0 * M_PI;
     static const double k_1OverPi = 1.0/M_PI;
     static const double k_PiOver180 = M_PI/180.0;
