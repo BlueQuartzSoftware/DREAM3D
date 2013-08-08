6d8aac2cb8a895c837e5d156b8931a3fad719f64
Michael Groeber <michael.groeber@wpafb.af.mil>
2013-08-08 08:59:54 -0400

    adding more symmetries

--- Source/DREAM3DLib/OrientationOps/TrigonalLowOps.cpp
+++ Source/DREAM3DLib/OrientationOps/TrigonalLowOps.cpp
@@ -58,13 +58,13 @@ namespace TrigonalLowMath {
       {0.0, 1.0, 0.0},
       {0.0, 0.0, 1.0}},
 
-     {{1.0, 0.0,  0.0},
-      {0.0, 0.0, -1.0},
-      {0.0, 1.0,  0.0}},
+     {{-0.5, DREAM3D::Constants::k_Root3Over2,  0.0},
+    {-DREAM3D::Constants::k_Root3Over2, -0.5, 0.0},
+      {0.0, 0.0,  1.0}},
 
-     {{1.0,  0.0,  0.0},
-      {0.0, -1.0,  0.0},
-      {0.0,  0.0, -1.0}}};
+     {{-0.5, -DREAM3D::Constants::k_Root3Over2,  0.0},
+    {DREAM3D::Constants::k_Root3Over2, -0.5, 0.0},
+      {0.0, 0.0,  1.0}}};
 
   }
 }
