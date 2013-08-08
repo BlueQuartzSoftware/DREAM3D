865d99d631049ed979096b16af1988e13e50d7d6
Michael Jackson <mike.jackson@bluequartz.net>
2013-08-08 14:52:17 -0400

    closes #110. Implemented updating a favorite pipeline with the current pipeline

--- Source/PipelineBuilder/PipelineTreeWidget.h
+++ Source/PipelineBuilder/PipelineTreeWidget.h
@@ -53,41 +53,41 @@ class QTreeWidgetItem;
 class PipelineTreeWidget : public QTreeWidget
 {
     Q_OBJECT;
- 
+
 public:
-	enum ItemType
-	{
-		Default_Item_Type = 0,
-		Favorite_Item_Type = 1,
-		Prebuilt_Item_Type = 2,
-		Favorite_Category_Item_Type = 3,
-		Prebuilt_Category_Item_Type = 4
-	};
+  enum ItemType
+  {
+    Default_Item_Type = 0,
+    Favorite_Item_Type = 1,
+    Prebuilt_Item_Type = 2,
+    Favorite_Category_Item_Type = 3,
+    Prebuilt_Category_Item_Type = 4
+  };
 
     PipelineTreeWidget(QFrame* parent = 0);
 
 
-	/**
+  /**
      * @brief Adds the actions in the actionList parameter to the right-click menu
      */
-	void addActionList(QList<QAction*> actionList);
+  void addActionList(QList<QAction*> actionList);
 
-	/**
+  /**
      * @brief Setter for the different action lists
      */
-	void setActionList(ItemType type, QList<QAction*> list);
- 
+  void setActionList(ItemType type, QList<QAction*> list);
+
 private slots:
     void onCustomContextMenuRequested(const QPoint& pos);
     void showContextMenu(QTreeWidgetItem* item, const QPoint& globalPos);
 
 private:
-	QMenu								m_Menu;
-	QList<QAction*>						m_FavoritesActions;
-	QList<QAction*>						m_PrebuiltActions;
-	QList<QAction*>						m_DefaultActions;
-	QList<QAction*>						m_FavoriteCategoryActions;
-	QList<QAction*>						m_PrebuiltCategoryActions;
+  QMenu                     m_Menu;
+  QList<QAction*>						m_FavoritesActions;
+  QList<QAction*>						m_PrebuiltActions;
+  QList<QAction*>						m_DefaultActions;
+  QList<QAction*>						m_FavoriteCategoryActions;
+  QList<QAction*>						m_PrebuiltCategoryActions;
 };
 
 #endif /* _TREEWIDGETBUILDER_H_ */
