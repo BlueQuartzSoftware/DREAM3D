b27eac56aa76584d8d9be643d7411ad0f8d9f270
Michael Jackson <mike.jackson@bluequartz.net>
2013-08-27 09:31:29 -0400

    Removing unused variables.

--- Source/DREAM3DLib/OrientationOps/MonoclinicOps.cpp
+++ Source/DREAM3DLib/OrientationOps/MonoclinicOps.cpp
@@ -514,7 +514,6 @@ void MonoclinicOps::generateIPFColor(double phi1, double phi, double phi2, doubl
   float g[3][3];
   float p[3];
   float refDirection[3];
-  float d[3];
   float eta, chi;
   float _rgb[3] = { 0.0, 0.0, 0.0 };
 
@@ -545,7 +544,6 @@ void MonoclinicOps::generateIPFColor(double phi1, double phi, double phi2, doubl
   float chiMax = 90.0;
   float etaDeg = eta*DREAM3D::Constants::k_180OverPi;
   float chiDeg = chi*DREAM3D::Constants::k_180OverPi;
-  float arg;
 
   _rgb[0] = 1.0 - chiDeg/chiMax;
   _rgb[2] = fabs(etaDeg-etaMin)/(etaMax-etaMin);
