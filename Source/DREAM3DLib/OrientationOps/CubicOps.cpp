54f3b466c08f248570419651fe9b4f950e97b005
Will Lenthe <wlenthe@engineering.ucsb.edu>
2013-09-05 13:32:18 -0700

    Optimizing and cleaning cubic misorientation coloring

--- Source/DREAM3DLib/OrientationOps/CubicOps.cpp
+++ Source/DREAM3DLib/OrientationOps/CubicOps.cpp
@@ -1561,51 +1561,7 @@ DREAM3D::Rgb CubicOps::generateMisorientationColor(const QuatF &q, const QuatF &
   n2=fabs(n2);
   n3=fabs(n3);
 
-
-  //_TripletSort(n1, n2, n3, x, y, z);
-
-  if(n1>=n2 && n1>=n3)//n1 largest
-  {
-      x=n1;
-      if(n2>=n3)
-      {
-          y=n2;
-          z=n3;
-      }
-      else
-      {
-          y=n3;
-          z=n2;
-      }
-  }
-  else if(n2>=n1 && n2>=n3)//n2 largest
-  {
-      x=n2;
-      if(n1>=n3)
-      {
-          y=n1;
-          z=n3;
-      }
-      else
-      {
-          y=n3;
-          z=n1;
-      }
-  }
-  else//n3 largest
-  {
-      x=n3;
-      if(n1>=n2)
-      {
-          y=n1;
-          z=n2;
-      }
-      else
-      {
-          y=n2;
-          z=n1;
-      }
-  }
+  _TripletSort(n1, n2, n3, z, y, x);
 
   //eq c9.1
   k=tan(w/2.0f);
@@ -1617,7 +1573,7 @@ DREAM3D::Rgb CubicOps::generateMisorientationColor(const QuatF &q, const QuatF &
   x1=x;
   y1=y;
   z1=z;
-  if(x>=(1.0f/3.0f) && atan2(z, y)>=((1.0f-2.0f*x)/x))
+  if(x>=DREAM3D::Constants::k_1Over3 && atan2(z, y)>=((1.0f-2.0f*x)/x))
   {
       y1=(x*(y+z))/(1.0f-x);
       z1=(x*z*(y+z))/(y*(1.0f-x));
@@ -1625,18 +1581,18 @@ DREAM3D::Rgb CubicOps::generateMisorientationColor(const QuatF &q, const QuatF &
 
   //eq c9.3
   x2=x1-DREAM3D::Constants::k_Tan_OneEigthPi;
-  y2=y1*cos((3.0f*M_PI)/8.0f)-z1*sin((3.0f*M_PI)/8.0f);
-  z2=y1*sin((3.0f*M_PI)/8.0f)+z1*cos((3.0f*M_PI)/8.0f);
+  y2=y1*DREAM3D::Constants::k_Cos_ThreeEightPi-z1*DREAM3D::Constants::k_Sin_ThreeEightPi;
+  z2=y1*DREAM3D::Constants::k_Sin_ThreeEightPi+z1*DREAM3D::Constants::k_Cos_ThreeEightPi;
 
   //eq c9.4
   x3=x2;
-  y3=y2*(1+(y2/z2)*tan(M_PI/8.0f));
-  z3=z2+y2*tan(M_PI/8.0f);
+  y3=y2*(1+(y2/z2)*DREAM3D::Constants::k_Tan_OneEigthPi);
+  z3=z2+y2*DREAM3D::Constants::k_Tan_OneEigthPi;
 
   //eq c9.5
   x4=x3;
-  y4=(y3*cos(M_PI/8.0f))/tan(M_PI/8.0f);
-  z4=z3-x3/cos(M_PI/8.0f);
+  y4=(y3*DREAM3D::Constants::k_Cos_OneEigthPi)/DREAM3D::Constants::k_Tan_OneEigthPi;
+  z4=z3-x3/DREAM3D::Constants::k_Cos_OneEigthPi;
 
   //eq c9.6
   k=atan2(-x4, y4);
@@ -1651,13 +1607,12 @@ DREAM3D::Rgb CubicOps::generateMisorientationColor(const QuatF &q, const QuatF &
   z6=z5;
 
   //eq c9.8 these hsv are from 0 to 1 in cartesian coordinates
-  //x7=(x6-y6*sqrt(3.0f))/(2.0f*tan(M_PI/8.0f));
-  x7=(x6*sqrt(3.0f)-y6)/(2.0f*tan(M_PI/8.0f));
-  y7=(x6+y6*sqrt(3.0f))/(2.0f*tan(M_PI/8.0f));
-  z7=z6*(cos(M_PI/8.0f)/tan(M_PI/8.0f));
+  x7=(x6*DREAM3D::Constants::k_Sqrt3-y6)/(2.0f*DREAM3D::Constants::k_Tan_OneEigthPi);
+  y7=(x6+y6*DREAM3D::Constants::k_Sqrt3)/(2.0f*DREAM3D::Constants::k_Tan_OneEigthPi);
+  z7=z6*(DREAM3D::Constants::k_Cos_OneEigthPi/DREAM3D::Constants::k_Tan_OneEigthPi);
 
   //convert to traditional hsv (0-1)
-  h=fmod(atan2f(y7, x7)+2.0f*M_PI, 2.0f*M_PI)/(2.0f*M_PI);
+  h=fmod(atan2f(y7, x7)+M_2PI, M_2PI)/M_2PI;
   s=sqrt(x7*x7+y7*y7);
   v=z7;
   if(v>0)
