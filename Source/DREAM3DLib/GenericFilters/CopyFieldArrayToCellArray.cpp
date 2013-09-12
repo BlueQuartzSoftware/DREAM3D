41c6391c595db1d944ce7d93c6adeb8490f1324b
Michael Jackson <mike.jackson@bluequartz.net>
2013-09-12 14:47:56 -0400

    Fixing Generic Filters

--- Source/DREAM3DLib/GenericFilters/CopyFieldArrayToCellArray.cpp
+++ Source/DREAM3DLib/GenericFilters/CopyFieldArrayToCellArray.cpp
@@ -105,7 +105,7 @@ int CopyFieldArrayToCellArray::writeFilterParameters(AbstractFilterParametersWri
 void CopyFieldArrayToCellArray::dataCheck(bool preflight, size_t voxels, size_t fields, size_t ensembles)
 {
   setErrorCondition(0);
-
+  QString ss;
   VolumeDataContainer* m = getVolumeDataContainer();
 
   GET_PREREQ_DATA( m, DREAM3D, CellData, GrainIds, -301, int32_t, Int32ArrayType, voxels, 1)
@@ -191,15 +191,14 @@ void CopyFieldArrayToCellArray::execute()
     return;
   }
   //int err = 0;
-
+  QString ss;
 
   IDataArray::Pointer inputData = m->getCellFieldData(m_SelectedFieldArrayName);
   if (NULL == inputData.get())
   {
-    ss.str("");
-    ss << "Selected array '" << m_SelectedFieldArrayName << "' does not exist in the Voxel Data Container. Was it spelled correctly?";
+    ss = QObject::tr("Selected array '%1' does not exist in the Voxel Data Container. Was it spelled correctly?").arg(m_SelectedFieldArrayName);
     setErrorCondition(-11001);
-    notifyErrorMessage(ss.str(), getErrorCondition());
+    notifyErrorMessage(ss, getErrorCondition());
     return;
   }
 
