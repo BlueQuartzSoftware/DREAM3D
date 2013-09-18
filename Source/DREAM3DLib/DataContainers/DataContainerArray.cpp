98ff160b4f250ac311068364172900f823967491
Michael Groeber <michael.groeber@wpafb.af.mil>
2013-09-18 13:27:07 -0400

    adding files

---
+++ Source/DREAM3DLib/DataContainers/DataContainerArray.cpp
@@ -0,0 +1,161 @@
+/* ============================================================================
+ * Copyright (c) 2011, Michael A. Jackson (BlueQuartz Software)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of Michael A. Jackson nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+#include "DataContainerArray.h"
+
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+DataContainerArray::DataContainerArray() :
+    Observer()
+{
+
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+DataContainerArray::~DataContainerArray()
+{
+}
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void DataContainerArray::pushFront(DataContainer::Pointer f)
+{
+  m_Array.push_front(f);
+}
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void DataContainerArray::popFront()
+{
+  m_Array.pop_front();
+}
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void DataContainerArray::pushBack(DataContainer::Pointer f)
+{
+  m_Array.push_back(f);
+}
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void DataContainerArray::popBack()
+{
+  m_Array.pop_back();
+}
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void DataContainerArray::insert(size_t index, DataContainer::Pointer f)
+{
+  QList<DataContainer::Pointer>::iterator it = m_Array.begin();
+  for(size_t i = 0; i < index; ++i)
+  {
+    ++it;
+  }
+  m_Array.insert(it, f);
+}
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void DataContainerArray::erase(size_t index)
+{
+  QList<DataContainer::Pointer>::iterator it = m_Array.begin();
+  for(size_t i = 0; i < index; ++i)
+  {
+    ++it;
+  }
+  m_Array.erase(it);
+}
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void DataContainerArray::clear()
+{
+  m_Array.clear();
+}
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+size_t DataContainerArray::size()
+{
+  return m_Array.size();
+}
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+bool DataContainerArray::empty()
+{
+  return m_Array.isEmpty();
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+DataContainer::Pointer DataContainerArray::removeDataContainerByName(const QString &name)
+{
+  DataContainer::Pointer f = DataContainer::NullPointer();
+  for(QList<DataContainer::Pointer>::iterator it = m_Array.begin(); it != m_Array.end(); ++it)
+  {
+    if((*it)->getName().compare(name) == 0)
+    {
+      f = *it;
+      m_Array.erase(it);
+      break;
+    }
+  }
+
+  return f;
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+QList<DataContainer::Pointer>& DataContainerArray::getDataContainerArray()
+{
+  return m_Array;
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void DataContainerArray::printDataContainerNames(QTextStream &out)
+{
+  out << "---------------------------------------------------------------------" ;
+  for (QList<DataContainer::Pointer>::iterator iter = m_Array.begin(); iter != m_Array.end(); ++iter )
+  {
+    out << (*iter)->getNameOfClass() ;
+  }
+  out << "---------------------------------------------------------------------" ;
+}
---
+++ Source/DREAM3DLib/DataContainers/DataContainerArray.h
@@ -0,0 +1,99 @@
+/* ============================================================================
+ * Copyright (c) 2011, Michael A. Jackson (BlueQuartz Software)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of Michael A. Jackson nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+#ifndef DATACONTAINERARRAY_H_
+#define DATACONTAINERARRAY_H_
+
+
+#include <QtCore/QString>
+#include <QtCore/QList>
+#include <QtCore/QTextStream>
+
+
+#include "DREAM3DLib/DREAM3DLib.h"
+#include "DREAM3DLib/Common/DREAM3DSetGetMacros.h"
+#include "DREAM3DLib/Common/Observer.h"
+#include "DREAM3DLib/DataContainers/DataContainer.h"
+
+/**
+ * @class DataContainerArray DataContainerArray.h DREAM3DLib/Common/DataContainerArray.h
+ * @brief  This class holds the list of filters that will be run. This should be
+ * the class (or a subclass) that you use to build up a pipeline in order to run
+ * it.
+ * @author Michael A. Jackson for BlueQuartz Software
+ * @date Sep 28, 2011
+ * @version 1.0
+ */
+class DREAM3DLib_EXPORT DataContainerArray : public Observer
+{
+  public:
+    DREAM3D_SHARED_POINTERS(DataContainerArray)
+    DREAM3D_TYPE_MACRO_SUPER(DataContainerArray, Observer)
+    DREAM3D_STATIC_NEW_MACRO(DataContainerArray)
+
+    virtual ~DataContainerArray();
+
+    /**
+     * @brief
+     */
+    virtual void pushFront(DataContainer::Pointer f);
+    virtual void popFront();
+    virtual void pushBack(DataContainer::Pointer f);
+    virtual void popBack();
+    virtual void insert(size_t index, DataContainer::Pointer f);
+    virtual void erase(size_t index);
+    virtual void clear();
+    virtual size_t size();
+    virtual bool empty();
+
+    //virtual FilterContainerType& getFilterContainer();
+
+    /**
+     * @brief
+     */
+    virtual DataContainer::Pointer removeDataContainerByName(const QString &name);
+    QList<DataContainer::Pointer>& getDataContainerArray();
+
+    virtual void printDataContainerNames(QTextStream &out);
+
+
+
+  protected:
+    DataContainerArray();
+
+
+  private:
+    QList<DataContainer::Pointer>  m_Array;
+
+    DataContainerArray(const DataContainerArray&); // Copy Constructor Not Implemented
+    void operator=(const DataContainerArray&); // Operator '=' Not Implemented
+};
+
+#endif /* DATACONTAINERARRAY_H_ */
