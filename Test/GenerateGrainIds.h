8337b08ac277ca602f79d5fcdf95f46943f583a4
Michael Groeber <michael.groeber@wpafb.af.mil>
2013-10-04 10:09:52 -0400

    fixing compile error

--- Test/GenerateGrainIds.h
+++ Test/GenerateGrainIds.h
@@ -105,7 +105,7 @@ class GenerateGrainIds : public AbstractFilter
  * @date
  * @version 1.0
  */
-class DREAM3DLib_EXPORT CreateVolumeDataContainer : public AbstractFilter
+class CreateVolumeDataContainer : public AbstractFilter
 {
   public:
     DREAM3D_SHARED_POINTERS(CreateVolumeDataContainer)
