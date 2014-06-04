acf6d9382fe7660ef458cbb2ea9da16c0263fc1d
Michael Groeber <michael.groeber@wpafb.af.mil>
2014-06-04 13:14:31 -0400

    adding some files to the imageProcessing plugin

---
+++ Source/Plugins/ImageProcessing/ImageProcessingFilters/GaussianBlur.cpp
@@ -0,0 +1,175 @@
+/*
+ * Your License or Copyright Information can go here
+ */
+
+#include "GaussianBlur.h"
+
+#include <QtCore/QString>
+
+#include "ITKUtilities.h"
+#include "itkGaussianBlurImageFunction.h"
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+GaussianBlur::GaussianBlur() :
+  AbstractFilter(),
+  m_SelectedCellArrayPath("", "", ""),
+  m_SaveAsNewArray(true),
+  m_SelectedCellArrayArrayName(""),
+  m_SelectedCellArray(NULL),
+  m_NewCellArrayName(""),
+  m_NewCellArray(NULL)
+  {
+  setupFilterParameters();
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+GaussianBlur::~GaussianBlur()
+{
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void GaussianBlur::setupFilterParameters()
+{
+  FilterParameterVector parameters;
+  parameters.push_back(FilterParameter::New("Array to Process", "SelectedCellArrayPath", FilterParameterWidgetType::DataArraySelectionWidget, getSelectedCellArrayPath(), false, ""));
+  QStringList linkedProps;
+  linkedProps << "NewCellArrayName";
+  parameters.push_back(FilterParameter::NewConditional("Save As New Array", "SaveAsNewArray", FilterParameterWidgetType::LinkedBooleanWidget, getSaveAsNewArray(), false, linkedProps));
+
+  parameters.push_back(FilterParameter::New("Created Array Name", "NewCellArrayName", FilterParameterWidgetType::StringWidget, getNewCellArrayName(), false, ""));
+  setFilterParameters(parameters);
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void GaussianBlur::readFilterParameters(AbstractFilterParametersReader* reader, int index)
+{
+  reader->openFilterGroup(this, index);
+  setSelectedCellArrayPath( reader->readDataArrayPath( "SelectedCellArrayPath", getSelectedCellArrayPath() ) );
+  setNewCellArrayName( reader->readString( "NewCellArrayName", getNewCellArrayName() ) );
+  setSaveAsNewArray( reader->readValue( "SaveAsNewArray", getSaveAsNewArray() ) );
+  reader->closeFilterGroup();
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+int GaussianBlur::writeFilterParameters(AbstractFilterParametersWriter* writer, int index)
+{
+  writer->openFilterGroup(this, index);
+  writer->writeValue("SelectedCellArrayPath", getSelectedCellArrayPath() );
+  writer->writeValue("NewCellArrayName", getNewCellArrayName() );
+  writer->writeValue("SaveAsNewArray", getSaveAsNewArray() );
+  writer->closeFilterGroup();
+  return ++index; // we want to return the next index that was just written to
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void GaussianBlur::dataCheck()
+{
+  setErrorCondition(0);
+  DataArrayPath tempPath;
+
+  QVector<size_t> dims(1, 1);
+  m_SelectedCellArrayPtr = getDataContainerArray()->getPrereqArrayFromPath<DataArray<ImageProcessing::DefaultPixelType>, AbstractFilter>(this, getSelectedCellArrayPath(), dims); /* Assigns the shared_ptr<> to an instance variable that is a weak_ptr<> */
+  if( NULL != m_SelectedCellArrayPtr.lock().get() ) /* Validate the Weak Pointer wraps a non-NULL pointer to a DataArray<T> object */
+  { m_SelectedCellArray = m_SelectedCellArrayPtr.lock()->getPointer(0); } /* Now assign the raw pointer to data from the DataArray<T> object */
+
+  if(m_SaveAsNewArray == false) m_NewCellArrayName = "thisIsATempName";
+  tempPath.update(getSelectedCellArrayPath().getDataContainerName(), getSelectedCellArrayPath().getAttributeMatrixName(), getNewCellArrayName() );
+  m_NewCellArrayPtr = getDataContainerArray()->createNonPrereqArrayFromPath<DataArray<ImageProcessing::DefaultPixelType>, AbstractFilter, ImageProcessing::DefaultPixelType>(this, tempPath, 0, dims); /* Assigns the shared_ptr<> to an instance variable that is a weak_ptr<> */
+  if( NULL != m_NewCellArrayPtr.lock().get() ) /* Validate the Weak Pointer wraps a non-NULL pointer to a DataArray<T> object */
+  { m_NewCellArray = m_NewCellArrayPtr.lock()->getPointer(0); } /* Now assign the raw pointer to data from the DataArray<T> object */
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void GaussianBlur::preflight()
+{
+  // These are the REQUIRED lines of CODE to make sure the filter behaves correctly
+  setInPreflight(true); // Set the fact that we are preflighting.
+  emit preflightAboutToExecute(); // Emit this signal so that other widgets can do one file update
+  emit updateFilterParameters(this); // Emit this signal to have the widgets push their values down to the filter
+  dataCheck(); // Run our DataCheck to make sure everthing is setup correctly
+  emit preflightExecuted(); // We are done preflighting this filter
+  setInPreflight(false); // Inform the system this filter is NOT in preflight mode anymore.
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void GaussianBlur::execute()
+{
+  int err = 0;
+  dataCheck();
+  if(getErrorCondition() < 0) { return; }
+
+  VolumeDataContainer* m = getDataContainerArray()->getDataContainerAs<VolumeDataContainer>(getSelectedCellArrayPath().getDataContainerName());
+  QString attrMatName = getSelectedCellArrayPath().getAttributeMatrixName();
+
+  //get filter to convert m_RawImageData to itk::image
+  ImageProcessing::ImportUInt8FilterType::Pointer importFilter=ITKUtilities::Dream3DtoITKImportFilter(m, attrMatName, m_SelectedCellArray);
+
+  //get image from filter
+  const ImageProcessing::UInt8ImageType* inputImage = inputImage=importFilter->GetOutput();
+  ImageProcessing::UInt8ImageType::RegionType filterRegion = inputImage->GetBufferedRegion();
+  ImageProcessing::UInt8ConstIteratorType it(inputImage, filterRegion);
+
+  //create guassian blur filter
+  typedef itk::GaussianBlurImageFunction< ImageProcessing::UInt8ImageType > GFunctionType;
+  GFunctionType::Pointer gaussianFunction = GFunctionType::New();
+  gaussianFunction->SetInputImage( inputImage );
+
+  //set guassian blur parameters
+  GFunctionType::ErrorArrayType setError;
+  setError.Fill( 0.01 );
+  gaussianFunction->SetMaximumError( setError );
+  gaussianFunction->SetSigma( 4 );
+  gaussianFunction->SetMaximumKernelWidth( 5 );
+
+  //loop over image running filter
+  notifyStatusMessage(getHumanLabel(), "Blurring");
+  it.GoToBegin();
+  int index=0;
+   while( !it.IsAtEnd() )
+  {
+    m_NewCellArray[index]=(gaussianFunction->EvaluateAtIndex(it.GetIndex() ));
+    ++it;
+    ++index;
+  }
+
+  //array name changing/cleanup
+  if(m_SaveAsNewArray == false)
+  {
+    AttributeMatrix::Pointer attrMat = m->getAttributeMatrix(m_SelectedCellArrayPath.getAttributeMatrixName());
+    attrMat->removeAttributeArray(m_SelectedCellArrayPath.getDataArrayName());
+    bool check = attrMat->renameAttributeArray(m_NewCellArrayName, m_SelectedCellArrayPath.getDataArrayName());
+  }
+
+  /* Let the GUI know we are done with this filter */
+  notifyStatusMessage(getHumanLabel(), "Complete");
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+AbstractFilter::Pointer GaussianBlur::newFilterInstance(bool copyFilterParameters)
+{
+  GaussianBlur::Pointer filter = GaussianBlur::New();
+  if(true == copyFilterParameters)
+  {
+    copyFilterParameterInstanceVariables(filter.get());
+  }
+  return filter;
+}
+
