e37b44dae66e587553b2362e050f9334b6fb208c
Michael Jackson <mike.jackson@bluequartz.net>
2014-06-14 16:32:17 -0400

    Changing how the human label is laid out so that is is more consistent with the other Reader filters.

--- Source/DREAM3DLib/IOFilters/SPParksTextReader.cpp
+++ Source/DREAM3DLib/IOFilters/SPParksTextReader.cpp
@@ -232,7 +232,7 @@ const QString SPParksTextReader::getGroupName()
 // -----------------------------------------------------------------------------
 const QString SPParksTextReader::getHumanLabel()
 {
-  return "SPParks Text Reader";
+  return "Read SPParks Text Files";
 }
 
 // -----------------------------------------------------------------------------
