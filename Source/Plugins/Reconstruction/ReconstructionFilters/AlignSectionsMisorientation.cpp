9df636c9ebd09fcb58e55e8e073a835863b37f86
Michael Jackson <mike.jackson@bluequartz.net>
2014-06-25 17:15:02 -0400

    Fixing optimization bug in the AlignSections to remove the old code workaround

--- Source/Plugins/Reconstruction/ReconstructionFilters/AlignSectionsMisorientation.cpp
+++ Source/Plugins/Reconstruction/ReconstructionFilters/AlignSectionsMisorientation.cpp
@@ -213,7 +213,7 @@ void AlignSectionsMisorientation::execute()
   notifyStatusMessage(getHumanLabel(), "Complete");
 }
 
-#if 0
+
 // -----------------------------------------------------------------------------
 //
 // -----------------------------------------------------------------------------
@@ -262,9 +262,9 @@ void AlignSectionsMisorientation::find_shifts(QVector<int>& xshifts, QVector<int
   int progInt = 0;
 
   // Allocate a 2D Array which will be reused from slice to slice
-  FloatArrayType::Pointer misorientsPtr = FloatArrayType::CreateArray(dims[0] * dims[1], "Misorients");
-  misorientsPtr->initializeWithValue(0.0);
-  float* misorients = misorientsPtr->getPointer(0); // Get the raw pointer to use in our calculations for speed.
+  BoolArrayType::Pointer misorientsPtr = BoolArrayType::CreateArray(dims[0] * dims[1], "Misorients");
+  misorientsPtr->initializeWithValue(false);
+  bool* misorients = misorientsPtr->getPointer(0); // Get the raw pointer to use in our calculations for speed.
 
   size_t idx = 0; // This will be used to compute the index into the flat array
   size_t xIdx = 0;
@@ -290,7 +290,7 @@ void AlignSectionsMisorientation::find_shifts(QVector<int>& xshifts, QVector<int
     newxshift = 0;
     newyshift = 0;
 
-    misorientsPtr->initializeWithValue(0.0); // Initialize everything to Zeros
+    misorientsPtr->initializeWithValue(false); // Initialize everything to Zeros
 
     while (newxshift != oldxshift || newyshift != oldyshift)
     {
@@ -305,8 +305,9 @@ void AlignSectionsMisorientation::find_shifts(QVector<int>& xshifts, QVector<int
           xIdx = k + oldxshift + halfDim0;
           yIdx = j + oldyshift + halfDim1;
           idx = (dims[0] * yIdx) + xIdx;
-          if(misorients[idx] == 0 && abs(k + oldxshift) < halfDim0
-              && (j + oldyshift) < halfDim1)
+
+          if(misorients[idx] == false && abs(k + oldxshift) < halfDim0
+              && abs(j + oldyshift) < halfDim1)
           {
             for (DimType l = 0; l < dims[1]; l = l + 4)
             {
@@ -349,7 +350,7 @@ void AlignSectionsMisorientation::find_shifts(QVector<int>& xshifts, QVector<int
             xIdx = k + oldxshift + int(halfDim0);
             yIdx = j + oldyshift + int(halfDim1);
             idx = (dims[0] * yIdx) + xIdx;
-            misorients[idx] = disorientation;
+            misorients[idx] = true;
             if(disorientation < mindisorientation || (disorientation == mindisorientation && ((abs(k + oldxshift) < abs(newxshift)) || (abs(j + oldyshift) < abs(newyshift)))))
             {
               newxshift = k + oldxshift;
@@ -372,162 +373,6 @@ void AlignSectionsMisorientation::find_shifts(QVector<int>& xshifts, QVector<int
     outFile.close();
   }
 }
-
-#else
-
-// -----------------------------------------------------------------------------
-//
-// -----------------------------------------------------------------------------
-void AlignSectionsMisorientation::find_shifts(QVector<int> &xshifts, QVector<int> &yshifts)
-{
-  VolumeDataContainer* m = getDataContainerArray()->getDataContainerAs<VolumeDataContainer>(getDataContainerName());
-
-  std::ofstream outFile;
-  if (getWriteAlignmentShifts() == true)
-  {
-    outFile.open(getAlignmentShiftFileName().toLatin1().data());
-  }
-
-  size_t udims[3] = {0,0,0};
-  m->getDimensions(udims);
-#if (CMP_SIZEOF_SIZE_T == 4)
-  typedef int32_t DimType;
-#else
-  typedef int64_t DimType;
-#endif
-  DimType dims[3] = {
-    static_cast<DimType>(udims[0]),
-    static_cast<DimType>(udims[1]),
-    static_cast<DimType>(udims[2]),
-  };
-
-  float disorientation = 0;
-  float mindisorientation = 100000000;
-  int newxshift = 0;
-  int newyshift = 0;
-  int oldxshift = 0;
-  int oldyshift = 0;
-  float count = 0;
-  int slice = 0;
-  //  int xspot, yspot;
-  float w;
-  float n1, n2, n3;
-  QuatF q1;
-  QuatF q2;
-  int refposition = 0;
-  int curposition = 0;
-  QuatF* quats = reinterpret_cast<QuatF*>(m_Quats);
-
-  unsigned int phase1, phase2;
-  int progInt = 0;
-
-  std::vector<std::vector<float> >  misorients;
-  misorients.resize(dims[0]);
-  for (DimType a = 0; a < dims[0]; a++)
-  {
-    misorients[a].resize(dims[1], 0.0);
-  }
-  for (DimType iter = 1; iter < dims[2]; iter++)
-  {
-    progInt = ((float)iter / dims[2]) * 100.0f;
-    QString ss = QObject::tr("Determining Shifts - %1% Complete").arg(progInt);
-    notifyStatusMessage(getMessagePrefix(), getHumanLabel(), ss);
-    if (getCancel() == true)
-    {
-      return;
-    }
-    mindisorientation = 100000000;
-    slice = static_cast<int>( (dims[2] - 1) - iter );
-    oldxshift = -1;
-    oldyshift = -1;
-    newxshift = 0;
-    newyshift = 0;
-    for (DimType a = 0; a < dims[0]; a++)
-    {
-      for (DimType b = 0; b < dims[1]; b++)
-      {
-        misorients[a][b] = 0;
-      }
-    }
-    while (newxshift != oldxshift || newyshift != oldyshift)
-    {
-      oldxshift = newxshift;
-      oldyshift = newyshift;
-      for (int j = -3; j < 4; j++)
-      {
-        for (int k = -3; k < 4; k++)
-        {
-          disorientation = 0;
-          count = 0;
-          if(misorients[k + oldxshift + size_t(dims[0] / 2)][j + oldyshift + (size_t)(dims[1] / 2)] == 0 && abs(k + oldxshift) < (dims[0] / 2)
-             && (j + oldyshift) < (dims[1] / 2))
-          {
-            for (DimType l = 0; l < dims[1]; l = l + 4)
-            {
-              for (DimType n = 0; n < dims[0]; n = n + 4)
-              {
-                if((l + j + oldyshift) >= 0 && (l + j + oldyshift) < dims[1] && (n + k + oldxshift) >= 0 && (n + k + oldxshift) < dims[0])
-                {
-                  count++;
-                  refposition = static_cast<int>( ((slice + 1) * dims[0] * dims[1]) + (l * dims[0]) + n );
-                  curposition = static_cast<int>( (slice * dims[0] * dims[1]) + ((l + j + oldyshift) * dims[0]) + (n + k + oldxshift) );
-                  if(m_GoodVoxels[refposition] == true && m_GoodVoxels[curposition] == true)
-                  {
-                    w = 10000.0;
-                    if(m_CellPhases[refposition] > 0 && m_CellPhases[curposition] > 0)
-                    {
-                      QuaternionMathF::Copy(quats[refposition], q1);
-//                      q1[1] = m_Quats[refposition * 5 + 1];
-//                      q1[2] = m_Quats[refposition * 5 + 2];
-//                      q1[3] = m_Quats[refposition * 5 + 3];
-//                      q1[4] = m_Quats[refposition * 5 + 4];
-                      phase1 = m_CrystalStructures[m_CellPhases[refposition]];
-                      QuaternionMathF::Copy(quats[curposition], q2);
-//                      q2[1] = m_Quats[curposition * 5 + 1];
-//                      q2[2] = m_Quats[curposition * 5 + 2];
-//                      q2[3] = m_Quats[curposition * 5 + 3];
-//                      q2[4] = m_Quats[curposition * 5 + 4];
-                      phase2 = m_CrystalStructures[m_CellPhases[curposition]];
-                      if(phase1 == phase2 && phase1 < m_OrientationOps.size())
-                      {
-                        w = m_OrientationOps[phase1]->getMisoQuat(q1, q2, n1, n2, n3);
-                      }
-                    }
-                    if(w > m_MisorientationTolerance) disorientation++;
-                  }
-                  if(m_GoodVoxels[refposition] == true && m_GoodVoxels[curposition] == false) disorientation++;
-                  if(m_GoodVoxels[refposition] == false && m_GoodVoxels[curposition] == true) disorientation++;
-                }
-                else
-                {
-
-                }
-              }
-            }
-            disorientation = disorientation/count;
-            misorients[k + oldxshift + int(dims[0] / 2)][j + oldyshift + int(dims[1] / 2)] = disorientation;
-            if(disorientation < mindisorientation || (disorientation == mindisorientation && ((abs(k+oldxshift) < abs(newxshift)) || (abs(j+oldyshift) < abs(newyshift)))))
-            {
-              newxshift = k + oldxshift;
-              newyshift = j + oldyshift;
-              mindisorientation = disorientation;
-            }
-          }
-        }
-      }
-    }
-    xshifts[iter] = xshifts[iter-1] + newxshift;
-    yshifts[iter] = yshifts[iter-1] + newyshift;
-    if (getWriteAlignmentShifts() == true) {
-      outFile << slice << "	" << slice+1 << "	" << newxshift << "	" << newyshift << "	" << xshifts[iter] << "	" << yshifts[iter] << std::endl;
-    }
-  }
-  if (getWriteAlignmentShifts() == true) {
-    outFile.close();
-  }
-}
-
-#endif
 
 // -----------------------------------------------------------------------------
 //
