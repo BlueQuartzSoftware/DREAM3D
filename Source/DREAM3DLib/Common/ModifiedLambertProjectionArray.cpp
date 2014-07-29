BlueQuartzSoftware/DREAM3D

commit 3e8316402b1e73a436b6114c1de1a2dd82d97dda

Wrote method to copy + reorder data array

Source/DREAM3DLib/Common/ModifiedLambertProjectionArray.cpp
@@ -232,6 +232,27 @@ int ModifiedLambertProjectionArray::copyTuple(size_t currentPos, size_t newPos)
 // -----------------------------------------------------------------------------
 //
 // -----------------------------------------------------------------------------
+IDataArray::Pointer ModifiedLambertProjectionArray::reorderCopy(QVector<size_t> newOrderMap)
+{
+  if(newOrderMap.size()!=getNumberOfTuples())
+  {
+    return NULL;
+  }
+  ModifiedLambertProjectionArray::Pointer daCopyPtr = ModifiedLambertProjectionArray::New();
+  daCopyPtr->resize(getNumberOfTuples());
+  daCopyPtr->initializeWithZeros();
+  ModifiedLambertProjectionArray& daCopy = *daCopyPtr;
+  for(size_t i = 0; i < getNumberOfTuples(); i++)
+  {
+    daCopy[newOrderMap[i]] = m_ModifiedLambertProjectionArray[i];
+  }
+
+  return daCopyPtr;
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
 void ModifiedLambertProjectionArray::initializeTuple(size_t i, double p)
 {
   BOOST_ASSERT(false);

Source/DREAM3DLib/DataArrays/DataArray.hpp
@@ -700,6 +700,27 @@ class DataArray : public IDataArray
       return 0;
     }
 
+    virtual IDataArray::Pointer reorderCopy(QVector<size_t> newOrderMap)
+    {
+      if(newOrderMap.size()!=getNumberOfTuples())
+      {
+        return NULL;
+      }
+      IDataArray::Pointer daCopy = createNewArray(getNumberOfTuples(), getComponentDimensions(), getName(), m_IsAllocated);
+      daCopy->initializeWithZeros();
+      if(m_IsAllocated == true)
+      {
+        size_t chunkSize = getNumberOfComponents()*sizeof(T);
+        for(int i=0; i<getNumberOfTuples(); i++)
+        {
+          T* src = getPointer(i*getNumberOfComponents());
+          void* dest = daCopy->getVoidPointer(newOrderMap[i]*getNumberOfComponents());
+          ::memcpy(dest, src, chunkSize);
+        }
+      }
+      return daCopy;
+    }
+
     /**
      * @brief Returns the number of bytes that make up the data type.
      * 1 = char

Source/DREAM3DLib/DataArrays/ManagedArrayOfArrays.hpp
@@ -392,6 +392,29 @@ class ManagedArrayOfArrays : public IDataArray
       return 0;
     }
 
+    virtual IDataArray::Pointer reorderCopy(QVector<size_t> newOrderMap)
+    {
+      // This is NOT the way to do this. You are not COPYING the actual data.
+      assert(false);
+      if(newOrderMap.size()!=getNumberOfTuples())
+      {
+        return NULL;
+      }
+      IDataArray::Pointer daCopy = createNewArray(getNumberOfTuples(), getComponentDimensions(), getName(), m_IsAllocated);
+      daCopy->initializeWithZeros();
+      if(m_IsAllocated == true)
+      {
+        size_t chunkSize = getNumberOfComponents()*sizeof(T);
+        for(int i=0; i<getNumberOfTuples(); i++)
+        {
+          T* src = getPointer(i*getNumberOfComponents());
+          void* dest = daCopy->getVoidPointer(newOrderMap[i]*getNumberOfComponents());
+          ::memcpy(dest, src, chunkSize);
+        }
+      }
+      return daCopy;
+    }
+
     /**
      * @brief Returns the number of bytes that make up the data type.
      * 1 = char

Source/DREAM3DLib/DataArrays/NeighborList.hpp
@@ -397,6 +397,26 @@ class NeighborList : public IDataArray
       return 0;
     }
 
+
+    virtual IDataArray::Pointer reorderCopy(QVector<size_t> newOrderMap)
+    {
+      if(newOrderMap.size()!=getNumberOfTuples())
+      {
+        return NULL;
+      }
+
+      typename NeighborList<T>::Pointer daCopyPtr = NeighborList<T>::CreateArray(getNumberOfTuples(), "Copy of NeighborList", true);
+      daCopyPtr->initializeWithZeros();
+      for(size_t i = 0; i < getNumberOfTuples(); i++)
+      {
+        typename NeighborList<T>::SharedVectorType sharedNeiLst(new std::vector<T>);
+        sharedNeiLst = m_Array[i];
+        daCopyPtr->setList(newOrderMap[i], sharedNeiLst);
+      }
+
+      return daCopyPtr;
+    }
+
     /**
      * @brief Splats the same value c across all values in the Tuple
      * @param i The index of the Tuple

Source/DREAM3DLib/DataArrays/StatsDataArray.cpp
@@ -215,6 +215,27 @@ int StatsDataArray::copyTuple(size_t currentPos, size_t newPos)
 // -----------------------------------------------------------------------------
 //
 // -----------------------------------------------------------------------------
+IDataArray::Pointer StatsDataArray::reorderCopy(QVector<size_t> newOrderMap)
+{
+  if(newOrderMap.size()!=getNumberOfTuples())
+  {
+    return NULL;
+  }
+  StatsDataArray::Pointer daCopyPtr = StatsDataArray::New();
+  daCopyPtr->resize(getNumberOfTuples());
+  daCopyPtr->initializeWithZeros();
+  StatsDataArray& daCopy = *daCopyPtr;
+  for(size_t i = 0; i < getNumberOfTuples(); i++)
+  {
+    daCopy[newOrderMap[i]] = m_StatsDataArray[i];
+  }
+
+  return daCopyPtr;
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
 void StatsDataArray::initializeTuple(size_t i, double p)
 {
   BOOST_ASSERT(false);

Source/DREAM3DLib/DataArrays/StringDataArray.hpp
@@ -294,6 +294,21 @@ class StringDataArray : public IDataArray
       return 0;
     }
 
+    virtual IDataArray::Pointer reorderCopy(QVector<size_t> newOrderMap)
+    {
+      if(newOrderMap.size()!=getNumberOfTuples())
+      {
+        return NULL;
+      }
+      StringDataArray::Pointer daCopy = StringDataArray::CreateArray(getNumberOfTuples(), getName());
+      daCopy->initializeWithZeros();
+      for(QVector<QString>::size_type i = 0; i < m_Array.size(); ++i)
+      {
+        daCopy->setValue(newOrderMap[i], m_Array[i]);
+      }
+      return daCopy;
+    }
+
     /**
      * @brief Does Nothing
      * @param pos The index of the Tuple

Source/DREAM3DLib/DataArrays/StructArray.hpp
@@ -403,6 +403,27 @@ class StructArray : public IDataArray
       return 0;
     }
 
+    virtual IDataArray::Pointer reorderCopy(QVector<size_t> newOrderMap)
+    {      
+      if(newOrderMap.size()!=getNumberOfTuples())
+      {
+        return NULL;
+      }
+      IDataArray::Pointer daCopy = createNewArray(getNumberOfTuples(), getComponentDimensions(), getName(), m_IsAllocated);
+      daCopy->initializeWithZeros();
+      if(m_IsAllocated == true)
+      {
+        size_t chunkSize = getNumberOfComponents()*sizeof(T);
+        for(int i=0; i<getNumberOfTuples(); i++)
+        {
+          T* src = getPointer(i*getNumberOfComponents());
+          void* dest = daCopy->getVoidPointer(newOrderMap[i]*getNumberOfComponents());
+          ::memcpy(dest, src, chunkSize);
+        }
+      }
+      return daCopy;
+    }
+
     /**
      * @brief Returns the number of bytes that make up the data type.
      * 1 = char

Source/DREAM3DLib/Common/ModifiedLambertProjectionArray.h
@@ -241,6 +241,8 @@ class DREAM3DLib_EXPORT ModifiedLambertProjectionArray : public IDataArray
      */
     virtual int copyTuple(size_t currentPos, size_t newPos);
 
+    virtual IDataArray::Pointer reorderCopy(QVector<size_t> newOrderMap);
+
     /**
      * @brief Splats the same value c across all values in the Tuple
      * @param i The index of the Tuple

commit 3fcdf7bd699bbfca47d0ccd21d132d27d55db422

Fixed OS X Compile errors introduced with commit 3e83164

Source/DREAM3DLib/Common/ModifiedLambertProjectionArray.cpp
@@ -236,7 +236,7 @@ IDataArray::Pointer ModifiedLambertProjectionArray::reorderCopy(QVector<size_t> 
 {
   if(newOrderMap.size()!=getNumberOfTuples())
   {
-    return NULL;
+    return IDataArray::NullPointer();
   }
   ModifiedLambertProjectionArray::Pointer daCopyPtr = ModifiedLambertProjectionArray::New();
   daCopyPtr->resize(getNumberOfTuples());

Source/DREAM3DLib/DataArrays/DataArray.hpp
@@ -704,7 +704,7 @@ class DataArray : public IDataArray
     {
       if(newOrderMap.size()!=getNumberOfTuples())
       {
-        return NULL;
+        return IDataArray::NullPointer();
       }
       IDataArray::Pointer daCopy = createNewArray(getNumberOfTuples(), getComponentDimensions(), getName(), m_IsAllocated);
       daCopy->initializeWithZeros();

Source/DREAM3DLib/DataArrays/ManagedArrayOfArrays.hpp
@@ -398,7 +398,7 @@ class ManagedArrayOfArrays : public IDataArray
       assert(false);
       if(newOrderMap.size()!=getNumberOfTuples())
       {
-        return NULL;
+        return IDataArray::NullPointer();
       }
       IDataArray::Pointer daCopy = createNewArray(getNumberOfTuples(), getComponentDimensions(), getName(), m_IsAllocated);
       daCopy->initializeWithZeros();

Source/DREAM3DLib/DataArrays/NeighborList.hpp
@@ -397,12 +397,16 @@ class NeighborList : public IDataArray
       return 0;
     }
 
-
+    /**
+     * @brief reorderCopy
+     * @param newOrderMap
+     * @return
+     */
     virtual IDataArray::Pointer reorderCopy(QVector<size_t> newOrderMap)
     {
       if(newOrderMap.size()!=getNumberOfTuples())
       {
-        return NULL;
+        return IDataArray::NullPointer();
       }
 
       typename NeighborList<T>::Pointer daCopyPtr = NeighborList<T>::CreateArray(getNumberOfTuples(), "Copy of NeighborList", true);

Source/DREAM3DLib/DataArrays/StatsDataArray.cpp
@@ -219,7 +219,7 @@ IDataArray::Pointer StatsDataArray::reorderCopy(QVector<size_t> newOrderMap)
 {
   if(newOrderMap.size()!=getNumberOfTuples())
   {
-    return NULL;
+    return IDataArray::Pointer();
   }
   StatsDataArray::Pointer daCopyPtr = StatsDataArray::New();
   daCopyPtr->resize(getNumberOfTuples());

Source/DREAM3DLib/DataArrays/StringDataArray.hpp
@@ -294,11 +294,16 @@ class StringDataArray : public IDataArray
       return 0;
     }
 
+    /**
+     * @brief reorderCopy
+     * @param newOrderMap
+     * @return
+     */
     virtual IDataArray::Pointer reorderCopy(QVector<size_t> newOrderMap)
     {
       if(newOrderMap.size()!=getNumberOfTuples())
       {
-        return NULL;
+        return IDataArray::NullPointer();
       }
       StringDataArray::Pointer daCopy = StringDataArray::CreateArray(getNumberOfTuples(), getName());
       daCopy->initializeWithZeros();

Source/DREAM3DLib/DataArrays/StructArray.hpp
@@ -404,10 +404,10 @@ class StructArray : public IDataArray
     }
 
     virtual IDataArray::Pointer reorderCopy(QVector<size_t> newOrderMap)
-    {      
+    {
       if(newOrderMap.size()!=getNumberOfTuples())
       {
-        return NULL;
+        return IDataArray::NullPointer();
       }
       IDataArray::Pointer daCopy = createNewArray(getNumberOfTuples(), getComponentDimensions(), getName(), m_IsAllocated);
       daCopy->initializeWithZeros();

Source/Plugins/Statistics/StatisticsFilters/FindNeighbors.cpp
@@ -43,7 +43,8 @@
 
 #include "DREAM3DLib/Common/Constants.h"
 
-#include "DREAM3DLib/DataArrays/IDataArray.h"
+#include "StatisticsConstants.h"
+
 
 // -----------------------------------------------------------------------------
 //

Source/Plugins/Statistics/StatisticsFilters/FindNeighbors.h
@@ -48,7 +48,7 @@
 #include "DREAM3DLib/DataContainers/VolumeDataContainer.h"
 #include "DREAM3DLib/DataArrays/NeighborList.hpp"
 
-#include "Statistics/StatisticsConstants.h"
+
 /**
  * @class FindNeighbors FindNeighbors.h DREAM3DLib/GenericFilters/FindNeighbors.h
  * @brief