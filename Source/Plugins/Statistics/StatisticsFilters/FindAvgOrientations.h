215fc65cfc5e1b66c86a94f2403da3e420763e74
Michael Jackson <mike.jackson@bluequartz.net>
2014-08-28 10:46:43 -0400

    Adding a default value for Average Euler Angles. Adjusting the user inputs for the filter to use newer widgets to create a data array at a given path, remove unnecessary FilterParameters.

--- Source/Plugins/Statistics/StatisticsFilters/FindAvgOrientations.h
+++ Source/Plugins/Statistics/StatisticsFilters/FindAvgOrientations.h
@@ -68,9 +68,10 @@ class FindAvgOrientations : public AbstractFilter
     DREAM3D_TYPE_MACRO_SUPER(FindAvgOrientations, AbstractFilter)
 
     virtual ~FindAvgOrientations();
+#if 0
     DREAM3D_FILTER_PARAMETER(DataArrayPath, CellFeatureAttributeMatrixName)
     Q_PROPERTY(DataArrayPath CellFeatureAttributeMatrixName READ getCellFeatureAttributeMatrixName WRITE setCellFeatureAttributeMatrixName)
-
+#endif
     DREAM3D_FILTER_PARAMETER(DataArrayPath, FeatureIdsArrayPath)
     Q_PROPERTY(DataArrayPath FeatureIdsArrayPath READ getFeatureIdsArrayPath WRITE setFeatureIdsArrayPath)
 
@@ -83,11 +84,20 @@ class FindAvgOrientations : public AbstractFilter
     DREAM3D_FILTER_PARAMETER(DataArrayPath, CrystalStructuresArrayPath)
     Q_PROPERTY(DataArrayPath CrystalStructuresArrayPath READ getCrystalStructuresArrayPath WRITE setCrystalStructuresArrayPath)
 
+#if 0
     DREAM3D_FILTER_PARAMETER(QString, AvgQuatsArrayName)
     Q_PROPERTY(QString AvgQuatsArrayName READ getAvgQuatsArrayName WRITE setAvgQuatsArrayName)
 
     DREAM3D_FILTER_PARAMETER(QString, FeatureEulerAnglesArrayName)
     Q_PROPERTY(QString FeatureEulerAnglesArrayName READ getFeatureEulerAnglesArrayName WRITE setFeatureEulerAnglesArrayName)
+#endif
+
+    DREAM3D_FILTER_PARAMETER(DataArrayPath, AvgQuatsArrayPath)
+    Q_PROPERTY(DataArrayPath AvgQuatsArrayPath READ getAvgQuatsArrayPath WRITE setAvgQuatsArrayPath)
+
+    DREAM3D_FILTER_PARAMETER(DataArrayPath, AvgEulerAnglesArrayPath)
+    Q_PROPERTY(DataArrayPath AvgEulerAnglesArrayPath READ getAvgEulerAnglesArrayPath WRITE setAvgEulerAnglesArrayPath)
+
 
     virtual const QString getCompiledLibraryName();
     virtual AbstractFilter::Pointer newFilterInstance(bool copyFilterParameters);
