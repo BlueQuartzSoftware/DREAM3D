b3952f6788010598c8af87265a09555a4ef45666
Joey Kleingers <joey.kleingers@bluequartz.net>
2014-11-18 11:13:17 -0500

    Finished MoveData test.  Moving on to the other 3 tests.

--- Test/MoveDataTest.cpp
+++ Test/MoveDataTest.cpp
@@ -15,12 +15,12 @@
 
 
 /*
-"It's a known, long-standing bug in the compiler system's headers.  For
-some reason the manufacturer, in its infinite wisdom, chose to #define
-macros min() and max() in violation of the upper-case convention and so
-break any legitimate functions with those names, including those in the
-standard C++ library."
-*/
+ "It's a known, long-standing bug in the compiler system's headers.  For
+ some reason the manufacturer, in its infinite wisdom, chose to #define
+ macros min() and max() in violation of the upper-case convention and so
+ break any legitimate functions with those names, including those in the
+ standard C++ library."
+ */
 
 #ifndef NOMINMAX
 #define NOMINMAX
@@ -66,19 +66,30 @@ DataContainerArray::Pointer createDataContainerArray()
     DataContainerArray::Pointer dca = DataContainerArray::New();
     DataContainer::Pointer dc1 = DataContainer::New("DataContainer1");
     DataContainer::Pointer dc2 = DataContainer::New("DataContainer2");
-    AttributeMatrix::Pointer am1 = AttributeMatrix::New(QVector<size_t>(1), "AttributeMatrix1", 0);
-    AttributeMatrix::Pointer am2 = AttributeMatrix::New(QVector<size_t>(1), "AttributeMatrix2", 0);
-    AttributeMatrix::Pointer am3 = AttributeMatrix::New(QVector<size_t>(1), "AttributeMatrix3", 0);
-    IDataArray::Pointer da1 = DataArray<int>::CreateArray(3, "DataArray1");
-    da1->setName("DataArray1");
+    AttributeMatrix::Pointer am1 = AttributeMatrix::New(QVector<size_t>(3,2), "AttributeMatrix1", 0);
+    AttributeMatrix::Pointer am2 = AttributeMatrix::New(QVector<size_t>(7,2), "AttributeMatrix2", 0);
+    AttributeMatrix::Pointer am3 = AttributeMatrix::New(QVector<size_t>(4,3), "AttributeMatrix3", 0);
+    AttributeMatrix::Pointer am4 = AttributeMatrix::New(QVector<size_t>(7,2), "AttributeMatrix4", 0);
+    IDataArray::Pointer da1 = DataArray<int>::CreateArray(8, "DataArray1");
+    IDataArray::Pointer da2 = DataArray<int>::CreateArray(128, "DataArray2");
+    IDataArray::Pointer da3 = DataArray<int>::CreateArray(128, "DataArray3");
+    IDataArray::Pointer da4 = DataArray<int>::CreateArray(81, "DataArray4");
+    IDataArray::Pointer da5 = DataArray<int>::CreateArray(81, "DataArray5");
+    
+    am1->addAttributeArray("DataArray1", da1);
+    am2->addAttributeArray("DataArray2", da2);
+    am2->addAttributeArray("DataArray3", da3);
+    am3->addAttributeArray("DataArray4", da4);
+    am3->addAttributeArray("DataArray5", da5);
     
     dc1->addAttributeMatrix("AttributeMatrix1", am1);
     dc1->addAttributeMatrix("AttributeMatrix2", am2);
     dc2->addAttributeMatrix("AttributeMatrix3", am3);
+    dc2->addAttributeMatrix("AttributeMatrix4", am4);
     
     dca->addDataContainer(dc1);
     dca->addDataContainer(dc2);
-
+    
     return dca;
 }
 
@@ -89,71 +100,113 @@ void MoveDataTest()
 {
     DataContainerArray::Pointer dca = createDataContainerArray();
     
-    MoveData::Pointer ptr = MoveData::New();
-    ptr->setDataContainerArray(dca);
-    ptr->setWhatToMove(k_MoveAttributeMatrix);
+    MoveData::Pointer moveDataPtr = MoveData::New();
+    moveDataPtr->setDataContainerArray(dca);
+    moveDataPtr->setWhatToMove(k_MoveAttributeMatrix);
     
     // Testing "Destination Data Container Does Not Exist" (Error Code -11011)
-    ptr->setDataContainerDestination("ThisDataContainerShouldNotExist");
+    moveDataPtr->setDataContainerDestination("ThisDataContainerShouldNotExist");
     DataArrayPath amSource("DataContainer1", "AttributeMatrix1", "");
-    ptr->setAttributeMatrixSource(amSource);
-    ptr->execute();
-    int err = ptr->getErrorCondition();
-    DREAM3D_REQUIRE_EQUAL(err, DC_DEST_NOT_FOUND)
+    moveDataPtr->setAttributeMatrixSource(amSource);
+    moveDataPtr->execute();
+    DREAM3D_REQUIRE_EQUAL(moveDataPtr->getErrorCondition(), DC_DEST_NOT_FOUND)
     
     // Testing "Source Data Container Does Not Exist" (Error Code -11012)
-    ptr->setDataContainerDestination("DataContainer2");
-    ptr->setAttributeMatrixSource(DataArrayPath("ThisDataContainerShouldNotExist", "AttributeMatrix1", ""));
-    ptr->execute();
-    int err2 = ptr->getErrorCondition();
-    DREAM3D_REQUIRE_EQUAL(err2, DC_SRC_NOT_FOUND)
+    moveDataPtr->setDataContainerDestination("DataContainer2");
+    moveDataPtr->setAttributeMatrixSource(DataArrayPath("ThisDataContainerShouldNotExist", "AttributeMatrix1", ""));
+    moveDataPtr->execute();
+    DREAM3D_REQUIRE_EQUAL(moveDataPtr->getErrorCondition(), DC_SRC_NOT_FOUND)
     
     // Testing "Source Attribute Matrix Does Not Exist" (Error Code -11013)
-    ptr->setDataContainerDestination("DataContainer2");
-    ptr->setAttributeMatrixSource(DataArrayPath("DataContainer1", "ThisAttributeMatrixShouldNotExist", ""));
-    ptr->execute();
-    int err3 = ptr->getErrorCondition();
-    DREAM3D_REQUIRE_EQUAL(err3, AM_SRC_NOT_FOUND)
+    moveDataPtr->setDataContainerDestination("DataContainer2");
+    moveDataPtr->setAttributeMatrixSource(DataArrayPath("DataContainer1", "ThisAttributeMatrixShouldNotExist", ""));
+    moveDataPtr->execute();
+    DREAM3D_REQUIRE_EQUAL(moveDataPtr->getErrorCondition(), AM_SRC_NOT_FOUND)
     
-    DataArrayPath daSrcPath("", "", "");
-    IDataArray::Pointer daSrcDataArray = dca->getExistingPrereqArrayFromPath<IDataArray,AbstractFilter>(ptr, daSrcPath);
+    // Testing "Move Attribute Matrix to a Destination Data Container" (Check that it works)
+    moveDataPtr->setAttributeMatrixSource(DataArrayPath("DataContainer1", "AttributeMatrix1", ""));
+    moveDataPtr->setDataContainerDestination("DataContainer2");
+    moveDataPtr->execute();
+    DREAM3D_REQUIRE_EQUAL(moveDataPtr->getErrorCondition(), 0)
     
-/************************************Test************************************/
+    moveDataPtr->setWhatToMove(k_MoveDataArray);
     
-//    IDataArray::Pointer daSrcDataArray = getDataContainerArray()->getExistingPrereqArrayFromPath<IDataArray, AbstractFilter>(this, daSrcPath);
-//    if (getErrorCondition() < 0)
-//    {
-//        return;
-//    }
-
-/************************************Test************************************/
+    // Testing Creation of Data Array pointer (Error Code < 0)
+    moveDataPtr->setDataArraySource(DataArrayPath("DataContainer1", "AttributeMatrix1", "ThisShouldNotExist"));
+    moveDataPtr->execute();
+    DREAM3D_REQUIRED(moveDataPtr->getErrorCondition(), <, 0)
     
-//    AttributeMatrix::Pointer daDestAttributeMatrix = getDataContainerArray()->getPrereqAttributeMatrixFromPath<DataContainer>(this, amDestPath, -11016);
-
-/************************************Test************************************/
+    moveDataPtr->setDataArraySource(DataArrayPath("DataContainer1", "ThisShouldNotExist", "DataArray1"));
+    moveDataPtr->execute();
+    DREAM3D_REQUIRED(moveDataPtr->getErrorCondition(), <, 0)
     
-//    if (daDestAttributeMatrix->getNumTuples() != daSrcDataArray->getNumberOfTuples())
-//        setErrorCondition(-11019);
-//        QString ss = QObject::tr("The number of tuples of source and destination do not match");
-
-/************************************Test************************************/
+    moveDataPtr->setDataArraySource(DataArrayPath("ThisShouldNotExist", "AttributeMatrix1", "DataArray1"));
+    moveDataPtr->execute();
+    DREAM3D_REQUIRED(moveDataPtr->getErrorCondition(), <, 0)
     
-//    else if (daSrcAttributeMatrix->getName() == daDestAttributeMatrix->getName())
-//        QString ss = QObject::tr("The source and destination Attribute Matrix are the same.  Is this what you meant to do?");
+    moveDataPtr->setDataArraySource(DataArrayPath("DataContainer1", "AttributeMatrix1", "DataArray1"));
+    
+    // Testing Creation of Attribute Matrix Pointer (Error Code < 0)
+    moveDataPtr->setAttributeMatrixDestination(DataArrayPath("DataContainer2", "ThisShouldNotExist", ""));
+    moveDataPtr->execute();
+    DREAM3D_REQUIRED(moveDataPtr->getErrorCondition(), <, 0)
+    
+    moveDataPtr->setAttributeMatrixDestination(DataArrayPath("ThisShouldNotExist", "AttributeMatrix3", ""));
+    moveDataPtr->execute();
+    DREAM3D_REQUIRED(moveDataPtr->getErrorCondition(), <, 0)
+    
+    // Testing numTuples Comparison
+    moveDataPtr->setAttributeMatrixDestination(DataArrayPath("DataContainer2", "AttributeMatrix3", ""));
+    moveDataPtr->setDataArraySource(DataArrayPath("DataContainer1", "AttributeMatrix2", "DataArray3"));
+    moveDataPtr->execute();
+    DREAM3D_REQUIRE_EQUAL(moveDataPtr->getErrorCondition(), TUPLES_NOT_MATCH)
+    
+    // Testing "Move Data Array to a Destination Attribute Matrix" (Check that it works)
+    moveDataPtr->setAttributeMatrixDestination(DataArrayPath("DataContainer2", "AttributeMatrix4", ""));
+    moveDataPtr->setDataArraySource(DataArrayPath("DataContainer1", "AttributeMatrix2", "DataArray3"));
+    moveDataPtr->execute();
+    DREAM3D_REQUIRE_EQUAL(moveDataPtr->getErrorCondition(), 0)
 }
 
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void CopyDataTest()
+{
+    
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void RenameDataTest()
+{
+    
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void RemoveDataTest()
+{
+    
+}
 
 // -----------------------------------------------------------------------------
 //  Use unit test framework
 // -----------------------------------------------------------------------------
 int main(int argc, char** argv)
 {
-  int err = EXIT_SUCCESS;
-
-  DREAM3D_REGISTER_TEST( MoveDataTest() )
-  PRINT_TEST_SUMMARY();
-
-  return err;
+    int err = EXIT_SUCCESS;
+    
+    DREAM3D_REGISTER_TEST( MoveDataTest() )
+    DREAM3D_REGISTER_TEST( CopyDataTest() )
+    DREAM3D_REGISTER_TEST( RenameDataTest() )
+    DREAM3D_REGISTER_TEST( RemoveDataTest() )
+    
+    PRINT_TEST_SUMMARY();
+    
+    return err;
 }
 
 
