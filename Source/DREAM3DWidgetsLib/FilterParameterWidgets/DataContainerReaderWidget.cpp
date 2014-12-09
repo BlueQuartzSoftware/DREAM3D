becd74ce0522ba72adde1dcd1c2636147e1691c5
Joey Kleingers <joey.kleingers@bluequartz.net>
2014-12-09 16:54:37 -0500

    Added changes to a slot to try and eliminate a bug that crashes the program.

--- Source/DREAM3DWidgetsLib/FilterParameterWidgets/DataContainerReaderWidget.cpp
+++ Source/DREAM3DWidgetsLib/FilterParameterWidgets/DataContainerReaderWidget.cpp
@@ -682,13 +682,17 @@ void DataContainerReaderWidget::on_selectBtn_clicked()
 // -----------------------------------------------------------------------------
 void DataContainerReaderWidget::on_dcaProxyView_updatePreviewWidget(const QModelIndex &index)
 {
+    if ( NULL != dcaProxyView->previewWidget())
+    {
+        delete dcaProxyView->previewWidget();
+    }
+
     QVariant var = index.data();
     QString name = var.toString();
 
-    DataArrayInformationDisplayWidget::Pointer widget = DataArrayInformationDisplayWidget::New();
+    DataArrayInformationDisplayWidget* widget = new DataArrayInformationDisplayWidget();
     widget->setNameText(name);
 
-
     // Create widget and add to dcaProxyView
-    dcaProxyView->setPreviewWidget(widget.get());
+    dcaProxyView->setPreviewWidget(widget);
 }
