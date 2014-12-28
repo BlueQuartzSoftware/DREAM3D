e26ac873d158014130faacdf63f9ee39486b7189
Michael Jackson <mike.jackson@bluequartz.net>
2014-12-28 12:46:47 -0500

    Fix warning about using NULL in arithmetic
    The macro incorrectly uses the NULL in a logical operation that forces
    GCC to evaluate the NULL to an arithmetic value. This is a test to see
    if reversing the comparison quiets the warning.

--- Test/DataContainerTest.cpp
+++ Test/DataContainerTest.cpp
@@ -468,15 +468,15 @@ void insertDeleteArray(VolumeDataContainer::Pointer m)
 {
   // This should fail because there is no attribute matrix
   AttributeMatrix::Pointer attrMatrix = m->getAttributeMatrix(getCellAttributeMatrixName());
-  DREAM3D_TEST_POINTER(attrMatrix.get(), ==, NULL)
+  DREAM3D_TEST_POINTER(NULL, ==, attrMatrix.get())
 
-      // Now add an AttributeMatrix to the DataContainer
-      QVector<size_t> tDims(1, 0);
+  // Now add an AttributeMatrix to the DataContainer
+  QVector<size_t> tDims(1, 0);
   AttributeMatrix::Pointer attrMat = m->createAndAddAttributeMatrix(tDims, getCellAttributeMatrixName(), DREAM3D::AttributeMatrixType::Cell);
-  DREAM3D_TEST_POINTER(attrMat.get(), !=, NULL)
+  DREAM3D_TEST_POINTER(NULL, !=, attrMat.get())
 
-      // Now create an Array and add it to the Attribute Matrix
-      typename T::Pointer p = T::CreateArray(5, "Test");
+  // Now create an Array and add it to the Attribute Matrix
+  typename T::Pointer p = T::CreateArray(5, "Test");
 
 
   tDims[0] = 5;
@@ -486,30 +486,30 @@ void insertDeleteArray(VolumeDataContainer::Pointer m)
 
   // Now get it back out as the specific type that we put it in as
   typename T::Pointer t = attrMat->getAttributeArrayAs<T>("Test");
-  DREAM3D_TEST_POINTER(t.get(), !=, NULL)
+  DREAM3D_TEST_POINTER(NULL, !=, t.get())
 
   // get the array as an IDataArray
   IDataArray::Pointer ida = attrMat->getAttributeArray("Test");
-  DREAM3D_TEST_POINTER(ida.get(), !=, NULL);
+  DREAM3D_TEST_POINTER(NULL, !=, ida.get());
 
   QVector<size_t> dims(1, 1);
   t = attrMat->getPrereqArray<T, AbstractFilter>(NULL, "Test", -723, dims);
-  DREAM3D_TEST_POINTER(t.get(), !=, NULL);
+  DREAM3D_TEST_POINTER(NULL, !=, ida.get());
 
   // Remove the AttributeArray from the AttributeMatrix
   ida = attrMat->removeAttributeArray( "Test" );
-  DREAM3D_TEST_POINTER(ida.get(), !=, NULL);
+  DREAM3D_TEST_POINTER(NULL, !=, ida.get());
 
   // Now try and get the array again. we should fail
   t = attrMat->getAttributeArrayAs<T>( "Test" );
-  DREAM3D_TEST_POINTER(t.get(), ==, NULL);
+  DREAM3D_TEST_POINTER(NULL, ==, t.get());
   // Try to get it as an IDataArray. We should fail
   ida = attrMat->getAttributeArray("Test");
-  DREAM3D_TEST_POINTER(ida.get(), ==, NULL);
+  DREAM3D_TEST_POINTER(NULL, ==, ida.get());
 
 
   t = attrMat->getPrereqArray<T, AbstractFilter>(NULL, "Test", -723, dims);
-  DREAM3D_TEST_POINTER(t.get(), ==, NULL);
+  DREAM3D_TEST_POINTER(NULL, ==, t.get());
 
 
   // Remove the AttributeMatrix to setup for the next test.
