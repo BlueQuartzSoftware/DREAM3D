22b0b5b286a5a58d53e8e26cfa96bc27cf7ede4d
Sally Connell <sally.connell@bluequartz.net>
2015-02-06 14:46:16 -0500

    Changed Ensemble Reader documentation to reflect use of ini file and wrote unit test for testing the new ini file code. updates #252

---
+++ Source/Plugins/IO/Test/EnsembleInfoReaderTest.cpp
@@ -0,0 +1,354 @@
+/* ============================================================================
+ * Copyright (c) 2010, Michael A. Jackson (BlueQuartz Software)
+ * Copyright (c) 2010, Dr. Michael A. Groeber (US Air Force Research Laboratories
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of Michael A. Groeber, Michael A. Jackson, the US Air Force,
+ * BlueQuartz Software nor the names of its contributors may be used to endorse
+ * or promote products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  This code was written under United States Air Force Contract number
+ *                           FA8650-07-D-5800
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+#include <QtCore/QCoreApplication>
+#include <QtCore/QDir>
+#include <QtCore/QFile>
+#include <QtCore/QString>
+
+#include "DREAM3DLib/DREAM3DLib.h"
+#include "DREAM3DLib/Common/DREAM3DSetGetMacros.h"
+#include "DREAM3DLib/DataArrays/DataArray.hpp"
+#include "DREAM3DLib/Common/FilterPipeline.h"
+#include "DREAM3DLib/Common/FilterManager.h"
+#include "DREAM3DLib/Common/FilterFactory.hpp"
+#include "DREAM3DLib/Plugin/DREAM3DPluginInterface.h"
+#include "DREAM3DLib/Plugin/DREAM3DPluginLoader.h"
+#include "DREAM3DLib/Utilities/UnitTestSupport.hpp"
+#include "DREAM3DLib/Utilities/QMetaObjectUtilities.h"
+
+#include "TestFileLocations.h"
+#include "GenerateFeatureIds.h"
+
+#include <stdlib.h>
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void RemoveTestFiles()
+{
+#if REMOVE_TEST_FILES
+  QFile::remove(UnitTest::EnsembleInfoReaderTest::TestFileIni);
+  QFile::remove(UnitTest::EnsembleInfoReaderTest::TestFileTxt);
+  QFile::remove(UnitTest::EnsembleInfoReaderTest::TestFileDoc);
+#endif
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+int TestFilterAvailability()
+{
+  // Now instantiate the EnsembleInfoReader Filter from the FilterManager
+  QString filtName = "EnsembleInfoReader";
+  FilterManager* fm = FilterManager::Instance();
+  IFilterFactory::Pointer filterFactory = fm->getFactoryForFilter(filtName);
+  if (NULL == filterFactory.get() )
+  {
+    std::stringstream ss;
+    ss << "The EnsembleInfoReaderTest Requires the use of the " << filtName.toStdString() << " filter which is found in the IO Plugin";
+    DREAM3D_TEST_THROW_EXCEPTION(ss.str())
+  }
+  return 0;
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void WriteTestFile(const QString& filePath, const QString& groups)
+{
+  FILE* f = NULL;
+  f = fopen(filePath.toLatin1().data(), "wb");
+  if (NULL == f)
+  {
+    QString ss = QObject::tr("Error writing output file '%1'\n ").arg(filePath);
+    DREAM3D_REQUIRE_EQUAL(0, 1)
+  }
+  fprintf(f, groups.toLatin1().data());
+  fclose(f);
+}
+
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+int TestEnsembleInfoWriter()
+{
+QString groups = "[EnsembleInfo]\nNumber_Phases = 2\n\n\
+[1]\nCrystalStructure = Cubic\nPhaseType = PrimaryPhase\n\n\
+[2]\nCrystalStructure = Hexagonal\nPhaseType = MatrixPhase\n"; // the correct groups and keys for writing the .ini file
+
+  // make .doc, .txt, .ini files
+  WriteTestFile(UnitTest::EnsembleInfoReaderTest::TestFileDoc, groups);
+  WriteTestFile(UnitTest::EnsembleInfoReaderTest::TestFileTxt, groups);
+  WriteTestFile(UnitTest::EnsembleInfoReaderTest::TestFileIni, groups);
+
+	DataContainerArray::Pointer dca = DataContainerArray::New();
+	VolumeDataContainer::Pointer vdc = VolumeDataContainer::New(DREAM3D::Defaults::VolumeDataContainerName);
+
+	// Now instantiate the EnsembleInfoReader Filter from the FilterManager
+  QString filtName = "EnsembleInfoReader";
+  FilterManager* fm = FilterManager::Instance();
+  IFilterFactory::Pointer filterFactory = fm->getFactoryForFilter(filtName);
+  if (NULL != filterFactory.get())
+  {
+    // If we get this far, the Factory is good so creating the filter should not fail unless something has
+    // horribly gone wrong in which case the system is going to come down quickly after this.
+    AbstractFilter::Pointer filter = filterFactory->create();
+
+    QVariant var;
+		int err = 0;
+    bool propWasSet;
+
+		dca->addDataContainer(vdc);
+		filter->setDataContainerArray(dca);
+    var.setValue(UnitTest::EnsembleInfoReaderTest::TestFileDoc); // should return -10018, .doc extension
+    propWasSet = filter->setProperty("InputFile", var);
+    DREAM3D_REQUIRE_EQUAL(propWasSet, true)
+    filter->preflight();
+		err = filter->getErrorCondition();
+		DREAM3D_REQUIRE_EQUAL(err, -10018);
+
+
+		// Reset The data container array with new instances for this sub test
+		dca = DataContainerArray::New();
+		vdc = VolumeDataContainer::New(DREAM3D::Defaults::VolumeDataContainerName);
+		dca->addDataContainer(vdc);
+		filter->setDataContainerArray(dca);
+
+    var.setValue(UnitTest::EnsembleInfoReaderTest::TestFileTxt); // should pass, .txt extension
+    propWasSet = filter->setProperty("InputFile", var);
+    DREAM3D_REQUIRE_EQUAL(propWasSet, true)
+    filter->preflight();
+    DREAM3D_REQUIRED(filter->getErrorCondition(), >= , 0);
+
+    // Reset The data container array with new instances for this sub test
+		dca = DataContainerArray::New();
+		vdc = VolumeDataContainer::New(DREAM3D::Defaults::VolumeDataContainerName);
+		dca->addDataContainer(vdc);
+		filter->setDataContainerArray(dca);
+
+    var.setValue(UnitTest::EnsembleInfoReaderTest::TestFileIni); // should pass, .ini extension
+    propWasSet = filter->setProperty("InputFile", var);
+    DREAM3D_REQUIRE_EQUAL(propWasSet, true)
+    filter->preflight();
+    err = filter->getErrorCondition();
+    DREAM3D_REQUIRE_EQUAL(err, 0);
+  }
+  else
+  {
+    QString ss = QObject::tr("EnsembleInfoReaderTest Error creating filter '%1'. Filter was not created/executed. Please notify the developers.").arg(filtName);
+    DREAM3D_REQUIRE_EQUAL(0, 1)
+  }
+  return 1;
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+int TestEnsembleInfoReader()
+{
+	DataContainerArray::Pointer dca = DataContainerArray::New();
+	VolumeDataContainer::Pointer vdc = VolumeDataContainer::New(DREAM3D::Defaults::VolumeDataContainerName);
+
+  // Now instantiate the EnsembleInfoReader Filter from the FilterManager
+  QString filtName = "EnsembleInfoReader";
+  FilterManager* fm = FilterManager::Instance();
+  IFilterFactory::Pointer filterFactory = fm->getFactoryForFilter(filtName);
+  if (NULL != filterFactory.get())
+	{
+    // If we get this far, the Factory is good so creating the filter should not fail unless something has
+    // horribly gone wrong in which case the system is going to come down quickly after this.
+    AbstractFilter::Pointer filter = filterFactory->create();
+  
+    QVariant var;
+		int err;
+		bool propWasSet;
+
+		dca->addDataContainer(vdc);
+		filter->setDataContainerArray(dca);
+    var.setValue(UnitTest::EnsembleInfoReaderTest::TestFileIni); // should pass
+    propWasSet = filter->setProperty("InputFile", var);
+    DREAM3D_REQUIRE_EQUAL(propWasSet, true)
+    filter->execute();
+    DREAM3D_REQUIRED(filter->getErrorCondition(), >= , 0);
+
+		// Reset The data container array with new instances for this sub test
+		dca = DataContainerArray::New();
+		vdc = VolumeDataContainer::New(DREAM3D::Defaults::VolumeDataContainerName);
+		dca->addDataContainer(vdc);
+		filter->setDataContainerArray(dca);
+
+    QString groups;
+    groups = "[EnsembleInf]\nNumber_Phases = 2\n\n[1]\nCrystalStructure = Cubic\nPhaseType = PrimaryPhase\n\n[2]\nCrystalStructure = Hexagonal\nPhaseType = MatrixPhase\n";
+    WriteTestFile(UnitTest::EnsembleInfoReaderTest::TestFileIni, groups);  //EnsembleInfo misspelled
+    var.setValue(UnitTest::EnsembleInfoReaderTest::TestFileIni);
+    propWasSet = filter->setProperty("InputFile", var);
+    DREAM3D_REQUIRE_EQUAL(propWasSet, true)
+    filter->execute();
+		err = filter->getErrorCondition();
+		DREAM3D_REQUIRE_EQUAL(err, -10003);
+
+		// Reset The data container array with new instances for this sub test
+		dca = DataContainerArray::New();
+		vdc = VolumeDataContainer::New(DREAM3D::Defaults::VolumeDataContainerName);
+		dca->addDataContainer(vdc);
+		filter->setDataContainerArray(dca);
+    groups = "[EnsembleInfo]\nNumber_Phases = 1\n\n[1]\nCrystalStructure = Cubic\nPhaseType = PrimaryPhase\n\n[2]\nCrystalStructure = Hexagonal\nPhaseType = MatrixPhase\n";
+    WriteTestFile(UnitTest::EnsembleInfoReaderTest::TestFileIni, groups); //Number_Phases = 1, but 2 phases
+    var.setValue(UnitTest::EnsembleInfoReaderTest::TestFileIni);
+    propWasSet = filter->setProperty("InputFile", var);
+    DREAM3D_REQUIRE_EQUAL(propWasSet, true)
+    filter->execute();
+		err = filter->getErrorCondition();
+	  DREAM3D_REQUIRE_EQUAL(err, -10008);
+
+		// Reset The data container array with new instances for this sub test
+		dca = DataContainerArray::New();
+		vdc = VolumeDataContainer::New(DREAM3D::Defaults::VolumeDataContainerName);
+		dca->addDataContainer(vdc);
+		filter->setDataContainerArray(dca);
+    groups = "[EnsembleInfo]\nNumber_Phases = 2\n\n[1]\nCrystalStructure = Cubic\n\n\n[2]\nCrystalStructure = Hexagonal\nPhaseType = MatrixPhase\n";
+		WriteTestFile(UnitTest::EnsembleInfoReaderTest::TestFileIni, groups); //PhaseType missing
+		var.setValue(UnitTest::EnsembleInfoReaderTest::TestFileIni); 
+    propWasSet = filter->setProperty("InputFile", var);
+    DREAM3D_REQUIRE_EQUAL(propWasSet, true)
+    filter->execute();
+		err = filter->getErrorCondition();
+		DREAM3D_REQUIRE_EQUAL(err, -10004);
+
+		// Reset The data container array with new instances for this sub test
+		dca = DataContainerArray::New();
+		vdc = VolumeDataContainer::New(DREAM3D::Defaults::VolumeDataContainerName);
+		dca->addDataContainer(vdc);
+		filter->setDataContainerArray(dca);
+    groups = "[EnsembleInfo]\nNumber_Phases = 2\n\n[1]\nCrystalStructure = Cubic\nPhaseType = PrimaryPhase\n\n[2]\n\nPhaseType = MatrixPhase\n";
+	  WriteTestFile(UnitTest::EnsembleInfoReaderTest::TestFileIni, groups); //CrystalStructure missing
+		var.setValue(UnitTest::EnsembleInfoReaderTest::TestFileIni); 
+    propWasSet = filter->setProperty("InputFile", var);
+    DREAM3D_REQUIRE_EQUAL(propWasSet, true)
+    filter->execute();
+		err = filter->getErrorCondition();
+		DREAM3D_REQUIRE_EQUAL(err, -10004);
+
+		// Reset The data container array with new instances for this sub test
+		dca = DataContainerArray::New();
+		vdc = VolumeDataContainer::New(DREAM3D::Defaults::VolumeDataContainerName);
+		dca->addDataContainer(vdc);
+		filter->setDataContainerArray(dca);
+		groups = "[EnsembleInfo]\nNumber_Phases = 2\n\n[1]\nCrystalStructure = Cubic\nPhaseType = PrimaryPase\n\n[2]\nCrystalStructure = Hexagonal\nPhaseType = MatrixPhase\n";
+		WriteTestFile(UnitTest::EnsembleInfoReaderTest::TestFileIni, groups);  //PrimaryPhase is misspelled
+		var.setValue(UnitTest::EnsembleInfoReaderTest::TestFileIni);
+    propWasSet = filter->setProperty("InputFile", var);
+    DREAM3D_REQUIRE_EQUAL(propWasSet, true)
+    filter->execute();
+		err = filter->getErrorCondition();
+		DREAM3D_REQUIRE_EQUAL(err, -10007);
+
+		// Reset The data container array with new instances for this sub test
+		dca = DataContainerArray::New();
+		vdc = VolumeDataContainer::New(DREAM3D::Defaults::VolumeDataContainerName);
+		dca->addDataContainer(vdc);
+		filter->setDataContainerArray(dca);
+		groups = "[EnsembleInfo]\nNumber_Phases = 2\n\n[1]\nCrystalStructure = Cubic\nPhaseType = PrimaryPhase\n\n[3]\nCrystalStructure = Hexagonal\nPhaseType = MatrixPhase\n";
+		WriteTestFile(UnitTest::EnsembleInfoReaderTest::TestFileIni, groups); //Group number is incorrect
+		var.setValue(UnitTest::EnsembleInfoReaderTest::TestFileIni);
+    propWasSet = filter->setProperty("InputFile", var);
+    DREAM3D_REQUIRE_EQUAL(propWasSet, true)
+    filter->execute();
+		err = filter->getErrorCondition();
+		DREAM3D_REQUIRE_EQUAL(err, -10005);
+
+		// Reset The data container array with new instances for this sub test
+		dca = DataContainerArray::New();
+		vdc = VolumeDataContainer::New(DREAM3D::Defaults::VolumeDataContainerName);
+		dca->addDataContainer(vdc);
+		filter->setDataContainerArray(dca);
+		groups = "[EnsembleInfo]\nNumber_Phases = 2\n\n[1]\nCrystalStructure = Cubc\nPhaseType = PrimaryPhase\n\n[2]\nCrystalStructure = Hexagonal\nPhaseType = MatrixPhase\n";
+		WriteTestFile(UnitTest::EnsembleInfoReaderTest::TestFileIni, groups); //Cubic is misspelled
+		var.setValue(UnitTest::EnsembleInfoReaderTest::TestFileIni);
+		propWasSet = filter->setProperty("InputFile", var);
+		DREAM3D_REQUIRE_EQUAL(propWasSet, true)
+			filter->execute();
+		err = filter->getErrorCondition();
+		DREAM3D_REQUIRE_EQUAL(err, -10006);
+
+  }
+  else
+  {
+    QString ss = QObject::tr("EnsembleInfoReaderTest Error creating filter '%1'. Filter was not created/executed. Please notify the developers.").arg(filtName);
+    DREAM3D_REQUIRE_EQUAL(0, 1)
+  }
+  return 1;
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void loadFilterPlugins()
+{
+  // Register all the filters including trying to load those from Plugins
+  FilterManager* fm = FilterManager::Instance();
+  DREAM3DPluginLoader::LoadPluginFilters(fm);
+
+  // Send progress messages from PipelineBuilder to this object for display
+  QMetaObjectUtilities::RegisterMetaTypes();
+}
+
+
+// -----------------------------------------------------------------------------
+//  Use test framework
+// -----------------------------------------------------------------------------
+int main(int argc, char** argv)
+{
+  // Instantiate the QCoreApplication that we need to get the current path and load plugins.
+  QCoreApplication app(argc, argv);
+  QCoreApplication::setOrganizationName("BlueQuartz Software");
+  QCoreApplication::setOrganizationDomain("bluequartz.net");
+  QCoreApplication::setApplicationName("EnsembleInfoReaderTest");
+
+  int err = EXIT_SUCCESS;
+  DREAM3D_REGISTER_TEST( loadFilterPlugins() );
+  DREAM3D_REGISTER_TEST( TestFilterAvailability() );
+
+  DREAM3D_REGISTER_TEST(TestEnsembleInfoWriter())
+  DREAM3D_REGISTER_TEST(TestEnsembleInfoReader())
+
+  DREAM3D_REGISTER_TEST( RemoveTestFiles() )
+  PRINT_TEST_SUMMARY();
+  return err;
+}
+
