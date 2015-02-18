f1181773bc1e8a7f937444cbbfdcc9ff8b4c80e8
Michael Jackson <mike.jackson@bluequartz.net>
2015-02-18 15:06:19 -0500

    Fixing compiler Error on OS X.
    updates #253

--- Source/Plugins/EMMPM/EMMPMFilters/MultiEmmpmFilter.cpp
+++ Source/Plugins/EMMPM/EMMPMFilters/MultiEmmpmFilter.cpp
@@ -27,7 +27,7 @@ m_OutputAttributeMatrixName("EMMPM Output"),
 m_OutputArrayPrefix("Segmented_"),
 m_UsePreviousMuSigma(true)
 {
-	setupFilterParameters();
+  setupFilterParameters();
 }
 
 // -----------------------------------------------------------------------------
@@ -42,7 +42,7 @@ MultiEmmpmFilter::~MultiEmmpmFilter()
 // -----------------------------------------------------------------------------
 void MultiEmmpmFilter::setupFilterParameters()
 {
-  FilterParameterVector& parameters = getFilterParameters();
+  FilterParameterVector parameters = getFilterParameters();
   parameters[0] = FilterParameter::New("Select Input AttributeMatrix", "InputAttributeMatrixPath", FilterParameterWidgetType::AttributeMatrixSelectionWidget, getInputDataArrayPath());
 
 
@@ -68,23 +68,23 @@ void MultiEmmpmFilter::setupFilterParameters()
 // -----------------------------------------------------------------------------
 void MultiEmmpmFilter::readFilterParameters(AbstractFilterParametersReader* reader, int index)
 {
-	reader->openFilterGroup(this, index);
-	setInputAttributeMatrixPath(reader->readDataArrayPath("InputAttributeMatrixPath", getInputAttributeMatrixPath()));
-	setNumClasses(reader->readValue("NumClasses", getNumClasses()));
-	setExchangeEnergy(reader->readValue("ExchangeEnergy", getExchangeEnergy()));
-	setHistogramLoops(reader->readValue("HistogramLoops", getHistogramLoops()));
-	setSegmentationLoops(reader->readValue("SegmentationLoops", getSegmentationLoops()));
-	setUseSimulatedAnnealing(reader->readValue("UseSimulatedAnnealing", getUseSimulatedAnnealing()));
-	setUseGradientPenalty(reader->readValue("UseGradientPenalty", getUseGradientPenalty()));
-	setGradientPenalty(reader->readValue("GradientPenalty", getGradientPenalty()));
-	setUseCurvaturePenalty(reader->readValue("UseCurvaturePenalty", getUseCurvaturePenalty()));
-	setCurvaturePenalty(reader->readValue("CurvaturePenalty", getCurvaturePenalty()));
-	setRMax(reader->readValue("RMax", getRMax()));
-	setEMLoopDelay(reader->readValue("EMLoopDelay", getEMLoopDelay()));
-	setOutputAttributeMatrixName(reader->readString("OutputAttributeMatrixName", getOutputAttributeMatrixName()));
+  reader->openFilterGroup(this, index);
+  setInputAttributeMatrixPath(reader->readDataArrayPath("InputAttributeMatrixPath", getInputAttributeMatrixPath()));
+  setNumClasses(reader->readValue("NumClasses", getNumClasses()));
+  setExchangeEnergy(reader->readValue("ExchangeEnergy", getExchangeEnergy()));
+  setHistogramLoops(reader->readValue("HistogramLoops", getHistogramLoops()));
+  setSegmentationLoops(reader->readValue("SegmentationLoops", getSegmentationLoops()));
+  setUseSimulatedAnnealing(reader->readValue("UseSimulatedAnnealing", getUseSimulatedAnnealing()));
+  setUseGradientPenalty(reader->readValue("UseGradientPenalty", getUseGradientPenalty()));
+  setGradientPenalty(reader->readValue("GradientPenalty", getGradientPenalty()));
+  setUseCurvaturePenalty(reader->readValue("UseCurvaturePenalty", getUseCurvaturePenalty()));
+  setCurvaturePenalty(reader->readValue("CurvaturePenalty", getCurvaturePenalty()));
+  setRMax(reader->readValue("RMax", getRMax()));
+  setEMLoopDelay(reader->readValue("EMLoopDelay", getEMLoopDelay()));
+  setOutputAttributeMatrixName(reader->readString("OutputAttributeMatrixName", getOutputAttributeMatrixName()));
   setUsePreviousMuSigma(reader->readValue("UsePreviousMuSigma", getUsePreviousMuSigma()));
   setOutputArrayPrefix(reader->readString("OutputArrayPrefix", getOutputArrayPrefix()));
-	reader->closeFilterGroup();
+  reader->closeFilterGroup();
 }
 
 // -----------------------------------------------------------------------------
@@ -92,26 +92,26 @@ void MultiEmmpmFilter::readFilterParameters(AbstractFilterParametersReader* read
 // -----------------------------------------------------------------------------
 int MultiEmmpmFilter::writeFilterParameters(AbstractFilterParametersWriter* writer, int index)
 {
-	writer->openFilterGroup(this, index);
+  writer->openFilterGroup(this, index);
 
-	DREAM3D_FILTER_WRITE_PARAMETER(InputAttributeMatrixPath)
-	DREAM3D_FILTER_WRITE_PARAMETER(NumClasses)
-	DREAM3D_FILTER_WRITE_PARAMETER(ExchangeEnergy)
-	DREAM3D_FILTER_WRITE_PARAMETER(HistogramLoops)
-	DREAM3D_FILTER_WRITE_PARAMETER(SegmentationLoops)
-	DREAM3D_FILTER_WRITE_PARAMETER(UseSimulatedAnnealing)
-	DREAM3D_FILTER_WRITE_PARAMETER(UseGradientPenalty)
-	DREAM3D_FILTER_WRITE_PARAMETER(GradientPenalty)
-	DREAM3D_FILTER_WRITE_PARAMETER(UseCurvaturePenalty)
-	DREAM3D_FILTER_WRITE_PARAMETER(CurvaturePenalty)
-	DREAM3D_FILTER_WRITE_PARAMETER(RMax)
-	DREAM3D_FILTER_WRITE_PARAMETER(EMLoopDelay)
-	DREAM3D_FILTER_WRITE_PARAMETER(OutputAttributeMatrixName)
+  DREAM3D_FILTER_WRITE_PARAMETER(InputAttributeMatrixPath)
+  DREAM3D_FILTER_WRITE_PARAMETER(NumClasses)
+  DREAM3D_FILTER_WRITE_PARAMETER(ExchangeEnergy)
+  DREAM3D_FILTER_WRITE_PARAMETER(HistogramLoops)
+  DREAM3D_FILTER_WRITE_PARAMETER(SegmentationLoops)
+  DREAM3D_FILTER_WRITE_PARAMETER(UseSimulatedAnnealing)
+  DREAM3D_FILTER_WRITE_PARAMETER(UseGradientPenalty)
+  DREAM3D_FILTER_WRITE_PARAMETER(GradientPenalty)
+  DREAM3D_FILTER_WRITE_PARAMETER(UseCurvaturePenalty)
+  DREAM3D_FILTER_WRITE_PARAMETER(CurvaturePenalty)
+  DREAM3D_FILTER_WRITE_PARAMETER(RMax)
+  DREAM3D_FILTER_WRITE_PARAMETER(EMLoopDelay)
+  DREAM3D_FILTER_WRITE_PARAMETER(OutputAttributeMatrixName)
   DREAM3D_FILTER_WRITE_PARAMETER(UsePreviousMuSigma)
   DREAM3D_FILTER_WRITE_PARAMETER(OutputArrayPrefix)
 
-	writer->closeFilterGroup();
-	return ++index; // we want to return the next index that was just written to
+  writer->closeFilterGroup();
+  return ++index; // we want to return the next index that was just written to
 }
 
 // -----------------------------------------------------------------------------
@@ -119,29 +119,29 @@ int MultiEmmpmFilter::writeFilterParameters(AbstractFilterParametersWriter* writ
 // -----------------------------------------------------------------------------
 void MultiEmmpmFilter::dataCheck()
 {
-	setErrorCondition(0);
+  setErrorCondition(0);
 
-	QVector<size_t> cDims(1, 1); // We need a single component, gray scale image
-	
+  QVector<size_t> cDims(1, 1); // We need a single component, gray scale image
+
 #if 0
   m_InputImagePtr = getDataContainerArray()->getPrereqArrayFromPath<DataArray<uint8_t>, AbstractFilter>(this, getInputDataArrayPath(), cDims); /* Assigns the shared_ptr<> to an instance variable that is a weak_ptr<> */
-	if (NULL != m_InputImagePtr.lock().get()) /* Validate the Weak Pointer wraps a non-NULL pointer to a DataArray<T> object */
-	{
-		m_InputImage = m_InputImagePtr.lock()->getPointer(0);
-	} /* Now assign the raw pointer to data from the DataArray<T> object */
-	if (getErrorCondition() < 0) { return; }
+  if (NULL != m_InputImagePtr.lock().get()) /* Validate the Weak Pointer wraps a non-NULL pointer to a DataArray<T> object */
+  {
+    m_InputImage = m_InputImagePtr.lock()->getPointer(0);
+  } /* Now assign the raw pointer to data from the DataArray<T> object */
+  if (getErrorCondition() < 0) { return; }
 
-	ImageGeom::Pointer image = getDataContainerArray()->getDataContainer(getInputDataArrayPath().getDataContainerName())->getPrereqGeometry<ImageGeom, AbstractFilter>(this);
-	if (getErrorCondition() < 0 || NULL == image.get()) { return; }
+  ImageGeom::Pointer image = getDataContainerArray()->getDataContainer(getInputDataArrayPath().getDataContainerName())->getPrereqGeometry<ImageGeom, AbstractFilter>(this);
+  if (getErrorCondition() < 0 || NULL == image.get()) { return; }
 
-	m_OutputImagePtr = getDataContainerArray()->createNonPrereqArrayFromPath<DataArray<uint8_t>, AbstractFilter, uint8_t>(this, getOutputDataArrayPath(), 0, cDims); /* Assigns the shared_ptr<> to an instance variable that is a weak_ptr<> */
-	if (NULL != m_OutputImagePtr.lock().get()) /* Validate the Weak Pointer wraps a non-NULL pointer to a DataArray<T> object */
-	{
-		m_OutputImage = m_OutputImagePtr.lock()->getPointer(0);
-	} /* Now assign the raw pointer to data from the DataArray<T> object */
+  m_OutputImagePtr = getDataContainerArray()->createNonPrereqArrayFromPath<DataArray<uint8_t>, AbstractFilter, uint8_t>(this, getOutputDataArrayPath(), 0, cDims); /* Assigns the shared_ptr<> to an instance variable that is a weak_ptr<> */
+  if (NULL != m_OutputImagePtr.lock().get()) /* Validate the Weak Pointer wraps a non-NULL pointer to a DataArray<T> object */
+  {
+    m_OutputImage = m_OutputImagePtr.lock()->getPointer(0);
+  } /* Now assign the raw pointer to data from the DataArray<T> object */
 
  #endif
-  
+
   if(getOutputArrayPrefix().isEmpty())
   {
     setErrorCondition(-62002);
@@ -163,9 +163,9 @@ void MultiEmmpmFilter::dataCheck()
   if(getErrorCondition() < 0)
   {
     return;
-  }  
-  
-  
+  }
+
+
   // Get the list of the array names from the input attribute matrix
   QList<QString> arrayNames = inAM->getAttributeArrayNames();
   DataArrayPath path = getInputAttributeMatrixPath();
@@ -177,19 +177,19 @@ void MultiEmmpmFilter::dataCheck()
   }
 
   // The EM/MPM Library has a hard coded MAX Classes of 16
-	if (getNumClasses() > 15)
-	{
-		setErrorCondition(-62000);
-		QString ss = QObject::tr("The Maximum number of classes is 15");
-		notifyErrorMessage(getHumanLabel(), ss, getErrorCondition());
-	}
+  if (getNumClasses() > 15)
+  {
+    setErrorCondition(-62000);
+    QString ss = QObject::tr("The Maximum number of classes is 15");
+    notifyErrorMessage(getHumanLabel(), ss, getErrorCondition());
+  }
   // It does not make any sense if we want anything less than 2 classes
-	if (getNumClasses() < 2)
-	{
-		setErrorCondition(-62001);
-		QString ss = QObject::tr("The Minimum number of classes is 2");
-		notifyErrorMessage(getHumanLabel(), ss, getErrorCondition());
-	}
+  if (getNumClasses() < 2)
+  {
+    setErrorCondition(-62001);
+    QString ss = QObject::tr("The Minimum number of classes is 2");
+    notifyErrorMessage(getHumanLabel(), ss, getErrorCondition());
+  }
 
 }
 
@@ -198,13 +198,13 @@ void MultiEmmpmFilter::dataCheck()
 // -----------------------------------------------------------------------------
 void MultiEmmpmFilter::preflight()
 {
-	// These are the REQUIRED lines of CODE to make sure the filter behaves correctly
-	setInPreflight(true); // Set the fact that we are preflighting.
-	emit preflightAboutToExecute(); // Emit this signal so that other widgets can do one file update
-	emit updateFilterParameters(this); // Emit this signal to have the widgets push their values down to the filter
-	dataCheck(); // Run our DataCheck to make sure everthing is setup correctly
-	emit preflightExecuted(); // We are done preflighting this filter
-	setInPreflight(false); // Inform the system this filter is NOT in preflight mode anymore.
+  // These are the REQUIRED lines of CODE to make sure the filter behaves correctly
+  setInPreflight(true); // Set the fact that we are preflighting.
+  emit preflightAboutToExecute(); // Emit this signal so that other widgets can do one file update
+  emit updateFilterParameters(this); // Emit this signal to have the widgets push their values down to the filter
+  dataCheck(); // Run our DataCheck to make sure everthing is setup correctly
+  emit preflightExecuted(); // We are done preflighting this filter
+  setInPreflight(false); // Inform the system this filter is NOT in preflight mode anymore.
 }
 
 // -----------------------------------------------------------------------------
@@ -212,30 +212,30 @@ void MultiEmmpmFilter::preflight()
 // -----------------------------------------------------------------------------
 void MultiEmmpmFilter::execute()
 {
-	int err = 0;
-	// typically run your dataCheck function to make sure you can get that far and all your variables are initialized
-	dataCheck();
-	// Check to make sure you made it through the data check. Errors would have been reported already so if something
-	// happens to fail in the dataCheck() then we simply return
-	if (getErrorCondition() < 0) { return; }
-	setErrorCondition(0);
+  int err = 0;
+  // typically run your dataCheck function to make sure you can get that far and all your variables are initialized
+  dataCheck();
+  // Check to make sure you made it through the data check. Errors would have been reported already so if something
+  // happens to fail in the dataCheck() then we simply return
+  if (getErrorCondition() < 0) { return; }
+  setErrorCondition(0);
 
-	AttributeMatrix::Pointer am = getDataContainerArray()->getAttributeMatrix(getInputAttributeMatrixPath());
-	QList<QString> arrayNames = am->getAttributeArrayNames();
-	QListIterator<QString> iter(arrayNames);
-  
+  AttributeMatrix::Pointer am = getDataContainerArray()->getAttributeMatrix(getInputAttributeMatrixPath());
+  QList<QString> arrayNames = am->getAttributeArrayNames();
+  QListIterator<QString> iter(arrayNames);
+
   QString msgPrefix = getMessagePrefix();
   int i = 1;
-	// This is the routine that sets up the EM/MPM to segment the image
-	while (iter.hasNext())
-	{
-		DataArrayPath arrayPath = getInputAttributeMatrixPath();
+  // This is the routine that sets up the EM/MPM to segment the image
+  while (iter.hasNext())
+  {
+    DataArrayPath arrayPath = getInputAttributeMatrixPath();
     QString name = iter.next();
-    
-		arrayPath.setDataArrayName(name);
-		setInputDataArrayPath(arrayPath);
-    
-    // Change the output AttributeMatrix 
+
+    arrayPath.setDataArrayName(name);
+    setInputDataArrayPath(arrayPath);
+
+    // Change the output AttributeMatrix
     arrayPath.setAttributeMatrixName(getOutputAttributeMatrixName());
     QString outName = getOutputArrayPrefix() + arrayPath.getDataArrayName();
     arrayPath.setDataArrayName(outName);
@@ -243,7 +243,7 @@ void MultiEmmpmFilter::execute()
 
     QString prefix = QObject::tr("%1 (Array %2 of %3)").arg(msgPrefix).arg(i).arg(arrayNames.size());
     setMessagePrefix(prefix);
-    
+
     if ( i == 2 && getUsePreviousMuSigma())
     {
       setEmmpmInitType(EMMPM_ManualInit);
@@ -255,22 +255,22 @@ void MultiEmmpmFilter::execute()
 
     EMMPMFilter::execute();
     if ( getErrorCondition() < 0 ) { break; }
-		i++;
-    
+    i++;
+
     if ( getCancel() ) { break; }
-	}
+  }
 
-	/* If some error occurs this code snippet can report the error up the call chain*/
-	if (getErrorCondition() < 0)
-	{
-		QString ss = QObject::tr("There was a generic error running the EMMPM Segmentation Filter. Please contact the developers.");
-		setErrorCondition(-60009);
-		notifyErrorMessage(getHumanLabel(), ss, getErrorCondition());
-		return;
-	}
+  /* If some error occurs this code snippet can report the error up the call chain*/
+  if (getErrorCondition() < 0)
+  {
+    QString ss = QObject::tr("There was a generic error running the EMMPM Segmentation Filter. Please contact the developers.");
+    setErrorCondition(-60009);
+    notifyErrorMessage(getHumanLabel(), ss, getErrorCondition());
+    return;
+  }
 
-	/* Let the GUI know we are done with this filter */
-	notifyStatusMessage(getHumanLabel(), "Complete");
+  /* Let the GUI know we are done with this filter */
+  notifyStatusMessage(getHumanLabel(), "Complete");
 }
 
 // -----------------------------------------------------------------------------
@@ -278,7 +278,7 @@ void MultiEmmpmFilter::execute()
 // -----------------------------------------------------------------------------
 const QString MultiEmmpmFilter::getCompiledLibraryName()
 {
-	return EMMPMPlugin::EMMPMBaseName;
+  return EMMPMPlugin::EMMPMBaseName;
 }
 
 // -----------------------------------------------------------------------------
@@ -286,7 +286,7 @@ const QString MultiEmmpmFilter::getCompiledLibraryName()
 // -----------------------------------------------------------------------------
 const QString MultiEmmpmFilter::getGroupName()
 {
-	return "Segmentation";
+  return "Segmentation";
 }
 
 // -----------------------------------------------------------------------------
@@ -294,7 +294,7 @@ const QString MultiEmmpmFilter::getGroupName()
 // -----------------------------------------------------------------------------
 const QString MultiEmmpmFilter::getHumanLabel()
 {
-	return "MultiArray EMMPM Segmentation";
+  return "MultiArray EMMPM Segmentation";
 }
 
 // -----------------------------------------------------------------------------
@@ -302,7 +302,7 @@ const QString MultiEmmpmFilter::getHumanLabel()
 // -----------------------------------------------------------------------------
 const QString MultiEmmpmFilter::getSubGroupName()
 {
-	return "EMMPM";
+  return "EMMPM";
 }
 
 
@@ -311,27 +311,27 @@ const QString MultiEmmpmFilter::getSubGroupName()
 // -----------------------------------------------------------------------------
 AbstractFilter::Pointer MultiEmmpmFilter::newFilterInstance(bool copyFilterParameters)
 {
-	/*
-	* write code to optionally copy the filter parameters from the current filter into the new instance
-	*/
-	MultiEmmpmFilter::Pointer filter = MultiEmmpmFilter::New();
-	if (true == copyFilterParameters)
-	{
-	  DREAM3D_COPY_INSTANCEVAR(InputAttributeMatrixPath)
-	  DREAM3D_COPY_INSTANCEVAR(NumClasses)
-	  DREAM3D_COPY_INSTANCEVAR(ExchangeEnergy)
-	  DREAM3D_COPY_INSTANCEVAR(HistogramLoops)
-	  DREAM3D_COPY_INSTANCEVAR(SegmentationLoops)
-	  DREAM3D_COPY_INSTANCEVAR(UseSimulatedAnnealing)
-	  DREAM3D_COPY_INSTANCEVAR(UseGradientPenalty)
-	  DREAM3D_COPY_INSTANCEVAR(GradientPenalty)
-	  DREAM3D_COPY_INSTANCEVAR(UseCurvaturePenalty)
-	  DREAM3D_COPY_INSTANCEVAR(CurvaturePenalty)
-	  DREAM3D_COPY_INSTANCEVAR(RMax)
-	  DREAM3D_COPY_INSTANCEVAR(EMLoopDelay)
-	  DREAM3D_COPY_INSTANCEVAR(OutputAttributeMatrixName)
+  /*
+  * write code to optionally copy the filter parameters from the current filter into the new instance
+  */
+  MultiEmmpmFilter::Pointer filter = MultiEmmpmFilter::New();
+  if (true == copyFilterParameters)
+  {
+    DREAM3D_COPY_INSTANCEVAR(InputAttributeMatrixPath)
+    DREAM3D_COPY_INSTANCEVAR(NumClasses)
+    DREAM3D_COPY_INSTANCEVAR(ExchangeEnergy)
+    DREAM3D_COPY_INSTANCEVAR(HistogramLoops)
+    DREAM3D_COPY_INSTANCEVAR(SegmentationLoops)
+    DREAM3D_COPY_INSTANCEVAR(UseSimulatedAnnealing)
+    DREAM3D_COPY_INSTANCEVAR(UseGradientPenalty)
+    DREAM3D_COPY_INSTANCEVAR(GradientPenalty)
+    DREAM3D_COPY_INSTANCEVAR(UseCurvaturePenalty)
+    DREAM3D_COPY_INSTANCEVAR(CurvaturePenalty)
+    DREAM3D_COPY_INSTANCEVAR(RMax)
+    DREAM3D_COPY_INSTANCEVAR(EMLoopDelay)
+    DREAM3D_COPY_INSTANCEVAR(OutputAttributeMatrixName)
 
-	}
-	return filter;
+  }
+  return filter;
 }
 
