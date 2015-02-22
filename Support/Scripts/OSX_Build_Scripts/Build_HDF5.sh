69e8e3e23501183203123a5c2de58516c1736b83
Michael Jackson <mike.jackson@bluequartz.net>
2015-02-22 11:21:12 -0500

    Updating HDF5 version to 1.8.14 for build scripts.

--- Support/Scripts/OSX_Build_Scripts/Build_HDF5.sh
+++ Support/Scripts/OSX_Build_Scripts/Build_HDF5.sh
@@ -47,34 +47,34 @@ fi
 
 # Build the HDF5 libraries we need and set our Environment Variable.
 
-if [ ! -e "$SDK_INSTALL/hdf5-1.8.13.tar.gz" ];
+if [ ! -e "$SDK_INSTALL/hdf5-1.8.14.tar.gz" ];
 then
   echo "-------------------------------------------"
-  echo " Downloading HDF5 Version 1.8.13 "
+  echo " Downloading HDF5 Version 1.8.14 "
   echo "-------------------------------------------"
-  $DOWNLOAD_PROG  "http://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.13.tar.gz" -o hdf5-1.8.13.tar.gz
+  $DOWNLOAD_PROG  "http://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.14.tar.gz" -o hdf5-1.8.14.tar.gz
 fi
 
-if [ ! -e "$SDK_INSTALL/hdf5-1.8.13_source" ];
+if [ ! -e "$SDK_INSTALL/hdf5-1.8.14_source" ];
 then
-  tar -xvzf hdf5-1.8.13.tar.gz
-  mv hdf5-1.8.13 hdf5-1.8.13_source
+  tar -xvzf hdf5-1.8.14.tar.gz
+  mv hdf5-1.8.14 hdf5-1.8.14_source
 fi
 # We assume we already have downloaded the source for HDF5 Version 1.8.7 and have it in a folder
 # called hdf5-188
-cd hdf5-1.8.13_source
+cd hdf5-1.8.14_source
 mkdir Build
 cd Build
-cmake cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$SDK_INSTALL/hdf5-1.8.13 -DHDF5_ENABLE_DEPRECATED_SYMBOLS=OFF  -DHDF5_BUILD_WITH_INSTALL_NAME=ON -DHDF5_BUILD_CPP_LIB=ON -DHDF5_BUILD_HL_LIB=ON ../
-make -j$PARALLEL_BUILD
+cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Debug -DHDF5_BUILD_WITH_INSTALL_NAME=ON -DHDF5_BUILD_CPP_LIB=ON -DHDF5_BUILD_HL_LIB=ON -DCMAKE_INSTALL_PREFIX=$SDK_INSTALL/hdf5-1.8.14 ../
+make -j
 make install
 cd ../
 mkdir zRel
 cd zRel
-cmake cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$SDK_INSTALL/hdf5-1.8.13 -DHDF5_ENABLE_DEPRECATED_SYMBOLS=OFF  -DHDF5_BUILD_WITH_INSTALL_NAME=ON -DHDF5_BUILD_CPP_LIB=ON -DHDF5_BUILD_HL_LIB=ON -DHDF5_BUILD_TOOLS=ON ../
-make -j$PARALLEL_BUILD
+cmake  -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DHDF5_BUILD_WITH_INSTALL_NAME=ON -DHDF5_BUILD_CPP_LIB=ON -DHDF5_BUILD_HL_LIB=ON -DCMAKE_INSTALL_PREFIX=$SDK_INSTALL/hdf5-1.8.14 ../
+make -j
 make install
-export HDF5_INSTALL=$SDK_INSTALL/hdf5-1.8.13
+export HDF5_INSTALL=$SDK_INSTALL/hdf5-1.8.14
 
 
 
