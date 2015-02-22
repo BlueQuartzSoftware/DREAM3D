9a2f51cd47d2194b54e68f6619e16b4b0bca944b
Michael Jackson <mike.jackson@bluequartz.net>
2015-02-22 11:20:38 -0500

    Fix compile errors on Windows due to missing cblas.h. Temporarily removing the compiled files until an open-source BLAS can be found for Windows.

--- Source/OrientationLib/Math/SourceList.cmake
+++ Source/OrientationLib/Math/SourceList.cmake
@@ -13,14 +13,13 @@
 
 set(OrientationLib_Math_HDRS
   ${OrientationLib_SOURCE_DIR}/Math/OrientationMath.h
-  ${OrientationLib_SOURCE_DIR}/Math/RotationTransforms.h
-  ${OrientationLib_SOURCE_DIR}/Math/RotArray.hpp
+#  ${OrientationLib_SOURCE_DIR}/Math/RotationTransforms.h
+#  ${OrientationLib_SOURCE_DIR}/Math/RotArray.hpp
 )
 
 set(OrientationLib_Math_SRCS
   ${OrientationLib_SOURCE_DIR}/Math/OrientationMath.cpp
-  ${OrientationLib_SOURCE_DIR}/Math/RotationTransforms.cpp
-
+#  ${OrientationLib_SOURCE_DIR}/Math/RotationTransforms.cpp
 )
 cmp_IDE_SOURCE_PROPERTIES( "OrientationLib/Math" "${OrientationLib_Math_HDRS}" "${OrientationLib_Math_SRCS}" "0")
 if( ${PROJECT_INSTALL_HEADERS} EQUAL 1 )
