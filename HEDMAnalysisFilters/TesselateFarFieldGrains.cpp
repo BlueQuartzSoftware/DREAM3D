d70d509276676cc4848dbdc7c95dce19dac51859
Joey Kleingers <joey.kleingers@bluequartz.net>
2015-06-09 09:01:31 -0400

    Adding include statements.

--- HEDMAnalysisFilters/TesselateFarFieldGrains.cpp
+++ HEDMAnalysisFilters/TesselateFarFieldGrains.cpp
@@ -56,6 +56,7 @@
 #include "DREAM3DLib/FilterParameters/AbstractFilterParametersReader.h"
 #include "DREAM3DLib/FilterParameters/AbstractFilterParametersWriter.h"
 #include "DREAM3DLib/FilterParameters/FileListInfoFilterParameter.h"
+#include "DREAM3DLib/FilterParameters/SeparatorFilterParameter.h"
 #include "DREAM3DLib/Math/DREAM3DMath.h"
 #include "DREAM3DLib/Math/MatrixMath.h"
 #include "DREAM3DLib/StatsData/PrimaryStatsData.h"
