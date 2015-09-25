6867ac42cd96a5a202cd57c26ff67a0e3aec6691
Michael Jackson <mike.jackson@bluequartz.net>
2015-09-25 09:43:40 -0400

    Modifying errors codes to ensure unique values

--- Source/Plugins/SyntheticBuilding/SyntheticBuildingFilters/InitializeSyntheticVolume.cpp
+++ Source/Plugins/SyntheticBuilding/SyntheticBuildingFilters/InitializeSyntheticVolume.cpp
@@ -291,7 +291,7 @@ QString InitializeSyntheticVolume::estimateNumFeatures(IntVec3_t dims, FloatVec3
   if (phaseType.get() == NULL)
   {
     QString ss = QObject::tr("Phase types array could not be downcast using boost::dynamic_pointer_cast<T> when estimating the number of grains. The path is %1").arg(getInputPhaseTypesArrayPath().serialize());
-    setErrorCondition(-80000);
+    setErrorCondition(-11002);
     notifyErrorMessage(getHumanLabel(), ss, getErrorCondition());
     return "0";
   }
@@ -302,7 +302,7 @@ QString InitializeSyntheticVolume::estimateNumFeatures(IntVec3_t dims, FloatVec3
   if (statsPtr.get() == NULL)
   {
     QString ss = QObject::tr("Statistics array could not be downcast using boost::dynamic_pointer_cast<T> when estimating the number of grains. The path is %1").arg(getInputStatsArrayPath().serialize());
-    notifyErrorMessage(getHumanLabel(), ss, -80001);
+    notifyErrorMessage(getHumanLabel(), ss, -11001);
     return "0";
   }
 
