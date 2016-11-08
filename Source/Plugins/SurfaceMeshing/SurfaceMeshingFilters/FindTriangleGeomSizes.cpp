41bc4d3fae9fba6657d34cebe3c0a0bd7de5cd56
Donegan Sean P Civ USAF AFMC AFRL/RXCM <1030681742@mil>
2016-11-08 12:52:12 -0500

    Add filter that computes the enclosed volume of surface meshed features
    The algorithm simply computes the signed volume of all tetrahedra formed by the
    feature triangle faces and another point (here chosen to be the origin). Also
    add a unit test and relevant documentation.
    
    updates #595
    closes #595

---
+++ Source/Plugins/SurfaceMeshing/SurfaceMeshingFilters/FindTriangleGeomSizes.cpp
@@ -0,0 +1,279 @@
+/* ============================================================================
+* Copyright (c) 2009-2016 BlueQuartz Software, LLC
+*
+* Redistribution and use in source and binary forms, with or without modification,
+* are permitted provided that the following conditions are met:
+*
+* Redistributions of source code must retain the above copyright notice, this
+* list of conditions and the following disclaimer.
+*
+* Redistributions in binary form must reproduce the above copyright notice, this
+* list of conditions and the following disclaimer in the documentation and/or
+* other materials provided with the distribution.
+*
+* Neither the name of BlueQuartz Software, the US Air Force, nor the names of its
+* contributors may be used to endorse or promote products derived from this software
+* without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+* USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+* The code contained herein was partially funded by the followig contracts:
+*    United States Air Force Prime Contract FA8650-07-D-5800
+*    United States Air Force Prime Contract FA8650-10-D-5210
+*    United States Prime Contract Navy N00173-07-C-2068
+*
+* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+#include "FindTriangleGeomSizes.h"
+
+#include <set>
+
+#include "SIMPLib/Common/Constants.h"
+
+#include "SIMPLib/FilterParameters/DataArraySelectionFilterParameter.h"
+#include "SIMPLib/FilterParameters/StringFilterParameter.h"
+#include "SIMPLib/Geometry/TriangleGeom.h"
+#include "SIMPLib/Math/MatrixMath.h"
+
+#include "SurfaceMeshing/SurfaceMeshingConstants.h"
+#include "SurfaceMeshing/SurfaceMeshingVersion.h"
+
+// Include the MOC generated file for this class
+#include "moc_FindTriangleGeomSizes.cpp"
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+FindTriangleGeomSizes::FindTriangleGeomSizes() :
+  AbstractFilter(),
+  m_FaceLabelsArrayPath(SIMPL::Defaults::TriangleDataContainerName, SIMPL::Defaults::FaceAttributeMatrixName, SIMPL::FaceData::SurfaceMeshFaceLabels)
+, m_FeatureAttributeMatrixName(SIMPL::Defaults::FaceFeatureAttributeMatrixName)
+, m_VolumesArrayName(SIMPL::FeatureData::Volumes)
+, m_FaceLabels(nullptr)
+, m_Volumes(nullptr)
+{
+  initialize();
+  setupFilterParameters();
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+FindTriangleGeomSizes::~FindTriangleGeomSizes()
+{
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void FindTriangleGeomSizes::initialize()
+{
+  setErrorCondition(0);
+  setCancel(false);
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void FindTriangleGeomSizes::setupFilterParameters()
+{
+  FilterParameterVector parameters;
+  DataArraySelectionFilterParameter::RequirementType dasReq = DataArraySelectionFilterParameter::CreateRequirement(SIMPL::TypeNames::Int32, 2, SIMPL::AttributeMatrixType::Face, SIMPL::GeometryType::TriangleGeometry);
+  parameters.push_back(SIMPL_NEW_DA_SELECTION_FP("Face Labels", FaceLabelsArrayPath, FilterParameter::RequiredArray, FindTriangleGeomSizes, dasReq));
+  parameters.push_back(SIMPL_NEW_STRING_FP("Feature Attribute Matrix", FeatureAttributeMatrixName, FilterParameter::CreatedArray, FindTriangleGeomSizes));
+  parameters.push_back(SIMPL_NEW_STRING_FP("Volumes", VolumesArrayName, FilterParameter::CreatedArray, FindTriangleGeomSizes));
+  setFilterParameters(parameters);
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void FindTriangleGeomSizes::dataCheck()
+{
+  setErrorCondition(0);
+
+  QVector<IDataArray::Pointer> dataArrays;
+
+  TriangleGeom::Pointer triangles = getDataContainerArray()->getPrereqGeometryFromDataContainer<TriangleGeom, AbstractFilter>(this, getFaceLabelsArrayPath().getDataContainerName());
+
+  if(getErrorCondition() < 0) { return; }
+
+  dataArrays.push_back(triangles->getTriangles());
+
+  QVector<size_t> cDims(1, 2);
+
+  m_FaceLabelsPtr =
+      getDataContainerArray()->getPrereqArrayFromPath<DataArray<int32_t>, AbstractFilter>(this, getFaceLabelsArrayPath(), cDims); /* Assigns the shared_ptr<> to an instance variable that is a weak_ptr<> */
+  if(nullptr != m_FaceLabelsPtr.lock().get()) /* Validate the Weak Pointer wraps a non-nullptr pointer to a DataArray<T> object */
+  {
+    m_FaceLabels = m_FaceLabelsPtr.lock()->getPointer(0);
+  } /* Now assign the raw pointer to data from the DataArray<T> object */
+  if(getErrorCondition() >= 0) { dataArrays.push_back(m_FaceLabelsPtr.lock()); }
+
+  DataContainer::Pointer tdc = getDataContainerArray()->getPrereqDataContainer<AbstractFilter>(this, getFaceLabelsArrayPath().getDataContainerName());
+
+  if(getErrorCondition() < 0) { return; }
+
+  QVector<size_t> tDims(1, 0);
+
+  tdc->createNonPrereqAttributeMatrix<AbstractFilter>(this, getFeatureAttributeMatrixName(), tDims, SIMPL::AttributeMatrixType::FaceFeature);
+
+  DataArrayPath path(getFaceLabelsArrayPath().getDataContainerName(), getFeatureAttributeMatrixName(), getVolumesArrayName());
+
+  cDims[0] = 1;
+
+  m_VolumesPtr =
+      getDataContainerArray()->createNonPrereqArrayFromPath<DataArray<float>, AbstractFilter>(this, path, 0, cDims); /* Assigns the shared_ptr<> to an instance variable that is a weak_ptr<> */
+  if(nullptr != m_VolumesPtr.lock().get()) /* Validate the Weak Pointer wraps a non-nullptr pointer to a DataArray<T> object */
+  {
+    m_Volumes = m_VolumesPtr.lock()->getPointer(0);
+  } /* Now assign the raw pointer to data from the DataArray<T> object */
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void FindTriangleGeomSizes::preflight()
+{
+  // These are the REQUIRED lines of CODE to make sure the filter behaves correctly
+  setInPreflight(true); // Set the fact that we are preflighting.
+  emit preflightAboutToExecute(); // Emit this signal so that other widgets can do one file update
+  emit updateFilterParameters(this); // Emit this signal to have the widgets push their values down to the filter
+  dataCheck(); // Run our DataCheck to make sure everthing is setup correctly
+  emit preflightExecuted(); // We are done preflighting this filter
+  setInPreflight(false); // Inform the system this filter is NOT in preflight mode anymore.
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+float FindTriangleGeomSizes::findTetrahedronVolume(int64_t vertIds[3], float* vertPtr)
+{
+  float vertMatrix[3][3] = { { vertPtr[3 * vertIds[1] + 0] - vertPtr[3 * vertIds[0] + 0],
+                               vertPtr[3 * vertIds[2] + 0] - vertPtr[3 * vertIds[0] + 0],
+                                                      0.0f - vertPtr[3 * vertIds[0] + 0] },
+                             { vertPtr[3 * vertIds[1] + 1] - vertPtr[3 * vertIds[0] + 1],
+                               vertPtr[3 * vertIds[2] + 1] - vertPtr[3 * vertIds[0] + 1],
+                                                      0.0f - vertPtr[3 * vertIds[0] + 1] },
+                             { vertPtr[3 * vertIds[1] + 2] - vertPtr[3 * vertIds[0] + 2],
+                               vertPtr[3 * vertIds[2] + 2] - vertPtr[3 * vertIds[0] + 2],
+                                                      0.0f - vertPtr[3 * vertIds[0] + 2] }
+                           };
+
+  return (MatrixMath::Determinant3x3(vertMatrix) / 6.0f);
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void FindTriangleGeomSizes::execute()
+{
+  initialize();
+  dataCheck();
+  if(getErrorCondition() < 0) { return; }
+
+  TriangleGeom::Pointer triangles = getDataContainerArray()->getDataContainer(m_FaceLabelsArrayPath.getDataContainerName())->getGeometryAs<TriangleGeom>();
+  float* vertPtr = triangles->getVertexPointer(0);
+
+  std::set<int32_t> featureSet;
+  int64_t numTriangles = triangles->getNumberOfTris();
+
+  for(int64_t i = 0; i < numTriangles; i++)
+  {
+    if(m_FaceLabels[2 * i + 0] > 0) { featureSet.insert(m_FaceLabels[2 * i + 0]); }
+    if(m_FaceLabels[2 * i + 1] > 0) { featureSet.insert(m_FaceLabels[2 * i + 1]); }
+  }
+
+  QVector<size_t> tDims(1, featureSet.size() + 1);
+  AttributeMatrix::Pointer featAttrMat = getDataContainerArray()->getDataContainer(m_FaceLabelsArrayPath.getDataContainerName())->getAttributeMatrix(m_FeatureAttributeMatrixName);
+  featAttrMat->resizeAttributeArrays(tDims);
+  m_Volumes = m_VolumesPtr.lock()->getPointer(0);
+
+  int64_t vertsAtTri[3] = {0, 0, 0};
+
+  for(int64_t i = 0; i < numTriangles; i++)
+  {
+    triangles->getVertsAtTri(i, vertsAtTri);
+    if(m_FaceLabels[2 * i + 0] == -1)
+    {
+      std::swap(vertsAtTri[2], vertsAtTri[1]);
+      m_Volumes[m_FaceLabels[2 * i + 1]] += findTetrahedronVolume(vertsAtTri, vertPtr);
+    }
+    else if(m_FaceLabels[2 * i + 1] == -1)
+    {
+      m_Volumes[m_FaceLabels[2 * i + 0]] += findTetrahedronVolume(vertsAtTri, vertPtr);
+    }
+    else
+    {
+      m_Volumes[m_FaceLabels[2 * i + 0]] += findTetrahedronVolume(vertsAtTri, vertPtr);
+      std::swap(vertsAtTri[2], vertsAtTri[1]);
+      m_Volumes[m_FaceLabels[2 * i + 1]] += findTetrahedronVolume(vertsAtTri, vertPtr);
+    }
+  }
+
+  notifyStatusMessage(getHumanLabel(), "Complete");
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+AbstractFilter::Pointer FindTriangleGeomSizes::newFilterInstance(bool copyFilterParameters)
+{
+  FindTriangleGeomSizes::Pointer filter = FindTriangleGeomSizes::New();
+  if(true == copyFilterParameters)
+  {
+    copyFilterParameterInstanceVariables(filter.get());
+  }
+  return filter;
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+const QString FindTriangleGeomSizes::getCompiledLibraryName()
+{ return SurfaceMeshingConstants::SurfaceMeshingBaseName; }
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+const QString FindTriangleGeomSizes::getBrandingString()
+{ return "SurfaceMeshing"; }
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+const QString FindTriangleGeomSizes::getFilterVersion()
+{
+  QString version;
+  QTextStream vStream(&version);
+  vStream <<  SurfaceMeshing::Version::Major() << "." << SurfaceMeshing::Version::Minor() << "." << SurfaceMeshing::Version::Patch();
+  return version;
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+const QString FindTriangleGeomSizes::getGroupName()
+{ return SIMPL::FilterGroups::StatisticsFilters; }
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+const QString FindTriangleGeomSizes::getSubGroupName()
+{ return SIMPL::FilterSubGroups::MorphologicalFilters; }
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+const QString FindTriangleGeomSizes::getHumanLabel()
+{ return "Find Feature Volumes from Triangle Geometry"; }
+
---
+++ Source/Plugins/SurfaceMeshing/Test/FindTriangleGeomSizesTest.cpp
@@ -0,0 +1,289 @@
+/* ============================================================================
+* Copyright (c) 2009-2016 BlueQuartz Software, LLC
+*
+* Redistribution and use in source and binary forms, with or without modification,
+* are permitted provided that the following conditions are met:
+*
+* Redistributions of source code must retain the above copyright notice, this
+* list of conditions and the following disclaimer.
+*
+* Redistributions in binary form must reproduce the above copyright notice, this
+* list of conditions and the following disclaimer in the documentation and/or
+* other materials provided with the distribution.
+*
+* Neither the name of BlueQuartz Software, the US Air Force, nor the names of its
+* contributors may be used to endorse or promote products derived from this software
+* without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+* USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+* The code contained herein was partially funded by the followig contracts:
+*    United States Air Force Prime Contract FA8650-07-D-5800
+*    United States Air Force Prime Contract FA8650-10-D-5210
+*    United States Prime Contract Navy N00173-07-C-2068
+*
+* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+#include <QtCore/QCoreApplication>
+#include <QtCore/QFile>
+
+#include "SIMPLib/SIMPLib.h"
+#include "SIMPLib/Common/SIMPLibSetGetMacros.h"
+#include "SIMPLib/DataArrays/DataArray.hpp"
+#include "SIMPLib/Common/FilterPipeline.h"
+#include "SIMPLib/Common/FilterManager.h"
+#include "SIMPLib/Common/FilterFactory.hpp"
+#include "SIMPLib/Geometry/TriangleGeom.h"
+#include "SIMPLib/Plugin/ISIMPLibPlugin.h"
+#include "SIMPLib/Plugin/SIMPLibPluginLoader.h"
+#include "SIMPLib/Utilities/UnitTestSupport.hpp"
+#include "SIMPLib/Utilities/QMetaObjectUtilities.h"
+
+#include "SurfaceMeshingTestFileLocations.h"
+
+class FindTriangleGeomSizesTest
+{
+
+  public:
+    FindTriangleGeomSizesTest() {}
+    virtual ~FindTriangleGeomSizesTest() {}
+
+
+  // -----------------------------------------------------------------------------
+  //
+  // -----------------------------------------------------------------------------
+  void RemoveTestFiles()
+  {
+  #if REMOVE_TEST_FILES
+    QFile::remove(UnitTest::FindTriangleGeomSizesTest::TestFile1);
+    QFile::remove(UnitTest::FindTriangleGeomSizesTest::TestFile2);
+  #endif
+  }
+
+  // -----------------------------------------------------------------------------
+  //
+  // -----------------------------------------------------------------------------
+  int TestFilterAvailability()
+  {
+    // Now instantiate the FindTriangleGeomSizesTest Filter from the FilterManager
+    QString filtName = "FindTriangleGeomSizes";
+    FilterManager* fm = FilterManager::Instance();
+    IFilterFactory::Pointer filterFactory = fm->getFactoryForFilter(filtName);
+    if (nullptr == filterFactory.get())
+    {
+      std::stringstream ss;
+      ss << "The SurfaceMeshing Requires the use of the " << filtName.toStdString() << " filter which is found in the SurfaceMeshing Plugin";
+      DREAM3D_TEST_THROW_EXCEPTION(ss.str())
+    }
+    return 0;
+  }
+
+  // -----------------------------------------------------------------------------
+  //
+  // -----------------------------------------------------------------------------
+  int TestFindTriangleGeomSizesTest()
+  {
+    DataContainerArray::Pointer dca = DataContainerArray::New();
+
+    DataContainer::Pointer tdc = DataContainer::New(SIMPL::Defaults::TriangleDataContainerName);
+    dca->addDataContainer(tdc);
+
+    // Basic idea is to create a surface mesh of a rectangular prism with edge lengths of 3x1x1 for a
+    // total volume of 3, where the entire enclosed volume represents one feature;
+    // triangle windings are mixed to make sure the filter is properly flipping
+    // windings where necessary to ensure consistency
+    SharedVertexList::Pointer vertex = TriangleGeom::CreateSharedVertexList(8);
+    TriangleGeom::Pointer triangle = TriangleGeom::CreateGeometry(12, vertex, SIMPL::Geometry::TriangleGeometry);
+    tdc->setGeometry(triangle);
+    float* vertices = triangle->getVertexPointer(0);
+    int64_t* tris = triangle->getTriPointer(0);
+
+    vertices[3 * 0 + 0] = -1.0f;
+    vertices[3 * 0 + 1] =  0.0f;
+    vertices[3 * 0 + 2] =  0.0f;
+
+    vertices[3 * 1 + 0] = -1.0f;
+    vertices[3 * 1 + 1] =  0.0f;
+    vertices[3 * 1 + 2] =  1.0f;
+
+    vertices[3 * 2 + 0] = -1.0f;
+    vertices[3 * 2 + 1] =  1.0f;
+    vertices[3 * 2 + 2] =  1.0f;
+
+    vertices[3 * 3 + 0] = -1.0f;
+    vertices[3 * 3 + 1] =  1.0f;
+    vertices[3 * 3 + 2] =  0.0f;
+
+    vertices[3 * 4 + 0] =  2.0f;
+    vertices[3 * 4 + 1] =  0.0f;
+    vertices[3 * 4 + 2] =  0.0f;
+
+    vertices[3 * 5 + 0] =  2.0f;
+    vertices[3 * 5 + 1] =  0.0f;
+    vertices[3 * 5 + 2] =  1.0f;
+
+    vertices[3 * 6 + 0] =  2.0f;
+    vertices[3 * 6 + 1] =  1.0f;
+    vertices[3 * 6 + 2] =  1.0f;
+
+    vertices[3 * 7 + 0] =  2.0f;
+    vertices[3 * 7 + 1] =  1.0f;
+    vertices[3 * 7 + 2] =  0.0f;
+
+    tris[3 * 0 + 0] = 0;
+    tris[3 * 0 + 1] = 1;
+    tris[3 * 0 + 2] = 3;
+
+    tris[3 * 1 + 0] = 1;
+    tris[3 * 1 + 1] = 2;
+    tris[3 * 1 + 2] = 3;
+
+    tris[3 * 2 + 0] = 0;
+    tris[3 * 2 + 1] = 1;
+    tris[3 * 2 + 2] = 5;
+
+    tris[3 * 3 + 0] = 0;
+    tris[3 * 3 + 1] = 4;
+    tris[3 * 3 + 2] = 5;
+
+    tris[3 * 4 + 0] = 5;
+    tris[3 * 4 + 1] = 6;
+    tris[3 * 4 + 2] = 4;
+
+    tris[3 * 5 + 0] = 4;
+    tris[3 * 5 + 1] = 7;
+    tris[3 * 5 + 2] = 6;
+
+    tris[3 * 6 + 0] = 2;
+    tris[3 * 6 + 1] = 3;
+    tris[3 * 6 + 2] = 7;
+
+    tris[3 * 7 + 0] = 7;
+    tris[3 * 7 + 1] = 6;
+    tris[3 * 7 + 2] = 2;
+
+    tris[3 * 8 + 0] = 1;
+    tris[3 * 8 + 1] = 2;
+    tris[3 * 8 + 2] = 6;
+
+    tris[3 * 9 + 0] = 6;
+    tris[3 * 9 + 1] = 5;
+    tris[3 * 9 + 2] = 1;
+
+    tris[3 * 10 + 0] = 0;
+    tris[3 * 10 + 1] = 4;
+    tris[3 * 10 + 2] = 7;
+
+    tris[3 * 11 + 0] = 7;
+    tris[3 * 11 + 1] = 3;
+    tris[3 * 11 + 2] = 0;
+
+    QVector<size_t> tDims(1, 12);
+
+    AttributeMatrix::Pointer faceAttrMat = AttributeMatrix::New(tDims, SIMPL::Defaults::FaceAttributeMatrixName, SIMPL::AttributeMatrixType::Face);
+    tdc->addAttributeMatrix(SIMPL::Defaults::FaceAttributeMatrixName, faceAttrMat);
+    QVector<size_t> cDims(1, 2);
+    Int32ArrayType::Pointer faceLabels = Int32ArrayType::CreateArray(12, cDims, SIMPL::FaceData::SurfaceMeshFaceLabels);
+    faceAttrMat->addAttributeArray(SIMPL::FaceData::SurfaceMeshFaceLabels, faceLabels);
+    int32_t* faceLabelsPtr = faceLabels->getPointer(0);
+
+    faceLabelsPtr[2 * 0 + 0] = -1;
+    faceLabelsPtr[2 * 0 + 1] =  1;
+
+    faceLabelsPtr[2 * 1 + 0] = -1;
+    faceLabelsPtr[2 * 1 + 1] =  1;
+
+    faceLabelsPtr[2 * 2 + 0] =  1;
+    faceLabelsPtr[2 * 2 + 1] = -1;
+
+    faceLabelsPtr[2 * 3 + 0] = -1;
+    faceLabelsPtr[2 * 3 + 1] =  1;
+
+    faceLabelsPtr[2 * 4 + 0] =  1;
+    faceLabelsPtr[2 * 4 + 1] = -1;
+
+    faceLabelsPtr[2 * 5 + 0] = -1;
+    faceLabelsPtr[2 * 5 + 1] =  1;
+
+    faceLabelsPtr[2 * 6 + 0] =  1;
+    faceLabelsPtr[2 * 6 + 1] = -1;
+
+    faceLabelsPtr[2 * 7 + 0] =  1;
+    faceLabelsPtr[2 * 7 + 1] = -1;
+
+    faceLabelsPtr[2 * 8 + 0] =  1;
+    faceLabelsPtr[2 * 8 + 1] = -1;
+
+    faceLabelsPtr[2 * 9 + 0] =  1;
+    faceLabelsPtr[2 * 9 + 1] = -1;
+
+    faceLabelsPtr[2 * 10 + 0] =  1;
+    faceLabelsPtr[2 * 10 + 1] = -1;
+
+    faceLabelsPtr[2 * 11 + 0] =  1;
+    faceLabelsPtr[2 * 11 + 1] = -1;
+
+    QString filtName = "FindTriangleGeomSizes";
+    FilterManager* fm = FilterManager::Instance();
+    IFilterFactory::Pointer factory = fm->getFactoryForFilter(filtName);
+    DREAM3D_REQUIRE(factory.get() != nullptr)
+
+    AbstractFilter::Pointer sizeFilter = factory->create();
+    DREAM3D_REQUIRE(sizeFilter.get() != nullptr)
+
+    sizeFilter->setDataContainerArray(dca);
+
+    bool propWasSet = true;
+    QVariant var;
+
+    DataArrayPath path(SIMPL::Defaults::TriangleDataContainerName, SIMPL::Defaults::FaceAttributeMatrixName, SIMPL::FaceData::SurfaceMeshFaceLabels);
+    var.setValue(path);
+    propWasSet = sizeFilter->setProperty("FaceLabelsArrayPath", var);
+    if(!propWasSet)
+    {
+      qDebug() << "Unable to set property FaceLabelsArrayPath";
+    }
+
+    sizeFilter->execute();
+    int32_t err = sizeFilter->getErrorCondition();
+    DREAM3D_REQUIRE_EQUAL(err, 0);
+
+    AttributeMatrix::Pointer faceFeatAttrMat = tdc->getAttributeMatrix(SIMPL::Defaults::FaceFeatureAttributeMatrixName);
+    FloatArrayType::Pointer volumes = faceFeatAttrMat->getAttributeArrayAs<FloatArrayType>(SIMPL::FeatureData::Volumes);
+
+    DREAM3D_REQUIRE_EQUAL(volumes->getNumberOfTuples(), 2);
+    DREAM3D_REQUIRE_EQUAL(volumes->getValue(1), 3.0f);
+
+    return EXIT_SUCCESS;
+  }
+
+  // -----------------------------------------------------------------------------
+  //
+  // -----------------------------------------------------------------------------
+  void operator()()
+  {
+    int err = EXIT_SUCCESS;
+
+    DREAM3D_REGISTER_TEST( TestFilterAvailability() );
+
+    DREAM3D_REGISTER_TEST( TestFindTriangleGeomSizesTest() )
+
+    DREAM3D_REGISTER_TEST( RemoveTestFiles() )
+  }
+
+  private:
+    FindTriangleGeomSizesTest(const FindTriangleGeomSizesTest&); // Copy Constructor Not Implemented
+    void operator=(const FindTriangleGeomSizesTest&); // Operator '=' Not Implemented
+
+
+};
+
