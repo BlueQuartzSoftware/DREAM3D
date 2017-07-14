93bced8d086d858e56b6a6ec4d6cf8d7ee4be704
Michael Jackson <mike.jackson@bluequartz.net>
2017-07-14 11:08:51 -0400

    CPACK: Adjusting how some of the scripts are created and packaged for generating shell script to upload binaries to the web server.

--- Resources/CPack/PackageProject.cmake
+++ Resources/CPack/PackageProject.cmake
@@ -107,9 +107,10 @@ else()
 endif()
 
 
-set(DREAM3D_WEBSITE_SERVER "thor.bluequartz.net")
-set(DREAM3D_WEBSITE_SERVER_PATH "/var/www/dream3d-wp.bluequartz.net")
-set(DREAM3D_WEBSITE_SCP_USERNAME "mjackson")
+set(DREAM3D_WEBSITE_SERVER "")
+set(DREAM3D_WEBSITE_SERVER_PATH "")
+set(DREAM3D_WEBSITE_SCP_USERNAME "")
+
 #-- Create a bash script file that will upload the latest version to the web server
 configure_file(${PROJECT_RESOURCES_DIR}/upload.sh.in
             ${PROJECT_BINARY_DIR}/upload.sh)
@@ -177,12 +178,6 @@ SET(CPACK_SOURCE_PACKAGE_FILE_NAME "DREAM3D-${DREAM3D_VERSION_SHORT}-Source")
 
 #-- Create a bash script file that will upload the latest version to the web server
 set(UPLOAD_FILE_NAME ${CPACK_SOURCE_PACKAGE_FILE_NAME}.tar.gz)
-configure_file(${PROJECT_RESOURCES_DIR}/upload.sh.in
-               ${PROJECT_BINARY_DIR}/src_upload.sh)
-
-#-- Create a bash script file that will upload a version file to the server
-configure_file(${PROJECT_RESOURCES_DIR}/version_upload.sh.in
-               ${PROJECT_BINARY_DIR}/version_upload.sh)
 
 SET(CPACK_SOURCE_TOPLEVEL_TAG "Source")
 SET(CPACK_IGNORE_FILES "/i386/;/x64/;/VS2008/;/zRel/;/Build/;/\\\\.git/;\\\\.*project")
