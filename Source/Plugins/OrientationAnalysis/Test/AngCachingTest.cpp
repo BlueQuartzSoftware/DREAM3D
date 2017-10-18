9ec2a7ab914cc87f4093a5da6c6cb6da6bc50e46
Michael Jackson <mike.jackson@bluequartz.net>
2017-10-18 09:04:43 -0400

    Actually spell the company name correctly.

--- Source/Plugins/OrientationAnalysis/Test/AngCachingTest.cpp
+++ Source/Plugins/OrientationAnalysis/Test/AngCachingTest.cpp
@@ -1,5 +1,5 @@
 /* ============================================================================
- * Copyright (c) 2015 BlueQuartz Softwae, LLC
+ * Copyright (c) 2015 BlueQuartz Software, LLC
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
