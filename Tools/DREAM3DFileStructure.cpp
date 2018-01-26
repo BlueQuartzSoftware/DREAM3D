3b1f3bfd7c1128ff5cca95d0e38d46ec0f9cee9f
Michael Jackson <mike.jackson@bluequartz.net>
2018-01-26 12:32:08 -0500

    Update class names to latest API and filename changes in SIMPL

--- Tools/DREAM3DFileStructure.cpp
+++ Tools/DREAM3DFileStructure.cpp
@@ -10,7 +10,7 @@
 #include "H5Support/H5Support.h"
 #include "H5Support/QH5Lite.h"
 #include "H5Support/QH5Utilities.h"
-#include "H5Support/HDF5ScopedFileSentinel.h"
+#include "H5Support/H5ScopedSentinel.h"
 
 #include "SIMPLib/Common/Constants.h"
 #include "SIMPLib/DataContainers/DataArrayProxy.h"
