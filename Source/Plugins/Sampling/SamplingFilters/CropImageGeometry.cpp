4d2a324892e60b5f2f38aacc69e227b27617e09c
Michael Jackson <mike.jackson@bluequartz.net>
2018-04-11 13:31:16 -0400

    Crop Volume (Image) needs to have a cancel check in the inner cropping loop.
    Fixes #784. Closes #784

--- Source/Plugins/Sampling/SamplingFilters/CropImageGeometry.cpp
+++ Source/Plugins/Sampling/SamplingFilters/CropImageGeometry.cpp
@@ -486,6 +486,7 @@ void CropImageGeometry::execute()
   QList<QString> voxelArrayNames = cellAttrMat->getAttributeArrayNames();
   for(int64_t i = 0; i < ZP; i++)
   {
+    if(getCancel()) { break; }
     QString ss = QObject::tr("Cropping Volume || Slice %1 of %2 Complete").arg(i).arg(ZP);
     notifyStatusMessage(getMessagePrefix(), getHumanLabel(), ss);
     planeold = (i + m_ZMin) * (srcCellDataContainer->getGeometryAs<ImageGeom>()->getXPoints() * srcCellDataContainer->getGeometryAs<ImageGeom>()->getYPoints());
@@ -508,6 +509,7 @@ void CropImageGeometry::execute()
       }
     }
   }
+  if(getCancel()) { return; }
   destCellDataContainer->getGeometryAs<ImageGeom>()->setDimensions(static_cast<size_t>(XP), static_cast<size_t>(YP), static_cast<size_t>(ZP));
   totalPoints = destCellDataContainer->getGeometryAs<ImageGeom>()->getNumberOfElements();
   QVector<size_t> tDims(3, 0);
@@ -553,6 +555,8 @@ void CropImageGeometry::execute()
     // Find the unique set of feature ids
     for(int64_t i = 0; i < totalPoints; ++i)
     {
+      if(getCancel()) { break; }
+      
       int32_t currentFeatureId = m_FeatureIds[i];
       if(currentFeatureId < totalFeatures)
       {
