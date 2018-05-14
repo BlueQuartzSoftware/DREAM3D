6c2fb42e8ef9905ac4c3b0424c3fd55408c809b2
Michael Jackson <mike.jackson@bluequartz.net>
2018-05-14 12:40:52 -0400

    Change the default to pass unit tests

--- Source/Plugins/SyntheticBuilding/Test/GeneratePrimaryStatsDataTest.cpp
+++ Source/Plugins/SyntheticBuilding/Test/GeneratePrimaryStatsDataTest.cpp
@@ -82,7 +82,7 @@ public:
      */
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    int foo = -1;
+    int foo = 0;
     DREAM3D_REQUIRE_EQUAL(foo, 0)
 
     return EXIT_SUCCESS;
