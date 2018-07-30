fd5f6b234faa723f2fcccf5349bba246d1b050f4
Michael Jackson <mike.jackson@bluequartz.net>
2018-07-30 11:57:26 -0400

    Updating code to use static string constants. Validating output path.

--- Tools/RESTServerInfo/RESTServerInfo.cpp
+++ Tools/RESTServerInfo/RESTServerInfo.cpp
@@ -21,99 +21,115 @@
 
 #include "DREAM3DToolsConfiguration.h"
 
+#include "RestServerConstants.h"
+
 class RESTServerInfo
 {
-
-public:
-  RESTServerInfo(const QString filePath)
-  : m_FilePath(filePath)
-  {
-  }
-
-  ~RESTServerInfo() = default;
-  RESTServerInfo(const RESTServerInfo&) = delete;            // Copy Constructor Not Implemented
-  RESTServerInfo(RESTServerInfo&&) = delete;                 // Move Constructor
-  RESTServerInfo& operator=(const RESTServerInfo&) = delete; // Copy Assignment Not Implemented
-  RESTServerInfo& operator=(RESTServerInfo&&) = delete;      // Move Assignment Not Implemented
-
-  /**
+    
+  public:
+    RESTServerInfo(const QString filePath)
+      : m_FilePath(filePath)
+    {
+    }
+    
+    ~RESTServerInfo() = default;
+    RESTServerInfo(const RESTServerInfo&) = delete;            // Copy Constructor Not Implemented
+    RESTServerInfo(RESTServerInfo&&) = delete;                 // Move Constructor Not Implemented
+    RESTServerInfo& operator=(const RESTServerInfo&) = delete; // Copy Assignment Not Implemented
+    RESTServerInfo& operator=(RESTServerInfo&&) = delete;      // Move Assignment Not Implemented
+    
+    /**
    * @brief execute
    */
-  void execute()
-  {
-    PluginManager* pm = PluginManager::Instance();
-    QVector<ISIMPLibPlugin*> plugins = pm->getPluginsVector();
-
-    // Write our File Version and DREAM3D Version strings
-    QJsonObject dream3dMeta;
-
-    dream3dMeta["Release Date"] = QDate::currentDate().toString();
-    dream3dMeta["Release Type"] = SIMPLProj_RELEASE_TYPE;
-    dream3dMeta["Major"] = SIMPLView::Version::Major();
-    dream3dMeta["Minor"] = SIMPLView::Version::Minor();
-    dream3dMeta["Patch"] = SIMPLView::Version::Patch();
-    dream3dMeta["Revision"] = SIMPLView::Version::Revision();
-    dream3dMeta["Download WebSite"] = "http://dream3d.bluequartz.net/?page_id=32";
-    QJsonObject m_Root;
-    m_Root["DREAM3D"] = dream3dMeta;
-
-    QJsonObject simplMeta;
-    simplMeta["Major"] = SIMPLib::Version::Major();
-    simplMeta["Minor"] = SIMPLib::Version::Minor();
-    simplMeta["Patch"] = SIMPLib::Version::Patch();
-    simplMeta["Revision"] = SIMPLib::Version::Revision();
-    m_Root["SIMPL"] = simplMeta;
-
-    QJsonArray plugArray;
-
-    for(int i = 0; i < plugins.size(); i++)
+    void execute()
     {
-      ISIMPLibPlugin* plug = plugins[i];
-      QJsonObject jobj;
-      jobj["Base Name"] = plug->getPluginBaseName();
-      jobj["Version"] = plug->getVersion();
-      jobj["Vendor"] = plug->getVendor();
-      jobj["Compatibility Version"] = plug->getCompatibilityVersion();
-      jobj["Display Name"] = plug->getPluginDisplayName();
-      jobj["URL"] = plug->getURL();
-      plugArray.append(jobj);
+      PluginManager* pm = PluginManager::Instance();
+      QVector<ISIMPLibPlugin*> plugins = pm->getPluginsVector();
+      
+      // Write our File Version and DREAM3D Version strings
+      QJsonObject dream3dMeta;
+      
+      dream3dMeta[RestServer::Strings::ReleaseDate] = QDate::currentDate().toString();
+      dream3dMeta[RestServer::Strings::ReleaseType] = SIMPLProj_RELEASE_TYPE;
+      dream3dMeta[RestServer::Strings::MajorVersion] = SIMPLView::Version::Major();
+      dream3dMeta[RestServer::Strings::MinorVersion] = SIMPLView::Version::Minor();
+      dream3dMeta[RestServer::Strings::PatchVersion] = SIMPLView::Version::Patch();
+      dream3dMeta[RestServer::Strings::Revision] = SIMPLView::Version::Revision();
+      dream3dMeta[RestServer::Strings::DownloadURL] = "http://dream3d.bluequartz.net/?page_id=32";
+      QJsonObject m_Root;
+      m_Root[RestServer::Strings::DREAM3D] = dream3dMeta;
+      
+      QJsonObject simplMeta;
+      simplMeta[RestServer::Strings::MajorVersion] = SIMPLib::Version::Major();
+      simplMeta[RestServer::Strings::MinorVersion] = SIMPLib::Version::Minor();
+      simplMeta[RestServer::Strings::PatchVersion] = SIMPLib::Version::Patch();
+      simplMeta[RestServer::Strings::Revision] = SIMPLib::Version::Revision();
+      m_Root[RestServer::Strings::SIMPL] = simplMeta;
+      
+      QJsonArray plugArray;
+      
+      for(int i = 0; i < plugins.size(); i++)
+      {
+        ISIMPLibPlugin* plug = plugins[i];
+        QJsonObject jobj;
+        jobj[RestServer::Strings::BaseName] = plug->getPluginBaseName();
+        jobj[RestServer::Strings::Version] = plug->getVersion();
+        jobj[RestServer::Strings::Vendor] = plug->getVendor();
+        jobj[RestServer::Strings::CompatibilityVersion] = plug->getCompatibilityVersion();
+        jobj[RestServer::Strings::DisplayName] = plug->getPluginDisplayName();
+        jobj[RestServer::Strings::URL] = plug->getURL();
+        plugArray.append(jobj);
+      }
+      m_Root[RestServer::Strings::Plugins] = plugArray;
+      
+      // Loop over all the filters
+      FilterManager* fm = FilterManager::Instance();
+      QJsonArray filterArray;
+      
+      FilterManager::Collection factories = fm->getFactories();
+      for(auto factory : factories)
+      {
+        AbstractFilter::Pointer filter = factory->create();
+        QJsonObject filtJson;
+        
+        filtJson[RestServer::Strings::Name] = filter->getHumanLabel();
+        filtJson[RestServer::Strings::ClassName] = filter->getNameOfClass();
+        filtJson[RestServer::Strings::Uuid] = filter->getUuid().toString();
+        filtJson[RestServer::Strings::PluginName] = filter->getCompiledLibraryName();
+        filtJson[RestServer::Strings::Version] = filter->getFilterVersion();
+        filtJson[RestServer::Strings::GroupName] = filter->getGroupName();
+        filtJson[RestServer::Strings::SubGroupName] = filter->getSubGroupName();
+        
+        filterArray.append(filtJson);
+      }
+      
+      m_Root[RestServer::Strings::Filters] = filterArray;
+      
+      QJsonDocument doc(m_Root);
+      
+      // Make sure any directory path is also available as the user may have just typed
+      // in a path without actually creating the full path
+      QFileInfo fi(m_FilePath);
+      QString parentPath = fi.path();
+      QDir dir;
+      if(!dir.mkpath(parentPath))
+      {
+        QString ss = QObject::tr("Error creating parent path '%1'").arg(parentPath);
+        qDebug() << ss;
+        return;
+      }
+      
+      
+      QFile outputFile(m_FilePath);
+      if(outputFile.open(QIODevice::WriteOnly))
+      {
+        outputFile.write(doc.toJson());
+        outputFile.close();
+      }
     }
-    m_Root["Plugins"] = plugArray;
-
-    // Loop over all the filters
-    FilterManager* fm = FilterManager::Instance();
-    QJsonArray filterArray;
-
-    FilterManager::Collection factories = fm->getFactories();
-    for(auto factory : factories)
-    {
-      AbstractFilter::Pointer filter = factory->create();
-      QJsonObject filtJson;
-
-      filtJson["Name"] = filter->getHumanLabel();
-      filtJson["ClassName"] = filter->getNameOfClass();
-      filtJson["Uuid"] = filter->getUuid().toString();
-      filtJson["Plugin Name"] = filter->getCompiledLibraryName();
-      filtJson["Version"] = filter->getFilterVersion();
-      filtJson["Group Name"] = filter->getGroupName();
-      filtJson["SubGroup Name"] = filter->getSubGroupName();
-
-      filterArray.append(filtJson);
-    }
-
-    m_Root["Filters"] = filterArray;
-
-    QJsonDocument doc(m_Root);
-    QFile outputFile(m_FilePath);
-    if(outputFile.open(QIODevice::WriteOnly))
-    {
-      outputFile.write(doc.toJson());
-      outputFile.close();
-    }
-  }
-
-private:
-  QString m_FilePath;
+    
+  private:
+    QString m_FilePath;
 };
 
 // -----------------------------------------------------------------------------
@@ -132,26 +148,26 @@ int main(int argc, char** argv)
   QCoreApplication app(argc, argv);
   QCoreApplication::setOrganizationName("BlueQuartz Software");
   QCoreApplication::setOrganizationDomain("bluequartz.net");
-  QCoreApplication::setApplicationName("FilterInfoUtility");
-
+  QCoreApplication::setApplicationName("RESTServerInfo");
+  
   if(argc != 2)
   {
     std::cout << "Program requires 1 argument:" << std::endl;
     std::cout << "   [1] Output File: Path (relative or absolute) to the output file." << std::endl;
     return EXIT_FAILURE;
   }
-
+  
   // Register all the filters including trying to load those from Plugins
   FilterManager* fm = FilterManager::Instance();
   // Load all the plugins
   SIMPLibPluginLoader::LoadPluginFilters(fm);
-
+  
   QString filePath = QString::fromLatin1(argv[1]);
   // Send progress messages from PipelineBuilder to this object for display
   RESTServerInfo rsi(filePath);
   rsi.execute();
-
+  
   int err = EXIT_SUCCESS;
-
+  
   return err;
 }
