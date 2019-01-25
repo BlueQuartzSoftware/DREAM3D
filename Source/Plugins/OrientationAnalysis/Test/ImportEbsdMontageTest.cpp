7d6c21e462ee05c455d17bd90ecaee2ea67b598d
Nathan Blair <nathan.blair@bluequartz.net>
2019-01-25 14:49:25 -0500

    Added license to Import filter unit test
    Signed-off-by: Nathan Blair <nathan.blair@bluequartz.net>

--- Source/Plugins/OrientationAnalysis/Test/ImportEbsdMontageTest.cpp
+++ Source/Plugins/OrientationAnalysis/Test/ImportEbsdMontageTest.cpp
@@ -1,5 +1,36 @@
-#pragma once
+/* ============================================================================
+ * Copyright (c) 2019 BlueQuartz Software, LLC
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the names of any of the BlueQuartz Software contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
+#pragma once
 
 #include <QtCore/QCoreApplication>
 #include <QtCore/QFile>
@@ -23,11 +54,6 @@ class ImportEbsdMontageTest
   AbstractFilter::Pointer m_EBSDMontageFilter{nullptr};
   const QString m_filtName{"ImportEbsdMontage"};
 
-  ImportEbsdMontageTest(const ImportEbsdMontageTest&) = delete;            // Copy Constructor
-  ImportEbsdMontageTest(ImportEbsdMontageTest&&) = delete;                 // Move Constructor
-  ImportEbsdMontageTest& operator=(const ImportEbsdMontageTest&) = delete; // Copy Assignment
-  ImportEbsdMontageTest& operator=(ImportEbsdMontageTest&&) = delete;      // Move Assignment
-
   int ValidateData()
   {
     return 0;
@@ -45,14 +71,17 @@ class ImportEbsdMontageTest
 public:
   ImportEbsdMontageTest()
   {
-    IFilterFactory::Pointer ebsdMontageFactory{
-      FilterManager::Instance()->getFactoryFromClassName(m_filtName)
-    };
+    IFilterFactory::Pointer ebsdMontageFactory{FilterManager::Instance()->getFactoryFromClassName(m_filtName)};
     DREAM3D_REQUIRE(ebsdMontageFactory.get() != nullptr);
 
-    m_EBSDMontageFilter =  ebsdMontageFactory->create();
+    m_EBSDMontageFilter = ebsdMontageFactory->create();
     DREAM3D_REQUIRE(m_EBSDMontageFilter.get() != nullptr);
   }
+  ImportEbsdMontageTest(const ImportEbsdMontageTest&) = delete;            // Copy Constructor
+  ImportEbsdMontageTest(ImportEbsdMontageTest&&) = delete;                 // Move Constructor
+  ImportEbsdMontageTest& operator=(const ImportEbsdMontageTest&) = delete; // Copy Assignment
+  ImportEbsdMontageTest& operator=(ImportEbsdMontageTest&&) = delete;      // Move Assignment
+
   ~ImportEbsdMontageTest() = default;
 
   int SetUp()
