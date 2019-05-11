e01ffcdd7f16017dd0ec35a1438569742902508e
Michael Jackson <mike.jackson@bluequartz.net>
2019-05-11 16:56:07 -0400

    Enable both Visual Studio and Ninja Unit Tests

--- Test/ctest_pipeline_driver.bat
+++ Test/ctest_pipeline_driver.bat
@@ -1,7 +1,42 @@
-@echo off
 
+@echo off
+:: Check to see if we were built using the Visual Studio generators, if so
+:: then we need an additional argument to tell this script which configuration
+:: directory to use as the source for the compiled files.
+:: set CONFIG_DIR=%CMAKE_CONFIG_TYPE%
+IF "@MSVC_IDE@" == "1" (
+    set CONFIG_DIR=%CMAKE_CONFIG_TYPE%
+) else (
+    set CONFIG_DIR=
+) 
+
+::-----------------------------------------------------------------------------
+IF "%CMAKE_CONFIG_TYPE%" == "Debug" (
+    set EXE_SFX="_debug"
+)
+
+::-----------------------------------------------------------------------------
+IF "%CMAKE_CONFIG_TYPE%" == "Release" (
+    set EXE_SFX=""
+)
+
+::-----------------------------------------------------------------------------
+IF "@CMAKE_BUILD_TYPE@" == "Debug" (
+    set CONFIG_DIR=
+    set EXE_SFX="_debug"
+)
+
+::-----------------------------------------------------------------------------
+IF "@CMAKE_BUILD_TYPE@" == "Release" (
+    set CONFIG_DIR=
+    set EXE_SFX=""
+)
+
+echo on
 @echo "[@test@.json] Prebuilt Pipeline Test Starting"
 
-cd @CMAKE_RUNTIME_OUTPUT_DIRECTORY@
+@echo "Running Executable at @CMAKE_RUNTIME_OUTPUT_DIRECTORY@\%CONFIG_DIR%\PipelineRunner%EXE_SFX%@EXE_EXT@"
 
-PipelineRunner@EXE_EXT@ --pipeline "@DREAM3D_SUPPORT_DIR@/@test@.json"
+cd @CMAKE_RUNTIME_OUTPUT_DIRECTORY@\%CONFIG_DIR%
+
+PipelineRunner%EXE_SFX%@EXE_EXT@ --pipeline "@DREAM3D_SUPPORT_DIR@/@test@.json"
