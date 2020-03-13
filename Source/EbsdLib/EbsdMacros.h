8ef0c0197002f335c8fed670d9cb634ea176d8db
Michael Jackson <mike.jackson@bluequartz.net>
2020-03-13 16:10:37 -0400

    Updating backwards compatibility macros for Qt5.9 through 5.14
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- Source/EbsdLib/EbsdMacros.h
+++ Source/EbsdLib/EbsdMacros.h
@@ -41,6 +41,43 @@
 
 #include <QtCore/QtGlobal>
 
+#if QT_VERSION < QT_VERSION_CHECK(5, 11, 0)
+#define QFONTMETRICS_WIDTH width
+#else
+#define QFONTMETRICS_WIDTH horizontalAdvance
+#endif
+
+#if QT_VERSION < QT_VERSION_CHECK(5, 14, 0)
+
+#ifndef QVECTOR_FROM_STD_VECTOR
+#define QVECTOR_FROM_STD_VECTOR(def, qvector, other) def qvector = def::fromStdVector(other);
+#endif
+
+#ifndef QVECTOR_TO_STD_VECTOR
+#define QVECTOR_TO_STD_VECTOR(def, qvector, other) def other = qvector.toStdVector();
+#endif
+
+#ifndef QLIST_FROM_QSET
+#define QLIST_FROM_QSET(def, qlist, other) def qlist = other.toList();
+#endif
+
+#else
+
+#ifndef QVECTOR_FROM_STD_VECTOR
+#define QVECTOR_FROM_STD_VECTOR(def, qvecVarName, svector) def qvecVarName(svector.begin(), svector.end());
+#endif
+
+#ifndef QVECTOR_TO_STD_VECTOR
+#define QVECTOR_TO_STD_VECTOR(def, qvecVarName, svector) def svector(qvecVarName.begin(), qvecVarName.end());
+#endif
+
+#ifndef QLIST_FROM_QSET
+#define QLIST_FROM_QSET(def, qlist, other) def qlist = other.toList();
+#endif
+
+#endif
+
+
 /**
  * @brief These macros are used to read header values from an HDF5 file, NOT From a .ang or .ctf file
  */
@@ -134,18 +171,6 @@
     }                                                                                                                                                                                                  \
     phase->set##key(static_cast<cast>(t));                                                                                                                                                             \
   }
-
-#if QT_VERSION < QT_VERSION_CHECK(5, 10, 0)
-
-#ifndef QVECTOR_FROM_STD_VECTOR
-#define QVECTOR_FROM_STD_VECTOR(def, qvector, svector) def qvector = def::fromStdVector(svector);
-#endif
-#else
-#ifndef QVECTOR_FROM_STD_VECTOR
-#define QVECTOR_FROM_STD_VECTOR(def, qvecVarName, svector) def qvecVarName(svector.begin(), svector.end());
-#endif
-
-#endif
 
 #define READ_PHASE_HEADER_ARRAY(cname, pid, Type, fqKey, key, phase)                                                                                                                                   \
   {                                                                                                                                                                                                    \
