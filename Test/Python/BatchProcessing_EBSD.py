d098584a93463d34fa88e5daaecc07800d7e53bb
Michael Jackson <mike.jackson@bluequartz.net>
2020-05-28 19:16:59 -0400

    Example Python scripts to batch process files or drive a pipeline from Python
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

---
+++ Test/Python/BatchProcessing_EBSD.py
@@ -0,0 +1,83 @@
+'''
+This example shows batch processing of .ang files to generate an IPF Color Image for 
+each slice of data. As each filter is instantiated in the loop the filter is executed. 
+We are not building up a pipeline and then executing that pipeline.
+'''
+
+import numpy as np
+
+import simpl
+import simplpy
+import simpl_helpers as sc
+import simpl_test_dirs as sd
+import orientationanalysispy
+import samplingpy
+import itkimageprocessing
+import itkimageprocessingpy
+
+def batch_process():
+
+    # Each filter is instantiated and executed immediately
+    # The programmer is responsible for providing some sort of feedback to the user of the script
+    for i in range(1,10,1):
+        # Create Data Container Array
+        dca = simpl.DataContainerArray()
+
+        input_file = "{}/Data/SmallIN100/Slice_{}.ang".format(sd.GetDataDirectory(), i)
+        print(f"i:{input_file}")
+        err = orientationanalysispy.read_ang_data(dca, 'EBSD Scan', 'Phase Data', 'Scan Data',
+                                              input_file)
+        if err < 0:
+            print('ReadAngData ErrorCondition: %d' % err)
+
+        # Rotate Sample Reference Frame
+        err = simplpy.rotate_sample_ref_frame(dca, simpl.DataArrayPath('EBSD Scan', 'Scan Data', ''),
+                                                simpl.FloatVec3([0.0, 1.0, 0.0]), 180.0, False, sc.CreateDynamicTableData([[0.0 for x in range(3)] for y in range(3)]), 0)
+        if err < 0:
+            print('RotateSampleRefFrame ErrorCondition: %d' % err)
+
+        # Rotate Euler Reference Frame
+        err = orientationanalysispy.rotate_euler_ref_frame(dca, simpl.FloatVec3([0.0, 0.0, 1.0]), 90.0,
+                                                        simpl.DataArrayPath('EBSD Scan', 'Scan Data', 'EulerAngles'))
+        if err < 0:
+            print('RotateEulerRefFrame ErrorCondition: %d' % err)
+
+        # Threshold Objects
+        err = sc.MultiThresholdObjects(dca, 'Mask', [('EBSD Scan', 'Scan Data', 'Confidence Index', '>', 0.1)])
+        if err < 0:
+            print('MultiThresholdObjects ErrorCondition: %d' % err)
+
+        # Generate IPF Colors
+        err = orientationanalysispy.generate_ipf_colors(dca, simpl.FloatVec3([0, 0, 1]),
+                                                        simpl.DataArrayPath('EBSD Scan', 'Scan Data', 'Phases'),
+                                                        simpl.DataArrayPath('EBSD Scan', 'Scan Data', 'EulerAngles'),
+                                                        simpl.DataArrayPath('EBSD Scan', 'Phase Data', 'CrystalStructures'),
+                                                        True,
+                                                        simpl.DataArrayPath('EBSD Scan', 'Scan Data', 'Mask'), 'IPFColor')
+        if err < 0:
+            print('GenerateIPFColors ErrorCondition: %d' % err)
+
+        # ITK Image Writer
+        image_file = "{}/Data/Output/Examples/Small_IN100_Batch_{}.png".format(sd.GetBuildDirectory(), i)
+
+        image_writer = itkimageprocessing.ITKImageWriter.New()
+        image_writer.registerImageIOFactories()
+        err = itkimageprocessingpy.itk_image_writer(dca, image_file,
+                                                    simpl.DataArrayPath('EBSD Scan', 'Scan Data', 'IPFColor'), 0)
+        if err < 0:
+            print('ITKImageWriter ErrorCondition: %d' % err)
+
+        # Write to DREAM3D file
+        output_file = "{}/Data/Output/Examples/Small_IN100_Batch_{}.dream3d".format(sd.GetBuildDirectory(), i)
+        err = sc.WriteDREAM3DFile(output_file, dca)
+        if err < 0:
+            print('WriteDREAM3DFile ErrorCondition: %d' % err)
+        #----------------------------------------------------------------------
+        # End of Processing Loop
+        #----------------------------------------------------------------------
+
+    print("FINISHED")
+
+
+if __name__ == '__main__':
+    batch_process()
---
+++ Test/Python/CreatePipeline.py
@@ -0,0 +1,106 @@
+'''
+This example shows building up an actual pipeline object with filters, 
+preflighting that pipeline and then executing that pipeline.
+The important note here is that none of the filters run until the pipeline.run()
+is executed.
+'''
+import numpy as np
+
+import simpl
+import simplpy as d3d
+import simpl_helpers as sc
+import simpl_test_dirs as sd
+import orientationanalysis
+import orientationanalysispy
+import itkimageprocessing
+
+def CreatePipeline():
+    pipeline = simpl.FilterPipeline()
+
+    #Create an Observer to report errors/progress from the executing pipeline
+    obs = simpl.Observer()
+    pipeline.addObserver(obs)
+
+    data_container_name =  'EBSD Scan'
+    cell_ensemble_attribute_matrix_name = 'Phase Data'
+    cell_attribute_matrix_name = 'Scan Data'
+    input_file = "{}/Data/SmallIN100/Slice_1.ang".format(sd.GetDataDirectory())
+
+    # Import the EDAX data from an .ang file
+    filter = orientationanalysis.ReadAngData()
+    filter.DataContainerName = data_container_name
+    filter.CellEnsembleAttributeMatrixName = cell_ensemble_attribute_matrix_name
+    filter.CellAttributeMatrixName = cell_attribute_matrix_name
+    filter.InputFile = input_file
+    pipeline.pushBack(filter)
+
+    # Rotate the Sample Reference Frame
+    filter = simpl.RotateSampleRefFrame()
+    filter.CellAttributeMatrixPath = simpl.DataArrayPath(data_container_name, cell_attribute_matrix_name, '')
+    filter.RotationAxis = simpl.FloatVec3([0.0, 1.0, 0.0])
+    filter.RotationAngle = 180.0
+    filter.SliceBySlice = False
+    filter.RotationRepresentationChoice = 0
+    pipeline.pushBack(filter)
+
+    # Rotate the Crystal reference frame
+    filter = orientationanalysis.RotateEulerRefFrame()
+    filter.RotationAxis = simpl.FloatVec3([0.0, 0.0, 1.0])
+    filter.RotationAngle = 90.0
+    filter.CellEulerAnglesArrayPath = simpl.DataArrayPath(data_container_name, cell_attribute_matrix_name, 'EulerAngles')
+    pipeline.pushBack(filter)
+
+    # Threshold the Data by Confidence Index
+    filter = simpl.MultiThresholdObjects2()
+    thresholds = simpl.ComparisonInputsAdvanced()
+    topLevelComparisonSet = simpl.ComparisonSet()
+    comparisonValue = simpl.ComparisonValue()
+    comparisonValue.AttributeArrayName = 'Confidence Index'
+    comparisonValue.CompOperator = 1 # '>'
+    comparisonValue.CompValue =  0.1
+    topLevelComparisonSet.addComparison(comparisonValue)
+    thresholds.addInput(topLevelComparisonSet)
+    thresholds.DataContainerName = data_container_name
+    thresholds.AttributeMatrixName = cell_attribute_matrix_name
+    filter.DestinationArrayName = 'Mask'
+    filter.SelectedThresholds = thresholds
+    pipeline.pushBack(filter)
+
+    # Generate IPF Colors
+    filter = orientationanalysis.GenerateIPFColors()
+    filter.ReferenceDir = simpl.FloatVec3([0, 0, 1])
+    filter.CellPhasesArrayPath = simpl.DataArrayPath(data_container_name, cell_attribute_matrix_name, 'Phases')
+    filter.CellEulerAnglesArrayPath = simpl.DataArrayPath(data_container_name, cell_attribute_matrix_name, 'EulerAngles')
+    filter.CrystalStructuresArrayPath = simpl.DataArrayPath(data_container_name, cell_ensemble_attribute_matrix_name, 'CrystalStructures')
+    filter.UseGoodVoxels = True
+    filter.GoodVoxelsArrayPath = simpl.DataArrayPath(data_container_name, cell_attribute_matrix_name, 'Mask')
+    filter.CellIPFColorsArrayName = 'IPFColor'
+    pipeline.pushBack(filter)
+
+    # ITK Image Writer
+    image_file = "{}/Data/Output/Python_Examples/CreatePipeline/Small_IN100_1.png".format(sd.GetBuildDirectory())
+    filter = itkimageprocessing.ITKImageWriter.New()
+    filter.registerImageIOFactories()
+    filter.FileName = image_file
+    filter.ImageArrayPath = simpl.DataArrayPath(data_container_name, cell_attribute_matrix_name, 'IPFColor')
+    filter.Plane = 0
+    pipeline.pushBack(filter)
+
+    # Write out a .dream3d file which will also save a copy of the pipeline as JSON
+    output_file = "{}/Data/Output/Python_Examples/CreatePipeline/Small_IN100_1.dream3d".format(sd.GetBuildDirectory())
+    filter = simpl.DataContainerWriter.New()
+    filter.OutputFile = output_file
+    pipeline.pushBack(filter)
+
+    # We can preflight the pipeline at this point
+    pipeline.preflightPipeline()
+    print(f'Preflight error = {pipeline.ErrorCode}')
+
+    # We can now run the built up pipeline
+    dca = pipeline.run()
+    print(f'Execution error = {pipeline.ErrorCode}')
+
+if __name__ == '__main__':
+    print('Starting the Pipeline construction example...')
+    CreatePipeline()
+    print('[CreatePipeline] Complete')
