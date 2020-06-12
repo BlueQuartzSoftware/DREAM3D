BlueQuartzSoftware/DREAM3D

commit 1ab713bcdc39c19f51892443936ad0b93aee984e

Convert all orientation operations to double precision where possible (#916)
* Rewrite the OrientationTransform class using more C++ like style
The original version was a direct port of the Fortran codes where the output variable
is passed in as an argument. This rewrite will implement a C++ style of interface
where something is returned instead. This will allow operations to be chained together.

* Converting the Laue Ops classes to use double precision by default
* Updates for the templated MatrixMath class
* Fix color constant and remove unnecessary multiplications in MergeColonies
* Updating Hexagonal ops with some compiler error fixes
* Fix failing unit tests for Quaternion and Orientation Transformation
* Add missing include directive
* Remove test files at end of unit test where possible.

Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

Source/Plugins/OrientationAnalysis/OrientationAnalysisFilters/FindSlipTransmissionMetrics.cpp
@@ -42,6 +42,11 @@
 #include "SIMPLib/FilterParameters/SeparatorFilterParameter.h"
 #include "SIMPLib/FilterParameters/StringFilterParameter.h"
 
+#include "OrientationLib/Core/Orientation.hpp"
+#include "OrientationLib/Core/OrientationTransformation.hpp"
+#include "OrientationLib/Core/Quaternion.hpp"
+#include "OrientationLib/LaueOps/LaueOps.h"
+
 #include "OrientationAnalysis/OrientationAnalysisConstants.h"
 #include "OrientationAnalysis/OrientationAnalysisVersion.h"
 
@@ -68,7 +73,6 @@ FindSlipTransmissionMetrics::FindSlipTransmissionMetrics()
 , m_FeaturePhasesArrayPath("", "", "")
 , m_CrystalStructuresArrayPath("", "", "")
 {
-  m_OrientationOps = LaueOps::getOrientationOpsQVector();
 
   m_F1List = NeighborList<float>::NullPointer();
   m_F1sptList = NeighborList<float>::NullPointer();
@@ -235,6 +239,7 @@ void FindSlipTransmissionMetrics::execute()
   {
     return;
   }
+  QVector<LaueOps::Pointer> m_OrientationOps = LaueOps::getOrientationOpsQVector();
 
   size_t totalFeatures = m_FeaturePhasesPtr.lock()->getNumberOfTuples();
 
@@ -248,13 +253,13 @@ void FindSlipTransmissionMetrics::execute()
   std::vector<std::vector<float>> F7lists;
   std::vector<std::vector<float>> mPrimelists;
 
-  float mprime = 0.0f, F1 = 0.0f, F1spt = 0.0f, F7 = 0.0f;
+  double mprime = 0.0f, F1 = 0.0f, F1spt = 0.0f, F7 = 0.0f;
   int32_t nname;
-  QuatF q1 = QuaternionMathF::New();
-  QuatF q2 = QuaternionMathF::New();
-  QuatF* avgQuats = reinterpret_cast<QuatF*>(m_AvgQuats);
 
-  float LD[3] = {0.0f, 0.0f, 1.0f};
+  // QuatF* avgQuats = reinterpret_cast<QuatF*>(m_AvgQuats);
+  FloatArrayType::Pointer avgQuatPtr = m_AvgQuatsPtr.lock();
+
+  double LD[3] = {0.0f, 0.0f, 1.0f};
 
   F1lists.resize(totalFeatures);
   F1sptlists.resize(totalFeatures);
@@ -270,14 +275,17 @@ void FindSlipTransmissionMetrics::execute()
     for(size_t j = 0; j < neighborlist[i].size(); j++)
     {
       nname = neighborlist[i][j];
-      QuaternionMathF::Copy(avgQuats[i], q1);
-      QuaternionMathF::Copy(avgQuats[nname], q1);
+      float* avgQuat = m_AvgQuats + i * 4;
+      QuatType q1(avgQuat[0], avgQuat[1], avgQuat[2], avgQuat[3]);
+      avgQuat = m_AvgQuats + nname * 4;
+      QuatType q2(avgQuat[0], avgQuat[1], avgQuat[2], avgQuat[3]);
+
       if(m_CrystalStructures[m_FeaturePhases[i]] == m_CrystalStructures[m_FeaturePhases[nname]] && m_FeaturePhases[i] > 0)
       {
-        m_OrientationOps[m_CrystalStructures[m_FeaturePhases[i]]]->getmPrime(q1, q2, LD, mprime);
-        m_OrientationOps[m_CrystalStructures[m_FeaturePhases[i]]]->getF1(q1, q2, LD, true, F1);
-        m_OrientationOps[m_CrystalStructures[m_FeaturePhases[i]]]->getF1spt(q1, q2, LD, true, F1spt);
-        m_OrientationOps[m_CrystalStructures[m_FeaturePhases[i]]]->getF7(q1, q2, LD, true, F7);
+        m_OrientationOps[m_CrystalStructures[m_FeaturePhases[i]]]->getmPrime(q1, q2, LD);
+        m_OrientationOps[m_CrystalStructures[m_FeaturePhases[i]]]->getF1(q1, q2, LD, true);
+        m_OrientationOps[m_CrystalStructures[m_FeaturePhases[i]]]->getF1spt(q1, q2, LD, true);
+        m_OrientationOps[m_CrystalStructures[m_FeaturePhases[i]]]->getF7(q1, q2, LD, true);
       }
       else
       {

commit 6cf178fe84d359b541124455c5c3fd602ddeeae2

Fix bug that was introduced in 1ab713bcdc39c where variable were never set.
Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

Source/Plugins/OrientationAnalysis/OrientationAnalysisFilters/FindSlipTransmissionMetrics.cpp
@@ -240,7 +240,7 @@ void FindSlipTransmissionMetrics::execute()
   std::vector<std::vector<float>> F7lists;
   std::vector<std::vector<float>> mPrimelists;
 
-  double mprime = 0.0f, F1 = 0.0f, F1spt = 0.0f, F7 = 0.0f;
+  double mprime = 0.0, F1 = 0.0, F1spt = 0.0, F7 = 0.0;
   int32_t nname;
 
   // QuatF* avgQuats = reinterpret_cast<QuatF*>(m_AvgQuats);
@@ -269,10 +269,10 @@ void FindSlipTransmissionMetrics::execute()
 
       if(m_CrystalStructures[m_FeaturePhases[i]] == m_CrystalStructures[m_FeaturePhases[nname]] && m_FeaturePhases[i] > 0)
       {
-        m_OrientationOps[m_CrystalStructures[m_FeaturePhases[i]]]->getmPrime(q1, q2, LD);
-        m_OrientationOps[m_CrystalStructures[m_FeaturePhases[i]]]->getF1(q1, q2, LD, true);
-        m_OrientationOps[m_CrystalStructures[m_FeaturePhases[i]]]->getF1spt(q1, q2, LD, true);
-        m_OrientationOps[m_CrystalStructures[m_FeaturePhases[i]]]->getF7(q1, q2, LD, true);
+        mprime = m_OrientationOps[m_CrystalStructures[m_FeaturePhases[i]]]->getmPrime(q1, q2, LD);
+        F1 = m_OrientationOps[m_CrystalStructures[m_FeaturePhases[i]]]->getF1(q1, q2, LD, true);
+        F1spt = m_OrientationOps[m_CrystalStructures[m_FeaturePhases[i]]]->getF1spt(q1, q2, LD, true);
+        F7 = m_OrientationOps[m_CrystalStructures[m_FeaturePhases[i]]]->getF7(q1, q2, LD, true);
       }
       else
       {