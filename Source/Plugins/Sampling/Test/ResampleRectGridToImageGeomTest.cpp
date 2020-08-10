e78b749418e95ccee55d0afc7e4e40163a978cbf
Michael Jackson <mike.jackson@bluequartz.net>
2020-08-10 11:45:35 -0400

    Missing unit test for ResampleRectGridToImageGeom
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- Source/Plugins/Sampling/Test/ResampleRectGridToImageGeomTest.cpp
+++ Source/Plugins/Sampling/Test/ResampleRectGridToImageGeomTest.cpp
@@ -31,63 +31,9 @@ class ResampleRectGridToImageGeomTest
     ResampleRectGridToImageGeomTest& operator=(ResampleRectGridToImageGeomTest&&) = delete;      // Move Assignment
 
   // -----------------------------------------------------------------------------
-  //
-  // -----------------------------------------------------------------------------
-  int TestFilterAvailability()
-  {
-    // Now instantiate the ResampleRectGridToImageGeomTest Filter from the FilterManager
-    QString filtName = "ResampleRectGridToImageGeom";
-    FilterManager* fm = FilterManager::Instance();
-    IFilterFactory::Pointer filterFactory = fm->getFactoryFromClassName(filtName);
-    if (nullptr == filterFactory.get())
-    {
-      std::stringstream ss;
-      ss << "The ResampleRectGridToImageGeomTest Requires the use of the " << filtName.toStdString()
-         << " filter which is found in the Sampling Plugin";
-      DREAM3D_TEST_THROW_EXCEPTION(ss.str())
-    }
-    return 0;
-  }
-
-  // -----------------------------------------------------------------------------
-  //
-  // -----------------------------------------------------------------------------
-  int TestResampleRectGridToImageGeomTest()
-  {
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-   /* Please write ResampleRectGridToImageGeomTest test code here.
-    *
-    * To create IO test files, please edit the file template at Sampling/Test/TestFileLocations.h.in.
-    * Add a ResampleRectGridToImageGeomTest namespace inside the UnitTest namespace, and add your test file paths to your new namespace.
-    *
-    * SIMPLib provides some macros that will throw exceptions when a test fails
-    * and thus report that during testing. These macros are located in the
-    * SIMPLib/Utilities/UnitTestSupport.hpp file. Some examples are:
-    *
-    * SIMPLib_REQUIRE_EQUAL(foo, 0)
-    * This means that if the variable foo is NOT equal to Zero then test will fail
-    * and the current test will exit immediately. If there are more tests registered
-    * with the SIMPLib_REGISTER_TEST() macro, the next test will execute. There are
-    * lots of examples in the SIMPLib/Test folder to look at.
-    */
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-    int foo = -1;
-    DREAM3D_REQUIRE_EQUAL(foo, 0)
-
-    return EXIT_SUCCESS;
-  }
-
-  // -----------------------------------------------------------------------------
-  //
-  // -----------------------------------------------------------------------------
   void operator()()
   {
     int err = EXIT_SUCCESS;
-
-    DREAM3D_REGISTER_TEST( TestFilterAvailability() );
-
-    DREAM3D_REGISTER_TEST( TestResampleRectGridToImageGeomTest() )
   }
 
   private:
