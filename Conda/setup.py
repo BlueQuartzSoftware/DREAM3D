e7171f670b9949422b5dbf7457e7bd20e108e7c5
Michael Jackson <mike.jackson@bluequartz.net>
2021-04-15 12:23:24 -0400

    Update Conda Package version of DREAM.3D
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- Conda/setup.py
+++ Conda/setup.py
@@ -4,10 +4,10 @@ from sysconfig import get_config_var
 ext = get_config_var('EXT_SUFFIX')
 
 setup(
-  name='dream3d',
-  version='1.0.0',
-  packages=find_packages(),
-  package_data={'dream3d': [f'*{ext}']},
-  include_package_data=True,
-  zip_safe=False
+    name='dream3d',
+    version='6.6.0',
+    packages=find_packages(),
+    package_data={'dream3d': [f'*{ext}']},
+    include_package_data=True,
+    zip_safe=False
 )
