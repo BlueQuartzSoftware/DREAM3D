279f30eae607b0c51485b48c328c3b938062622a
Jared Duffey <jared.duffey@bluequartz.net>
2021-08-10 15:38:34 -0400

    Changed setup.py python package version to 1.0.0 to match meta.yaml
    Signed-off-by: Jared Duffey <jared.duffey@bluequartz.net>

--- Conda/setup.py
+++ Conda/setup.py
@@ -5,7 +5,7 @@ ext = get_config_var('EXT_SUFFIX')
 
 setup(
     name='dream3d',
-    version='6.6.0',
+    version='1.0.0',
     packages=find_packages(),
     package_data={'dream3d': [f'*{ext}']},
     include_package_data=True,
