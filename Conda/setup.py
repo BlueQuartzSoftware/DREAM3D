fe4711ce08152bb5706d04d65e3879ca6c94f0f6
Michael Jackson <mike.jackson@bluequartz.net>
2021-08-10 15:58:51 -0400

    Increment the dream3d-conda version to 1.1.0
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- Conda/setup.py
+++ Conda/setup.py
@@ -5,7 +5,7 @@ ext = get_config_var('EXT_SUFFIX')
 
 setup(
     name='dream3d',
-    version='1.0.0',
+    version='1.1.0',
     packages=find_packages(),
     package_data={'dream3d': [f'*{ext}', '*.pyi', 'py.typed']},
     include_package_data=True,
