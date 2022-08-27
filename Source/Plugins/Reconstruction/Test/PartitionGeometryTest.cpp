3082c86b1222cdc1798ae21202b8153e6e78b56b
Joey Kleingers <joey.kleingers@bluequartz.net>
2022-08-27 10:30:03 -0400

    Updating licenses.
    Signed-off-by: Joey Kleingers <joey.kleingers@bluequartz.net>

--- Source/Plugins/Reconstruction/Test/PartitionGeometryTest.cpp
+++ Source/Plugins/Reconstruction/Test/PartitionGeometryTest.cpp
@@ -1,6 +1,33 @@
-// -----------------------------------------------------------------------------
-// Insert your license & copyright information here
-// -----------------------------------------------------------------------------
+/* ============================================================================
+ * Copyright (c) 2022-2022 BlueQuartz Software, LLC
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of BlueQuartz Software, the US Air Force, nor the names of its
+ * contributors may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
 #pragma once
 
 #include <QtCore/QCoreApplication>
@@ -8,27 +35,26 @@
 
 #include "SIMPLib/SIMPLib.h"
 #include "SIMPLib/DataArrays/DataArray.hpp"
-#include "SIMPLib/Filtering/FilterPipeline.h"
-#include "SIMPLib/Filtering/FilterManager.h"
 #include "SIMPLib/Filtering/FilterFactory.hpp"
+#include "SIMPLib/Filtering/FilterManager.h"
+#include "SIMPLib/Filtering/FilterPipeline.h"
+#include "SIMPLib/Filtering/QMetaObjectUtilities.h"
 #include "SIMPLib/Plugin/ISIMPLibPlugin.h"
 #include "SIMPLib/Plugin/SIMPLibPluginLoader.h"
-#include "SIMPLib/Filtering/QMetaObjectUtilities.h"
 
-#include "UnitTestSupport.hpp"
-
-#include "ReconstructionTestFileLocations.h"
+#include "Reconstruction/Test/ReconstructionTestFileLocations.h"
+#include "Reconstruction/Test/UnitTestSupport.hpp"
 
 class PartitionGeometryTest
 {
 
-  public:
-    PartitionGeometryTest() = default;
-    ~PartitionGeometryTest() = default;
-    PartitionGeometryTest(const PartitionGeometryTest&) = delete;            // Copy Constructor
-    PartitionGeometryTest(PartitionGeometryTest&&) = delete;                 // Move Constructor
-    PartitionGeometryTest& operator=(const PartitionGeometryTest&) = delete; // Copy Assignment
-    PartitionGeometryTest& operator=(PartitionGeometryTest&&) = delete;      // Move Assignment
+public:
+  PartitionGeometryTest() = default;
+  ~PartitionGeometryTest() = default;
+  PartitionGeometryTest(const PartitionGeometryTest&) = delete;            // Copy Constructor
+  PartitionGeometryTest(PartitionGeometryTest&&) = delete;                 // Move Constructor
+  PartitionGeometryTest& operator=(const PartitionGeometryTest&) = delete; // Copy Assignment
+  PartitionGeometryTest& operator=(PartitionGeometryTest&&) = delete;      // Move Assignment
 
   // -----------------------------------------------------------------------------
   //
@@ -39,11 +65,10 @@ class PartitionGeometryTest
     QString filtName = "PartitionGeometry";
     FilterManager* fm = FilterManager::Instance();
     IFilterFactory::Pointer filterFactory = fm->getFactoryFromClassName(filtName);
-    if (nullptr == filterFactory.get())
+    if(nullptr == filterFactory.get())
     {
       std::stringstream ss;
-      ss << "The PartitionGeometryTest Requires the use of the " << filtName.toStdString()
-         << " filter which is found in the Reconstruction Plugin";
+      ss << "The PartitionGeometryTest Requires the use of the " << filtName.toStdString() << " filter which is found in the Reconstruction Plugin";
       DREAM3D_TEST_THROW_EXCEPTION(ss.str())
     }
     return 0;
@@ -52,24 +77,24 @@ class PartitionGeometryTest
   // -----------------------------------------------------------------------------
   //
   // -----------------------------------------------------------------------------
-  int TestPartitionGeometryTest()
+  int TestImageGeometry()
   {
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-   /* Please write PartitionGeometryTest test code here.
-    *
-    * To create IO test files, please edit the file template at Reconstruction/Test/TestFileLocations.h.in.
-    * Add a PartitionGeometryTest namespace inside the UnitTest namespace, and add your test file paths to your new namespace.
-    *
-    * SIMPLib provides some macros that will throw exceptions when a test fails
-    * and thus report that during testing. These macros are located in the
-    * SIMPLib/Utilities/UnitTestSupport.hpp file. Some examples are:
-    *
-    * SIMPLib_REQUIRE_EQUAL(foo, 0)
-    * This means that if the variable foo is NOT equal to Zero then test will fail
-    * and the current test will exit immediately. If there are more tests registered
-    * with the SIMPLib_REGISTER_TEST() macro, the next test will execute. There are
-    * lots of examples in the SIMPLib/Test folder to look at.
-    */
+    /* Please write PartitionGeometryTest test code here.
+     *
+     * To create IO test files, please edit the file template at Reconstruction/Test/TestFileLocations.h.in.
+     * Add a PartitionGeometryTest namespace inside the UnitTest namespace, and add your test file paths to your new namespace.
+     *
+     * SIMPLib provides some macros that will throw exceptions when a test fails
+     * and thus report that during testing. These macros are located in the
+     * SIMPLib/Utilities/UnitTestSupport.hpp file. Some examples are:
+     *
+     * SIMPLib_REQUIRE_EQUAL(foo, 0)
+     * This means that if the variable foo is NOT equal to Zero then test will fail
+     * and the current test will exit immediately. If there are more tests registered
+     * with the SIMPLib_REGISTER_TEST() macro, the next test will execute. There are
+     * lots of examples in the SIMPLib/Test folder to look at.
+     */
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     int foo = -1;
@@ -84,14 +109,12 @@ class PartitionGeometryTest
   void operator()()
   {
     int err = EXIT_SUCCESS;
+    Q_UNUSED(err)
 
-    DREAM3D_REGISTER_TEST( TestFilterAvailability() );
+    DREAM3D_REGISTER_TEST(TestFilterAvailability());
 
-    DREAM3D_REGISTER_TEST( TestPartitionGeometryTest() )
+    DREAM3D_REGISTER_TEST(TestImageGeometry())
   }
 
-  private:
-
-
+private:
 };
-
